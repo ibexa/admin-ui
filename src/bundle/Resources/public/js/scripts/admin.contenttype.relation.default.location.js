ezsystems/ezplatform-admin-ui

IBX-808: As an Editor, I want to be able to reset starting location for ezobjectrelation FieldType (#1880)

src/bundle/Resources/public/js/scripts/admin.contenttype.relation.default.location.js
@@ -1,5 +1,7 @@
 (function (global, doc, React, ReactDOM) {
-    const btns = doc.querySelectorAll('.btn--udw-relation-default-location');
+    const SELECTOR_RESET_STARTING_LOCATION_BTN = '.ez-btn--reset-starting-location';
+    const resetStartingLocationBtns = doc.querySelectorAll(SELECTOR_RESET_STARTING_LOCATION_BTN);
+    const udwBtns = doc.querySelectorAll('.btn--udw-relation-default-location');
     const udwContainer = doc.getElementById('react-udw');
     const token = doc.querySelector('meta[name="CSRF-Token"]').content;
     const siteaccess = doc.querySelector('meta[name="SiteAccess"]').content;
@@ -11,6 +13,11 @@
         const locationName = items[0].ContentInfo.Content.TranslatedName;
         const objectRelationListSettingsWrapper = btn.closest('.ezobjectrelationlist-settings');
         const objectRelationSettingsWrapper = btn.closest('.ezobjectrelation-settings');
+
+        toggleResetStartingLocationBtn(
+            btn.parentNode.querySelector(SELECTOR_RESET_STARTING_LOCATION_BTN),
+            true
+        );
 
         if (objectRelationListSettingsWrapper) {
             objectRelationListSettingsWrapper.querySelector(btn.dataset.relationRootInputSelector).value = locationId;
@@ -35,6 +42,23 @@
             restInfo: { token, siteaccess }
         }, config)), udwContainer);
     };
+    const toggleResetStartingLocationBtn = (button, isEnabled) => {
+        if (isEnabled) {
+            button.removeAttribute('disabled');
+        } else {
+            button.setAttribute('disabled', true);
+        }
+    };
+    const resetStartingLocation = (event) => {
+        const button = event.currentTarget;
+        const { relationRootInputSelector, relationSelectedRootNameSelector } = button.dataset;
 
-    btns.forEach(btn => btn.addEventListener('click', openUDW, false));
+        doc.querySelector(relationRootInputSelector).value = '';
+        doc.querySelector(relationSelectedRootNameSelector).innerHTML = '';
+
+        toggleResetStartingLocationBtn(button, false);
+    };
+
+    udwBtns.forEach(btn => btn.addEventListener('click', openUDW, false));
+    resetStartingLocationBtns.forEach(btn => btn.addEventListener('click', resetStartingLocation, false));
 })(window, window.document, window.React, window.ReactDOM);

ibexa/admin-ui

src/bundle/Resources/public/js/scripts/admin.contenttype.relation.default.location.js
@@ -1,5 +1,7 @@
-(function(global, doc, eZ, React, ReactDOM) {
-    const btns = doc.querySelectorAll('.ibexa-btn--udw-relation-default-location');
+(function (global, doc, React, ReactDOM) {
+    const SELECTOR_RESET_STARTING_LOCATION_BTN = '.ibexa-btn--reset-starting-location';
+    const resetStartingLocationBtns = doc.querySelectorAll(SELECTOR_RESET_STARTING_LOCATION_BTN);
+    const udwBtns = doc.querySelectorAll('.ibexa-btn--udw-relation-default-location');
     const udwContainer = doc.getElementById('react-udw');
     const closeUDW = () => ReactDOM.unmountComponentAtNode(udwContainer);
     const onConfirm = (btn, items) => {
@@ -9,6 +11,11 @@
         const locationName = items[0].ContentInfo.Content.TranslatedName;
         const objectRelationListSettingsWrapper = btn.closest('.ezobjectrelationlist-settings');
         const objectRelationSettingsWrapper = btn.closest('.ezobjectrelation-settings');
+
+        toggleResetStartingLocationBtn(
+            btn.parentNode.querySelector(SELECTOR_RESET_STARTING_LOCATION_BTN),
+            true
+        );
 
         if (objectRelationListSettingsWrapper) {
             objectRelationListSettingsWrapper.querySelector(btn.dataset.relationRootInputSelector).value = locationId;
@@ -35,6 +42,23 @@
             udwContainer
         );
     };
+    const toggleResetStartingLocationBtn = (button, isEnabled) => {
+        if (isEnabled) {
+            button.removeAttribute('disabled');
+        } else {
+            button.setAttribute('disabled', true);
+        }
+    };
+    const resetStartingLocation = (event) => {
+        const button = event.currentTarget;
+        const { relationRootInputSelector, relationSelectedRootNameSelector } = button.dataset;
 
-    btns.forEach((btn) => btn.addEventListener('click', openUDW, false));
-})(window, window.document, window.eZ, window.React, window.ReactDOM);
+        doc.querySelector(relationRootInputSelector).value = '';
+        doc.querySelector(relationSelectedRootNameSelector).innerHTML = '';
+
+        toggleResetStartingLocationBtn(button, false);
+    };
+
+    udwBtns.forEach(btn => btn.addEventListener('click', openUDW, false));
+    resetStartingLocationBtns.forEach(btn => btn.addEventListener('click', resetStartingLocation, false));
+})(window, window.document, window.React, window.ReactDOM);