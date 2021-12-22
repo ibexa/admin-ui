5c48219827ab2db960c8562263a90aa9bd5de198
Dawid Parafiński <dawid.parafinski@ez.no>
2021-12-22 17:07:47 +0100

    IBX-1109: Aligned views with groupable user settings

--- src/bundle/Resources/public/js/scripts/admin.settings.datetimeformat.update.js
+++ src/bundle/Resources/public/js/scripts/admin.settings.datetimeformat.update.js
@@ -1,16 +1,36 @@
 (function(global, doc, moment) {
-    const SELECTOR_DATE_FORMAT = '#user_setting_update_value_date_format';
-    const SELECTOR_TIME_FORMAT = '#user_setting_update_value_time_format';
+    const SELECTOR_SHORT_DATE_FORMAT = '#user_setting_update_short_datetime_format_value_date_format';
+    const SELECTOR_SHORT_TIME_FORMAT = '#user_setting_update_short_datetime_format_value_time_format';
+
+    const SELECTOR_FULL_DATE_FORMAT = '#user_setting_update_full_datetime_format_value_date_format';
+    const SELECTOR_FULL_TIME_FORMAT = '#user_setting_update_full_datetime_format_value_time_format';
+
     const SELECTOR_VALUE_PREVIEW = '.ibexa-datetime-format-preview-value';
-    const valuePreview = doc.querySelector(SELECTOR_VALUE_PREVIEW);
-    const dateFormatSelect = doc.querySelector(SELECTOR_DATE_FORMAT);
-    const timeFormatSelect = doc.querySelector(SELECTOR_TIME_FORMAT);
-    const updateDateTimeFormatPreview = () => {
+
+    const dateFormatSelect = doc.querySelector(SELECTOR_SHORT_DATE_FORMAT);
+    const timeFormatSelect = doc.querySelector(SELECTOR_SHORT_TIME_FORMAT);
+
+    const fullDateFormatSelect = doc.querySelector(SELECTOR_FULL_DATE_FORMAT);
+    const fullTimeFormatSelect = doc.querySelector(SELECTOR_FULL_TIME_FORMAT);
+
+    const updateDateTimeFormatPreview = (valuePreview) => {
         valuePreview.innerHTML = moment().formatICU(`${dateFormatSelect.value} ${timeFormatSelect.value}`);
     };
 
-    dateFormatSelect.addEventListener('change', updateDateTimeFormatPreview);
-    timeFormatSelect.addEventListener('change', updateDateTimeFormatPreview);
+    if (dateFormatSelect) {
+        let valuePreview = dateFormatSelect.parentElement.parentElement.parentElement.parentElement.parentElement.querySelector(SELECTOR_VALUE_PREVIEW);
 
-    updateDateTimeFormatPreview();
+        dateFormatSelect.addEventListener('change', () => { updateDateTimeFormatPreview(valuePreview) });
+        timeFormatSelect.addEventListener('change', () => { updateDateTimeFormatPreview(valuePreview) });
+        updateDateTimeFormatPreview(valuePreview);
+    }
+
+    if (fullDateFormatSelect) {
+        let valuePreview = fullDateFormatSelect.parentElement.parentElement.parentElement.parentElement.parentElement.querySelector(SELECTOR_VALUE_PREVIEW);
+
+        fullDateFormatSelect.addEventListener('change', () => { updateDateTimeFormatPreview(valuePreview) });
+        fullTimeFormatSelect.addEventListener('change', () => { updateDateTimeFormatPreview(valuePreview) });
+        updateDateTimeFormatPreview(valuePreview);
+    }
+
 })(window, window.document, window.moment);
