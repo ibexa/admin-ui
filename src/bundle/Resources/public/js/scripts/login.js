4fd47e561903cdb2c5bf48b372419177acf952c3
Dariusz Szut <dew326@gmail.com>
2022-01-13 08:33:32 +0100

    IBX-1866: Disable login when fields are empty (#172)

--- src/bundle/Resources/public/js/scripts/login.js
+++ src/bundle/Resources/public/js/scripts/login.js
@@ -2,6 +2,14 @@
     const passwordInputNode = doc.querySelector('.ibexa-login__input--password');
     const viewIconNode = doc.querySelector('.ibexa-login__password-visibility-toggler .ibexa-icon--view');
     const viewHideIconNode = doc.querySelector('.ibexa-login__password-visibility-toggler .ibexa-icon--view-hide');
+    const loginBtn = doc.querySelector('.ibexa-login__btn--sign-in');
+    const nameInput = doc.querySelector('.ibexa-login__input--name');
+    const passwordInput = doc.querySelector('.ibexa-login__input--password');
+    const toggleLoginBtnState = () => {
+        const shouldBeDisabled = !nameInput.value || !passwordInput.value;
+
+        loginBtn.toggleAttribute('disabled', shouldBeDisabled);
+    };
 
     doc.querySelector('.ibexa-login__password-visibility-toggler').addEventListener('click', (event) => {
         if (passwordInputNode) {
@@ -12,4 +20,11 @@
             viewHideIconNode.classList.toggle('d-none');
         }
     });
+
+    if (loginBtn) {
+        nameInput.addEventListener('keyup', toggleLoginBtnState, false);
+        passwordInput.addEventListener('keyup', toggleLoginBtnState, false);
+
+        toggleLoginBtnState();
+    }
 })(window, window.document);
