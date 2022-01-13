56b062349f599fc5d496a7ab97c3f0fc30a616f7
Michał Grabowski <michal.grabowski@ibexa.co>
2022-01-13 12:52:09 +0100

    IBX-1880: [Taxonomy] Remove subitems from location view (#175)

--- src/bundle/Resources/public/js/scripts/sidebar/btn/location.create.js
+++ src/bundle/Resources/public/js/scripts/sidebar/btn/location.create.js
@@ -1,5 +1,10 @@
 (function(global, doc) {
     const createActions = doc.querySelector('.ibexa-extra-actions--create');
+
+    if (!createActions) {
+        return;
+    }
+
     const btns = createActions.querySelectorAll('.form-check [type="radio"]');
     const form = createActions.querySelector('form');
 
