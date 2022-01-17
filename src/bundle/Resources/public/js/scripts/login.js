102201719627e9ad41c35f86368783b593ee304d
Jakub Brzegowski <symfiz@gmail.com>
2022-01-17 14:43:07 +0100

    IBX-1956: Detect username/password automatic autofill on login page

--- src/bundle/Resources/public/js/scripts/login.js
+++ src/bundle/Resources/public/js/scripts/login.js
@@ -1,4 +1,5 @@
 (function(global, doc) {
+    const AUTOFILL_TIMEOUT = 500;
     const passwordInputNode = doc.querySelector('.ibexa-login__input--password');
     const viewIconNode = doc.querySelector('.ibexa-login__password-visibility-toggler .ibexa-icon--view');
     const viewHideIconNode = doc.querySelector('.ibexa-login__password-visibility-toggler .ibexa-icon--view-hide');
@@ -9,6 +10,13 @@
         const shouldBeDisabled = !nameInput.value || !passwordInput.value;
 
         loginBtn.toggleAttribute('disabled', shouldBeDisabled);
+    };
+    const handleAutofill = () => {
+        const isNameInputAutofilled = nameInput.matches(':-webkit-autofill');
+        const isPasswordInputAutofilled = nameInput.matches(':-webkit-autofill');
+        const shouldLoginBtnBeDisabled = !isNameInputAutofilled || !isPasswordInputAutofilled;
+
+        loginBtn.toggleAttribute('disabled', shouldLoginBtnBeDisabled);
     };
 
     doc.querySelector('.ibexa-login__password-visibility-toggler').addEventListener('click', (event) => {
@@ -26,5 +34,7 @@
         passwordInput.addEventListener('keyup', toggleLoginBtnState, false);
 
         toggleLoginBtnState();
+
+        global.setTimeout(handleAutofill, AUTOFILL_TIMEOUT);
     }
 })(window, window.document);
