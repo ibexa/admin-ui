0a37bbb9036d0ee4b4cfe5f2536e21a0ccbbf8b0
Dariusz Szut <dew326@gmail.com>
2022-01-30 10:30:52 +0100

    IBX-1761: Updated UDW design

--- src/bundle/ui-dev/src/modules/universal-discovery/components/content-create-button/content.create.button.js
+++ src/bundle/ui-dev/src/modules/universal-discovery/components/content-create-button/content.create.button.js
@@ -30,11 +30,11 @@ const ContentCreateButton = ({ isDisabled }) => {
 
     if (!selectedLocation && loadedLocationsMap.length) {
         selectedLocation = loadedLocationsMap[loadedLocationsMap.length - 1].subitems.find(
-            (subitem) => subitem.location.id === markedLocationId
+            (subitem) => subitem.location.id === markedLocationId,
         );
     }
 
-    const contentTypeInfo = contentTypesMap[selectedLocation?.location?.ContentInfo.Content.ContentType._href];
+    const contentTypeInfo = contentTypesMap[(selectedLocation?.location?.ContentInfo.Content.ContentType._href)];
     const isAllowedLocation = selectedLocation && (!allowedLocations || allowedLocations.includes(selectedLocation.parentLocationId));
     const hasAccess =
         !selectedLocation ||
@@ -53,9 +53,9 @@ const ContentCreateButton = ({ isDisabled }) => {
                 className="c-content-create-button__btn btn ibexa-btn ibexa-btn--dark"
                 disabled={isDisabled || !hasAccess || !isAllowedLocation || isLimitReached || !isContainer}
                 onClick={toggleContentCreateVisibility}
-                data-tooltip-container-selector=".c-top-menu"
-                title={createLabel}>
-                <Icon name="create" extraClasses="ibexa-icon--small" /> {createLabel}
+            >
+                <Icon name="create" extraClasses="ibexa-icon--small" />
+                <span className="ibexa-btn__label">{createLabel}</span>
             </button>
         </div>
     );
@@ -78,7 +78,7 @@ ibexa.addConfig(
             component: ContentCreateButton,
         },
     ],
-    true
+    true,
 );
 
 export default ContentCreateButton;
