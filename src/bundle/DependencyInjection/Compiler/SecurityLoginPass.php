2f2c9133304aabed8346f7e8e3e71ddeca7c1b32
Andrew Longosz <alongosz@users.noreply.github.com>
2022-02-01 18:51:51 +0100

    IBX-1696: Rebranded Container parameters and Config Resolver namespaces (#284)
    Rebranded service container parameter names and Ibexa Config Resolver namespaces to follow unified pattern.
    
    See https://issues.ibexa.co/browse/IBX-1696 for more details.

--- src/bundle/DependencyInjection/Compiler/SecurityLoginPass.php
+++ src/bundle/DependencyInjection/Compiler/SecurityLoginPass.php
@@ -21,7 +21,7 @@ class SecurityLoginPass implements CompilerPassInterface
     {
         $successHandlerDef = $container->getDefinition('security.authentication.success_handler');
         $successHandlerDef->setClass(RedirectToDashboardAuthenticationSuccessHandler::class);
-        $successHandlerDef->addArgument($container->getParameter('ezpublish.siteaccess.groups'));
+        $successHandlerDef->addArgument($container->getParameter('ibexa.site_access.groups'));
         $successHandlerDef->addArgument('ibexa.dashboard');
     }
 }
