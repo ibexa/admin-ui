f73b7300f57ec3021881a0383a5f2169b0c4d2aa
Michał Grabowski <michal.grabowski@ibexa.co>
2022-02-14 11:33:40 +0100

    clearInstance helper

--- src/bundle/Resources/public/js/scripts/helpers/object.instances.js
+++ src/bundle/Resources/public/js/scripts/helpers/object.instances.js
@@ -9,9 +9,13 @@
     const getInstance = (domElement) => {
         return domElement.ibexaInstance;
     }
+    const clearInstance = (domElement) => {
+        delete domElement.ibexaInstance;
+    }
 
     ibexa.addConfig('helpers.objectInstances', {
         setInstance,
         getInstance,
+        clearInstance,
     });
 })(window, window.document, window.ibexa);
