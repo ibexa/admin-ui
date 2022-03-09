0ba1613869a11a92a568257378e3ca266b0d6735
Michał Grabowski <michal.grabowski@ibexa.co>
2022-03-09 11:38:09 +0100

    hide on top

--- src/bundle/Resources/public/js/scripts/admin.back.to.top.js
+++ src/bundle/Resources/public/js/scripts/admin.back.to.top.js
@@ -1,11 +1,15 @@
 (function (global, doc) {
     const backToTopBtn = doc.querySelector('.ibexa-back-to-top__button');
     const backToTopAnchor = doc.querySelector('.ibexa-back-to-top-anchor');
+    const backToTopScrollContainer = doc.querySelector('.ibexa-back-to-top-scroll-container');
 
-    if (!backToTopBtn || !backToTopAnchor) {
+    if (!backToTopBtn || !backToTopAnchor || !backToTopScrollContainer) {
         return;
     }
 
+    backToTopScrollContainer.addEventListener('scroll', (event) => {
+        backToTopBtn.classList.toggle('ibexa-back-to-top__button--visible', event.target.scrollTop !== 0);
+    });
     backToTopBtn.addEventListener('click', () => {
         backToTopAnchor.scrollIntoView({
             behavior: 'smooth',
