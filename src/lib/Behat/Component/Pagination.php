589eba76b703abd829e5f2abac6a9bb8ddce1a51
Marek Nocoń <mnocon@users.noreply.github.com>
2022-03-21 15:51:06 +0100

    [Behat] Increased go to the top timeout

--- src/lib/Behat/Component/Pagination.php
+++ src/lib/Behat/Component/Pagination.php
@@ -23,7 +23,10 @@ class Pagination extends Component
         $currentPage = (int) $this->getHTMLPage()->find($this->getLocator('currentPage'))->getText();
         // scroll to the bottom to avoid "Go to top" button
         $this->getHTMLPage()->executeJavaScript("document.querySelector('.ibexa-back-to-top-scroll-container').scrollTo(0, document.querySelector('.ibexa-back-to-top-scroll-container').scrollHeight)");
-        $this->getHTMLPage()->find(new VisibleCSSLocator('backToTopWithTitle', '.ibexa-back-to-top__title--visible'))->assert()->textEquals('Go to top');
+        $this->getHTMLPage()
+            ->setTimeout(3)
+            ->find(new VisibleCSSLocator('backToTopWithTitle', '.ibexa-back-to-top__title--visible'))
+            ->assert()->textEquals('Go to top');
         $this->getHTMLPage()->find($this->getLocator('nextButton'))->click();
         $this->getHTMLPage()->setTimeout(10)->waitUntil(function () use ($currentPage) {
             $activePge = (int) $this->getHTMLPage()->find($this->getLocator('currentPage'))->getText();
