9f1a598847960681883df6297dbfd104c24beb6c
Lukasz Ostafin <ostafin.lucas@gmail.com>
2022-03-24 21:21:16 +0100

    Filters

--- src/bundle/Resources/public/js/scripts/filterField/base.js
+++ src/bundle/Resources/public/js/scripts/filterField/base.js
@@ -1,35 +1,3 @@
 (function (global, doc) {
-    const sidebarFilters = doc.querySelectorAll('.ibexa-sidebar-filter');
-    const sidebarFiltersHeader = doc.querySelector('.ibexa-sidebar-filter__header');
 
-    const toggleSearch = ({ currentTarget }) => {
-        const filterHeaderNode = currentTarget.closest('.ibexa-sidebar-filter__header');
-        const filterLabelNode = filterHeaderNode.querySelector('.ibexa-sidebar-filter__header-label');
-        const filterSearchInput = filterHeaderNode.querySelector('.ibexa-sidebar-filter__search-input');
-        const filterActionBtns = filterHeaderNode.querySelectorAll('.ibexa-sidebar-filter__header-actions .ibexa-btn:not(.accordion-button)');
-
-        filterLabelNode.classList.toggle('ibexa-sidebar-filter__header-label--hidden');
-        filterSearchInput.classList.toggle('ibexa-sidebar-filter__search-input--hidden');
-        filterActionBtns.forEach((actionBtn) => {
-            actionBtn.classList.toggle('ibexa-btn--hidden');
-        });
-    }
-
-    const attachEvents = (sidebarFilterNode) => {
-        const toggleSearchBtn = sidebarFilterNode.querySelector('.ibexa-sidebar-filter__search-input-toggle');
-
-        toggleSearchBtn.addEventListener('click', toggleSearch, false);
-    }
-
-    sidebarFilters.forEach((sidebarFilter) => {
-        attachEvents(sidebarFilter);
-    });
-
-    // doc.addEventListener('click', ({ target }) => {
-    //     const isClickOusideHeader = [...sidebarFiltersHeader].every((filterHeader) => {
-    //         filterHeader.contains(target);
-    //     });
-        
-    //     console.log(isClickOusideHeader);
-    // }, false);
 })(window, window.document);
--- src/bundle/Resources/public/js/scripts/filterField/selection.js
+++ src/bundle/Resources/public/js/scripts/filterField/selection.js
@@ -1,25 +1,129 @@
 (function (global, doc) {
-    const toggleListBtns = doc.querySelectorAll('.ibexa-sidebar-filter__toggle-list-btn');
-    const calculateListHeight = (items_limit_count) => {
+    class selectionFilter {
+        constructor(config = {}) {
+            // nodes
+            this.wrapper = config.wrapper;
 
-    };
-    const toggleList = ({ currentTarget }) => {
-        const { itemsShowLimit } = currentTarget.dataset;
-        const filterBody = currentTarget.closest('.ibexa-sidebar-filter__body');
-        const selectionList = filterBody.querySelector('.ibexa-sidebar-filter__selection-list');
-        const selectionListItems = selectionList.querySelectorAll('.ibexa-sidebar-filter__selection-list-item');
+            this.searchToggleBtn = this.wrapper.querySelector('.ibexa-sidebar-filter-selection__search-toggle-btn');
+            this.searchWrapper = this.wrapper.querySelector('.ibexa-sidebar-filter-selection__search-wrapper');
+            this.searchInput = this.wrapper.querySelector('.ibexa-sidebar-filter-selection__search-input');
 
-        selectionListItems.forEach((listItem, index) => {
-            const isShortList = selectionList.classList.contains('ibexa-sidebar-filter__selection-list-item--short');
-            const isIndexOverLimit = index >= itemsShowLimit;
-            const shouldHiddenItem = !isShortList && isIndexOverLimit;
+            this.toggleItemsCheckStateBtn = this.wrapper.querySelector('.ibexa-sidebar-filter__toggle-checks-state-btn');
+            this.list = this.wrapper.querySelector('.ibexa-sidebar-filter-selection__list');
+            this.listItems = this.wrapper.querySelectorAll('.ibexa-sidebar-filter-selection__list-item');
+            this.toggleListBtn = this.wrapper.querySelector('.ibexa-sidebar-filter-selection__list-toggle-btn');
 
-            listItem.classList.toggle('ibexa-sidebar-filter__selection-list-item--hidden', shouldHiddenItem);
+            // Varaibels
+            this.isSearchExpanded = config.isSearchExpanded || false;
+            this.isListExpanded = config.isListExpanded || false;
+            this.itemsCheckedCount = config.itemsCheckedCount || 0;
+            this.itemsShortListLimit = config.wrapper.dataset.itemsShortListLimit;
+
+            //Functions
+            this.toggleSearchBar = this.toggleSearchBar.bind(this);
+            this.toggleItemsCheckState = this.toggleItemsCheckState.bind(this);
+            this.setToggleItemsCheckStateBtnLabel = this.setToggleItemsCheckStateBtnLabel.bind(this);
+            this.filterItems = this.filterItems.bind(this);
+            this.toggleListItems = this.toggleListItems.bind(this);
+        }
+
+        toggleSearchBar() {
+            this.isSearchExpanded = !this.isSearchExpanded;
+
+            this.searchToggleBtn.classList.toggle('ibexa-btn--selected', this.isSearchExpanded);
+            this.searchWrapper.classList.toggle('ibexa-sidebar-filter-selection__search-wrapper--hidden', !this.isSearchExpanded);
+            this.toggleListBtn.classList.toggle('ibexa-sidebar-filter-selection__list-toggle-btn--hidden', this.isSearchExpanded);
+
+            if (!this.isSearchExpanded) {
+                this.searchInput.value = '';
+                this.toggleListItems(true);
+            }
+        }
+
+        toggleItemsCheckState() {
+            this.listItems.forEach((listItem) => {
+                const listItemCheckbox = listItem.querySelector('.ibexa-input--checkbox');
+
+                listItemCheckbox.checked = !this.itemsCheckedCount;
+            });
+
+            this.setToggleItemsCheckStateBtnLabel();
+        }
+
+        toggleListItems(forcedCollapse = false) {
+            const shouldCollapseList = forcedCollapse || (!forcedCollapse && this.isListExpanded);
+
+            this.listItems.forEach((listItem, index) => {
+                const isIndexOverLimit = index >= this.itemsShortListLimit;
+                const shouldHideItem = shouldCollapseList && isIndexOverLimit;
+
+                listItem.classList.toggle('ibexa-sidebar-filter-selection__list-item--hidden', shouldHideItem);
+            });
+
+            this.isListExpanded = !shouldCollapseList;
+            this.setToggleListBtnLabel()
+        }
+
+        setToggleItemsCheckStateBtnLabel() {
+            this.itemsCheckedCount = this.list
+                .querySelectorAll('.ibexa-sidebar-filter-selection__list-item .ibexa-input--checkbox:checked')
+                .length;
+
+            this.toggleItemsCheckStateBtn.innerHTML = this.itemsCheckedCount
+                ? `Clear (${this.itemsCheckedCount})`
+                : 'Select all';
+        }
+
+        setToggleListBtnLabel() {
+            const iconName = this.isListExpanded ? 'undo' : 'create';
+            const label = this.toggleListBtn.querySelector('.ibexa-btn__label');
+
+            label.innerHTML = this.isListExpanded
+                ? 'Less'
+                : 'More';
+        }
+
+        filterItems({ currentTarget }) {
+            const fieldFilterQueryLowerCase = currentTarget.value.toLowerCase();
+
+            this.listItems.forEach((listItem) => {
+                const itemNameNode = listItem.querySelector('.ibexa-sidebar-filter-selection__list-item-label');
+                const itemNameLowerCase = itemNameNode.innerText.toLowerCase();
+                const shouldHideItem = !itemNameLowerCase.includes(fieldFilterQueryLowerCase);
+
+                listItem.classList.toggle('ibexa-sidebar-filter-selection__list-item--hidden', shouldHideItem);
+            });
+        }
+
+        init() {
+            this.toggleItemsCheckStateBtn.addEventListener('click', this.toggleItemsCheckState, false);
+
+            this.searchInput.addEventListener('keyup', this.filterItems, false);
+            this.searchInput.addEventListener('input', this.filterItems, false);
+
+            this.listItems.forEach((listItem) => {
+                const listItemCheckbox = listItem.querySelector('.ibexa-input--checkbox');
+
+                listItemCheckbox.addEventListener('change', this.setToggleItemsCheckStateBtnLabel, false);
+            });
+
+            if (this.searchToggleBtn) {
+                this.searchToggleBtn.addEventListener('click', this.toggleSearchBar, false);
+            }
+
+            if (this.toggleListBtn) {
+                this.toggleListBtn.addEventListener('click', () => this.toggleListItems(), false);
+            }
+        }
+    }
+
+    doc.querySelectorAll('.ibexa-sidebar-filter-selection').forEach((selectionFilterWrapper) => {
+        const filter = new selectionFilter({
+            wrapper: selectionFilterWrapper
         });
-        selectionList.classList.toggle('ibexa-sidebar-filter__selection-list-item--short');
-    };
 
-    toggleListBtns.forEach((toggleListBtn) => {
-        toggleListBtn.addEventListener('click', toggleList, false)
-    });
+        filter.init();
+    })
+
+
 })(window, window.document);
