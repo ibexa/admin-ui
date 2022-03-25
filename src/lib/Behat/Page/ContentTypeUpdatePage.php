a7f7e819ed723136f800d98c825a9f7f4e331e8e
tomaszszopinski <tomasz.szopinski@ez.no>
2022-03-25 12:36:20 +0100

    sorting imports

--- src/lib/Behat/Page/ContentTypeUpdatePage.php
+++ src/lib/Behat/Page/ContentTypeUpdatePage.php
@@ -9,8 +9,8 @@ declare(strict_types=1);
 namespace Ibexa\AdminUi\Behat\Page;
 
 use Ibexa\Behat\Browser\Element\Condition\ElementExistsCondition;
-use Ibexa\Behat\Browser\Element\Criterion\ElementAttributeCriterion;
 use Ibexa\Behat\Browser\Element\Condition\ElementTransitionHasEndedCondition;
+use Ibexa\Behat\Browser\Element\Criterion\ElementAttributeCriterion;
 use Ibexa\Behat\Browser\Element\Criterion\ElementTextCriterion;
 use Ibexa\Behat\Browser\Element\Mapper\ElementTextMapper;
 use Ibexa\Behat\Browser\Locator\VisibleCSSLocator;
