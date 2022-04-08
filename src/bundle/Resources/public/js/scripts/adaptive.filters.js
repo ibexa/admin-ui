2d4406710fe62af9f9018eb7ba18bd1e8da51f4e
Jakub Brzegowski <symfiz@gmail.com>
2022-04-08 13:21:51 +0200

    IBX-2658: Introduce adaptive filters

---
+++ src/bundle/Resources/public/js/scripts/adaptive.filters.js
@@ -0,0 +1,38 @@
+(function (global, doc, ibexa) {
+    const adaptiveFilters = doc.querySelectorAll('.ibexa-adaptive-filters');
+    const initializeAdaptiveFilters = (adaptiveFilter) => {
+        const adaptiveItemsContainer = adaptiveFilter.querySelector('.ibexa-adaptive-filters__items');
+        const adaptiveItemsCollapsibleContainer = adaptiveFilter.querySelector('.ibexa-adaptive-filters__hidden');
+        const actionsContainer = adaptiveFilter.querySelector('.ibexa-adaptive-filters__actions');
+        const toggleBtn = adaptiveFilter.querySelector('.ibexa-adaptive-filters__toggler');
+        const collapse = global.bootstrap.Collapse.getOrCreateInstance(adaptiveItemsCollapsibleContainer, {
+            toggle: false,
+        });
+        const adaptiveItems = new ibexa.core.AdaptiveItems({
+            itemHiddenClass: 'ibexa-adaptive-filters__item--hidden',
+            container: adaptiveItemsContainer,
+            getActiveItem: () => null,
+            prepareItemsBeforeAdapt: () => {
+                [...adaptiveItemsCollapsibleContainer.children].forEach((child) =>
+                    adaptiveItemsContainer.insertBefore(child, actionsContainer),
+                );
+            },
+            onAdapted: (visibleItems, hiddenItems) => {
+                if (hiddenItems.size === 0) {
+                    collapse.hide();
+                }
+
+                hiddenItems.forEach((hiddenItem) => adaptiveItemsCollapsibleContainer.append(hiddenItem));
+            },
+        });
+        adaptiveItemsCollapsibleContainer.addEventListener('hide.bs.collapse', () => {
+            toggleBtn.classList.add('ibexa-adaptive-filters__toggler--collapsed');
+        });
+        adaptiveItemsCollapsibleContainer.addEventListener('show.bs.collapse', () => {
+            toggleBtn.classList.remove('ibexa-adaptive-filters__toggler--collapsed');
+        });
+        adaptiveItems.init();
+    };
+
+    adaptiveFilters.forEach((adaptiveFilter) => initializeAdaptiveFilters(adaptiveFilter));
+})(window, window.document, window.ibexa);
