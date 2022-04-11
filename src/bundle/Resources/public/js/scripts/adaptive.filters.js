559a023a7e78b07926816c763d8dc916562c5eb7
Jakub Brzegowski <symfiz@gmail.com>
2022-04-11 09:12:45 +0200

    small refactor

--- src/bundle/Resources/public/js/scripts/adaptive.filters.js
+++ src/bundle/Resources/public/js/scripts/adaptive.filters.js
@@ -2,7 +2,7 @@
     const adaptiveFilters = doc.querySelectorAll('.ibexa-adaptive-filters');
     const initializeAdaptiveFilters = (adaptiveFilter) => {
         const adaptiveItemsContainer = adaptiveFilter.querySelector('.ibexa-adaptive-filters__items');
-        const adaptiveItemsCollapsibleContainer = adaptiveFilter.querySelector('.ibexa-adaptive-filters__hidden');
+        const adaptiveItemsCollapsibleContainer = adaptiveFilter.querySelector('.ibexa-adaptive-filters__collapsible');
         const actionsContainer = adaptiveFilter.querySelector('.ibexa-adaptive-filters__actions');
         const toggleBtn = adaptiveFilter.querySelector('.ibexa-adaptive-filters__toggler');
         const collapse = global.bootstrap.Collapse.getOrCreateInstance(adaptiveItemsCollapsibleContainer, {
