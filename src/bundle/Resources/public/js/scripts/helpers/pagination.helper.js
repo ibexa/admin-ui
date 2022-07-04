c6f1237b41e117f31cf661eaf13e9633c7f6f075
Michał Grabowski <michal.grabowski@ibexa.co>
2022-07-04 10:53:21 +0200

    added comments

--- src/bundle/Resources/public/js/scripts/helpers/pagination.helper.js
+++ src/bundle/Resources/public/js/scripts/helpers/pagination.helper.js
@@ -1,3 +1,18 @@
+/**
+ * Computes array with pagination pages.
+ *
+ * Example 1: [ 1, "...", 5, 6, 7, 8, 9, 10 ] (for: proximity = 2; pagesNumber = 10; activePageIndex = 7)
+ * Example 2: [ 1, "...", 3, 4, 5, 6, 7, "...", 10 ] (for: proximity = 2; pagesNumber = 10; activePageIndex = 5)
+ * Example 3: [ 1, "...", 8, 9, 10, 11, 12, "...", 20 ] (for: proximity = 2; pagesNumber = 20; activePageIndex = 10)
+ *
+ * @param {Object} params
+ * @param {Number} params.proximity
+ * @param {Number} params.activePageIndex
+ * @param {Number} params.pagesCount
+ * @param {String} params.separator
+ *
+ * @returns {Array}
+ */
 export const computePages = ({ proximity, activePageIndex, pagesCount, separator }) => {
     const pages = [];
     let wasSeparator = false;
