3014e31d94a16451acf4aba5d8f945647e94679f
Marek Nocoń <mnocon@users.noreply.github.com>
2022-07-05 11:43:04 +0200

    [Behat] Added checking that the dropdown is visible

--- src/lib/Behat/Component/IbexaDropdown.php
+++ src/lib/Behat/Component/IbexaDropdown.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
 namespace Ibexa\AdminUi\Behat\Component;
 
 use Ibexa\Behat\Browser\Component\Component;
+use Ibexa\Behat\Browser\Element\Condition\ElementTransitionHasEndedCondition;
 use Ibexa\Behat\Browser\Element\Criterion\ElementTextCriterion;
 use Ibexa\Behat\Browser\Locator\VisibleCSSLocator;
 
@@ -16,7 +17,9 @@ class IbexaDropdown extends Component
 {
     public function verifyIsLoaded(): void
     {
-        //TODO: check if needed
+        $this->getHTMLPage()
+            ->setTimeout(2)
+            ->waitUntilCondition(new ElementTransitionHasEndedCondition($this->getHTMLPage(), $this->getLocator('isIbexaDropdownVisible')));
     }
 
     protected function specifyLocators(): array
