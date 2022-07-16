511953003a95f66fdb4d4dcc41dce43c2b32592c
tischsoic <symfiz@gmail.com>
2022-07-16 21:31:28 +0200

    IBX-3305: Add register page

--- src/bundle/Resources/public/js/scripts/admin.input.text.js
+++ src/bundle/Resources/public/js/scripts/admin.input.text.js
@@ -1,4 +1,19 @@
 (function (global, doc) {
+    const togglePasswordVisibility = (event) => {
+        const passwordTogglerBtn = event.currentTarget;
+        const passwordShowIcon = passwordTogglerBtn.querySelector('.ibexa-input-text-wrapper__password-show');
+        const passwordHideIcon = passwordTogglerBtn.querySelector('.ibexa-input-text-wrapper__password-hide');
+        const inputTextWrapper = passwordTogglerBtn.closest('.ibexa-input-text-wrapper');
+        const input = inputTextWrapper.querySelector('.ibexa-input--text');
+
+        if (input) {
+            const inputTypeToSet = input.type === 'password' ? 'text' : 'password';
+
+            input.type = inputTypeToSet;
+            passwordShowIcon.classList.toggle('d-none');
+            passwordHideIcon.classList.toggle('d-none');
+        }
+    };
     const clearText = ({ currentTarget }) => {
         const inputWrapper = currentTarget.closest('.ibexa-input-text-wrapper');
         const textInput = inputWrapper.querySelector('.ibexa-input--text');
@@ -16,8 +31,10 @@
     };
     const attachListenersToAllInputs = () => {
         const textInputClearBtns = doc.querySelectorAll('.ibexa-input-text-wrapper__action-btn--clear');
+        const passwordTogglersBtns = doc.querySelectorAll('.ibexa-input-text-wrapper__action-btn--password-toggler');
 
         textInputClearBtns.forEach((clearBtn) => clearBtn.addEventListener('click', clearText, false));
+        passwordTogglersBtns.forEach((passwordTogglerBtn) => passwordTogglerBtn.addEventListener('click', togglePasswordVisibility, false));
     };
 
     doc.body.addEventListener('ibexa-inputs:added', attachListenersToAllInputs, false);
