979c64cdfb5391000b014f76f09c24d975b9990c
Jakub Brzegowski <symfiz@gmail.com>
2022-07-28 10:29:01 +0200

    IBX-3305: Add register page (#493)
    * IBX-3305: Add register page
    
    * Fix readonly input styles
    
    * Change info-col styles
    
    * fix
    
    * yaml
    
    * Deprecate form_fields from account/
    
    * Fix registration
    
    * restore change_password/form_fields.html.twig

--- src/bundle/Resources/public/js/scripts/login.js
+++ src/bundle/Resources/public/js/scripts/login.js
@@ -1,8 +1,5 @@
 (function (global, doc) {
     const AUTOFILL_TIMEOUT = 500;
-    const passwordInputNode = doc.querySelector('.ibexa-login__input--password');
-    const viewIconNode = doc.querySelector('.ibexa-login__password-visibility-toggler .ibexa-icon--view');
-    const viewHideIconNode = doc.querySelector('.ibexa-login__password-visibility-toggler .ibexa-icon--view-hide');
     const loginBtn = doc.querySelector('.ibexa-login__btn--sign-in');
     const nameInput = doc.querySelector('.ibexa-login__input--name');
     const passwordInput = doc.querySelector('.ibexa-login__input--password');
@@ -20,16 +17,6 @@
             loginBtn.removeAttribute('disabled');
         }
     };
-
-    doc.querySelector('.ibexa-login__password-visibility-toggler').addEventListener('click', () => {
-        if (passwordInputNode) {
-            const inputTypeToSet = passwordInputNode.type === 'password' ? 'text' : 'password';
-
-            passwordInputNode.type = inputTypeToSet;
-            viewIconNode.classList.toggle('d-none');
-            viewHideIconNode.classList.toggle('d-none');
-        }
-    });
 
     if (loginBtn) {
         nameInput.addEventListener('keyup', toggleLoginBtnState, false);
