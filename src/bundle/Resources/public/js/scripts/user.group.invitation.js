58ac46dc1cdc6c1428543cf781109f8e05512f94
Jakub Brzegowski <symfiz@gmail.com>
2022-08-02 13:38:31 +0200

    IBX-2534: Improved invite users modal design (#501)
    * IBX-2534: Improved invite users modal design
    
    * Finishing implementation
    
    * missing semicolon
    
    * Move countFilledLinesInFile to base class
    
    * fix toggleDuplicateEntryState
    
    * Modal title improvement (entries count)
    
    * change translation variable name
    
    * add return value to addEntry in UserGroupInvitationModal
    
    * Add empty email validation
    
    * Validate on submit
    
    * after CR
    
    * Update title after fiel upload
    
    * missing semicolon
    
    * ibexa-inputs:added

--- src/bundle/Resources/public/js/scripts/user.group.invitation.js
+++ src/bundle/Resources/public/js/scripts/user.group.invitation.js
@@ -19,6 +19,8 @@ import { UserInvitationModal } from './user.invitation.modal';
         }
 
         resetEntry(entry) {
+            super.resetEntry(entry);
+
             const emailInput = entry.querySelector('.ibexa-user-group-invitation__entry-email');
 
             emailInput.value = null;
@@ -31,12 +33,28 @@ import { UserInvitationModal } from './user.invitation.modal';
         }
 
         addEntry(isFileRelated = false, invitationData = null) {
-            const { insertedEntry } = super.addEntry(isFileRelated, invitationData);
+            const addEntryData = super.addEntry(isFileRelated, invitationData);
+            const { insertedEntry } = addEntryData;
 
             const email = invitationData?.email ?? null;
             const emailInput = insertedEntry.querySelector('.ibexa-user-group-invitation__entry-email');
 
             emailInput.value = email;
+
+            return addEntryData;
+        }
+
+        checkEntryMatchesSearch(entry, searchText) {
+            const emailInput = entry.querySelector('.ibexa-user-group-invitation__entry-email');
+            const email = emailInput.value;
+
+            return email.includes(searchText);
+        }
+
+        checkIsEntryDuplicate(invitationData, entryToCompare) {
+            const entryToCompareEmailInput = entryToCompare.querySelector('.ibexa-user-group-invitation__entry-email');
+
+            return invitationData.email === entryToCompareEmailInput.value;
         }
     }
 
