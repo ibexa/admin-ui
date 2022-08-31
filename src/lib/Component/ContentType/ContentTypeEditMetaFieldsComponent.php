112216214418ae09b55835d1981a5de7a778a474
Tomasz Kryszan <tomasz.kryszan@ez.no>
2022-08-31 12:53:11 +0200

    Added ContentTypeEditMetaFieldsComponent

---
+++ src/lib/Component/ContentType/ContentTypeEditMetaFieldsComponent.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\AdminUi\Component\ContentType;
+
+use Ibexa\Contracts\AdminUi\Component\Renderable;
+use Ibexa\Contracts\Core\SiteAccess\ConfigResolverInterface;
+use Twig\Environment;
+
+final class ContentTypeEditMetaFieldsComponent implements Renderable
+{
+    private const NO_CONTENT = '';
+
+    private Environment $twig;
+
+    private ConfigResolverInterface $configResolver;
+
+    public function __construct(
+        Environment $twig,
+        ConfigResolverInterface $configResolver
+    ) {
+        $this->twig = $twig;
+        $this->configResolver = $configResolver;
+    }
+
+    /**
+     * @param array<string, mixed> $parameters
+     *
+     * @return string
+     */
+    public function render(array $parameters = []): string
+    {
+        /** @var \Ibexa\Contracts\Core\Repository\Values\ContentType\ContentType $contentType */
+        $contentType = $parameters['content_type'];
+
+        $metaFieldTypeIdentifiers = $this->getMetaFieldTypeIdentifiers();
+
+        if (empty($metaFieldTypeIdentifiers)) {
+            return self::NO_CONTENT;
+        }
+
+        $parameters['meta_fields'] = [];
+        foreach ($metaFieldTypeIdentifiers as $identifier) {
+            $fields = $contentType->getFieldDefinitionsOfType($identifier);
+            $parameters['meta_fields'] = array_merge($parameters['meta_fields'], array_column($fields->toArray(), 'identifier'));
+        }
+
+        return $this->twig->render(
+            '@ibexadesign/content_type/components/meta_fields.html.twig',
+            $parameters
+        );
+    }
+
+    /**
+     * @return array<string>
+     */
+    private function getMetaFieldTypeIdentifiers(): array
+    {
+        $fieldTypeConfig = $this->configResolver->getParameter('admin_ui_forms.content_type_edit.fieldtypes');
+
+        return array_keys(
+            array_filter(
+                $fieldTypeConfig,
+                static fn (array $config): bool => true === $config['meta']
+            )
+        );
+    }
+}
