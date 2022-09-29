a37ea59cc43755d6e2e9cdd0812c05383dc0ac0c
Marek Nocoń <mnocon@users.noreply.github.com>
2022-09-29 10:57:30 +0200

    fixup! [Behat] Adapted drag and drop tests

--- src/lib/Behat/Page/ContentTypeUpdatePage.php
+++ src/lib/Behat/Page/ContentTypeUpdatePage.php
@@ -45,22 +45,29 @@ class ContentTypeUpdatePage extends AdminUpdateItemPage
             new VisibleCSSLocator('field', '.form-group'),
             new VisibleCSSLocator('contentTypeAddButton', '.ibexa-content-type-edit__add-field-definitions-group-btn'),
             new VisibleCSSLocator('contentTypeCategoryList', ' div.ibexa-content-type-edit__add-field-definitions-group > ul > li:nth-child(n):not(.ibexa-popup-menu__item-action--disabled)'),
-            new VisibleCSSLocator('availableFieldLabelList', '.ibexa-available-field-types__list > li'),
-            new VisibleCSSLocator('workspace', '#content_collapse > div.ibexa-collapse__body-content > div'),
+            new VisibleCSSLocator('availableFieldLabelList', '.ibexa-available-field-types__list > li:not(.ibexa-available-field-type--hidden)'),
+            new VisibleCSSLocator('workspace', '.ibexa-field-definitions-placeholder-full'),
             new VisibleCSSLocator('fieldDefinitionToggle', '.ibexa-collapse:nth-last-child(2) > div.ibexa-collapse__header > button:last-child:not([data-bs-target="#content_collapse"])'),
             new VisibleCSSLocator('selectLaunchEditorMode', '.form-check .ibexa-input--radio'),
             new VisibleCSSLocator('fieldDefinitionOpenContainer', '[data-collapsed="false"] .ibexa-content-type-edit__field-definition-content'),
             new VisibleCSSLocator('fieldDefinitionOpenContainerEdit', '#content_collapse > div > div[data-collapsed="false"]'),
             new VisibleCSSLocator('selectBlocksDropdown', '.ibexa-page-select-items__toggler'),
+            new VisibleCSSLocator('fieldDefinitionSearch', '.ibexa-available-field-types__sidebar-filter'),
         ]);
     }
 
     public function addFieldDefinition(string $fieldName)
     {
         $currentFieldDefinitionCount = $this->getHTMLPage()->findAll($this->getLocator('fieldDefinition'))->count();
-
+        $this->getHTMLPage()->find($this->getLocator('fieldDefinitionSearch'))->setValue($fieldName);
         $fieldPosition = array_search($fieldName, $this->getHTMLPage()->findAll($this->getLocator('availableFieldLabelList'))->mapBy(new ElementTextMapper()), true) + 1;
-        $fieldSelector = new VisibleCSSLocator('field', sprintf('.ibexa-available-field-types__list li:nth-child(%d) .ibexa-available-field-type__content', $fieldPosition));
+        $fieldSelector = new VisibleCSSLocator(
+            'field',
+            sprintf(
+                '.ibexa-available-field-types__list > li:not(.ibexa-available-field-type--hidden) .ibexa-available-field-type__content:nth-of-type(%d)',
+                $fieldPosition
+            )
+        );
         $this->getHTMLPage()->find($fieldSelector)->mouseOver();
         $this->getHTMLPage()->setTimeout(3)->waitUntilCondition(new ElementTransitionHasEndedCondition($this->getHTMLPage(), $fieldSelector));
 
