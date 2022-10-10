e7a465d312fa295338e556cc88034c0445868c0b
Tomasz Kryszan <tomasz.kryszan@ez.no>
2022-10-10 12:56:44 +0200

    IBX-3962: Introduced DisableFieldDefinitionFieldsSubscriber
    https://issues.ibexa.co/browse/IBX-3962

---
+++ src/lib/Form/Type/Extension/EventSubscriber/DisableFieldDefinitionFieldsSubscriber.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\AdminUi\Form\Type\Extension\EventSubscriber;
+
+use Ibexa\AdminUi\Form\Type\FieldDefinition\FieldDefinitionType;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+
+/**
+ * Modifies CT editing form by rebuilding field definition list with custom options on given field type.
+ */
+final class DisableFieldDefinitionFieldsSubscriber implements EventSubscriberInterface
+{
+    private string $fieldTypeIdentifier;
+
+    /** @var array<string> */
+    private array $modifiedOptions;
+
+    /**
+     * @param array<string> $modifiedOptions
+     */
+    public function __construct(string $fieldTypeIdentifier, array $modifiedOptions)
+    {
+        $this->fieldTypeIdentifier = $fieldTypeIdentifier;
+        $this->modifiedOptions = $modifiedOptions;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            FormEvents::PRE_SET_DATA => ['onPreSetData'],
+        ];
+    }
+
+    public function onPreSetData(FormEvent $event): void
+    {
+        /** @var array<string, \Ibexa\AdminUi\Form\Data\FieldDefinitionData>|null $data */
+        $data = $event->getData();
+        $form = $event->getForm();
+
+        if (null === $data) {
+            return;
+        }
+
+        foreach ($data as $fieldTypeIdentifier => $fieldTypeData) {
+            if ($this->fieldTypeIdentifier !== $fieldTypeData->fieldDefinition->fieldTypeIdentifier) {
+                continue;
+            }
+
+            if (!$form->has($fieldTypeIdentifier)) {
+                return;
+            }
+
+            $baseFieldForm = $form->get($fieldTypeIdentifier);
+            $baseFieldFormName = $baseFieldForm->getName();
+
+            $form->remove($baseFieldFormName);
+
+            $options = array_merge(
+                $baseFieldForm->getConfig()->getOptions(),
+                array_fill_keys(
+                    array_values($this->modifiedOptions),
+                    true
+                )
+            );
+
+            $form->add($baseFieldFormName, FieldDefinitionType::class, $options);
+        }
+    }
+}
---
+++ tests/lib/Form/Type/Extension/EventSubscriber/DisableFieldDefinitionFieldsSubscriberTest.php
@@ -0,0 +1,170 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Tests\AdminUi\Form\Type\Extension\EventSubscriber;
+
+use Ibexa\AdminUi\Form\Data\FieldDefinitionData;
+use Ibexa\AdminUi\Form\Type\Extension\EventSubscriber\DisableFieldDefinitionFieldsSubscriber;
+use Ibexa\AdminUi\Form\Type\FieldDefinition\FieldDefinitionType;
+use Ibexa\Core\Repository\Values\ContentType\FieldDefinition;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormInterface;
+
+/**
+ * @covers \Ibexa\AdminUi\Form\Type\Extension\EventSubscriber\DisableFieldDefinitionFieldsSubscriber
+ */
+final class DisableFieldDefinitionFieldsSubscriberTest extends TestCase
+{
+    private const FIELD_TYPE_IDENTIFIER = 'foo';
+    private const MODIFIED_OPTIONS = [
+        'disable_identifier_field',
+        'disable_required_field',
+        'disable_translatable_field',
+        'disable_remove',
+    ];
+
+    private DisableFieldDefinitionFieldsSubscriber $disableFieldDefinitionFieldsSubscriber;
+
+    /** @var \Symfony\Component\Form\FormInterface|\PHPUnit\Framework\MockObject\MockObject */
+    private FormInterface $form;
+
+    private FormBuilderInterface $formBuilder;
+
+    protected function setUp(): void
+    {
+        $this->form = $this->createMock(FormInterface::class);
+        $this->formBuilder = $this->createMock(FormBuilderInterface::class);
+        $this->disableFieldDefinitionFieldsSubscriber = new DisableFieldDefinitionFieldsSubscriber(
+            self::FIELD_TYPE_IDENTIFIER,
+            self::MODIFIED_OPTIONS
+        );
+    }
+
+    public function testDisableFields(): void
+    {
+        $fieldIdentifier = 'field_123456789';
+        $data = $this->getFormData($fieldIdentifier);
+        $event = new FormEvent($this->form, $data);
+        $options = [
+            'required' => true,
+            'label' => 'foo',
+            'csrf_protection' => 'foo',
+            'label_format' => null,
+        ];
+
+        $this->mockFormGet($fieldIdentifier);
+        $this->mockFormGetName($fieldIdentifier);
+        $this->mockFormGetConfig();
+        $this->mockFormBuilderGetOptions($options);
+        $this->mockFormRemove($fieldIdentifier);
+
+        $options['disable_required_field'] = true;
+
+        $this->mockFormAdd(
+            $fieldIdentifier,
+            array_merge(
+                $options,
+                array_fill_keys(
+                    array_values(self::MODIFIED_OPTIONS),
+                    true
+                )
+            )
+        );
+
+        $this->disableFieldDefinitionFieldsSubscriber->onPreSetData($event);
+    }
+
+    private function getFormData(string $identifier): array
+    {
+        return [
+            $identifier => new FieldDefinitionData(
+                [
+                    'identifier' => $identifier,
+                    'isRequired' => true,
+                    'isThumbnail' => false,
+                    'isInfoCollector' => false,
+                    'fieldDefinition' => new FieldDefinition(
+                        [
+                            'identifier' => $identifier,
+                            'fieldTypeIdentifier' => self::FIELD_TYPE_IDENTIFIER,
+                        ]
+                    ),
+                ]
+            ),
+        ];
+    }
+
+    private function mockFormGet(string $identifier): void
+    {
+        $this->form
+            ->expects(self::once())
+            ->method('has')
+            ->with($identifier)
+            ->willReturn(true);
+
+        $this->form
+            ->expects(self::once())
+            ->method('get')
+            ->with($identifier)
+            ->willReturn($this->form);
+    }
+
+    private function mockFormGetName(string $identifier): void
+    {
+        $this->form
+            ->expects(self::once())
+            ->method('getName')
+            ->willReturn($identifier);
+    }
+
+    private function mockFormGetConfig(): void
+    {
+        $this->form
+            ->expects(self::once())
+            ->method('getConfig')
+            ->willReturn($this->formBuilder);
+    }
+
+    /**
+     * @param array<string, scalar|bool> $options
+     */
+    private function mockFormBuilderGetOptions(array $options): void
+    {
+        $this->formBuilder
+            ->expects(self::once())
+            ->method('getOptions')
+            ->willReturn($options);
+    }
+
+    private function mockFormRemove(string $identifier): void
+    {
+        $this->form
+            ->expects(self::once())
+            ->method('remove')
+            ->with($identifier)
+            ->willReturn($this->form);
+    }
+
+    /**
+     * @param array<string, scalar|bool> $options
+     */
+    private function mockFormAdd(string $identifier, array $options): void
+    {
+        $this->form
+            ->expects(self::once())
+            ->method('add')
+            ->with(
+                $identifier,
+                FieldDefinitionType::class,
+                $options,
+            )
+            ->willReturn($this->form);
+    }
+}
