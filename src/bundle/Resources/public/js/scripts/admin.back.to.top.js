a6ce48b255fcc94b55ffd3a44efa60eea8abb822
Michał Grabowski <michal.grabowski@ibexa.co>
2022-10-11 13:59:42 +0200

    IBX-3987: Go to top button overlaps with pagination in subitems list when using grid view

--- src/bundle/Resources/public/js/scripts/admin.back.to.top.js
+++ src/bundle/Resources/public/js/scripts/admin.back.to.top.js
@@ -8,18 +8,35 @@
     }
 
     const backToTopBtnTitle = backToTopBtn.querySelector('.ibexa-back-to-top__title');
-
-    backToTopScrollContainer.addEventListener('scroll', (event) => {
-        const container = event.target;
+    let currentBackToTopAnchorHeight = backToTopAnchor.offsetHeight;
+    const toggleBackToTopBtnText = (container) => {
         const isTitleVisible = Math.abs(container.scrollHeight - container.scrollTop - container.clientHeight) <= 2;
 
         backToTopBtn.classList.toggle('ibexa-back-to-top__btn--visible', container.scrollTop !== 0);
         backToTopBtn.classList.toggle('ibexa-btn--no-text', !isTitleVisible);
         backToTopBtnTitle.classList.toggle('ibexa-back-to-top__title--visible', isTitleVisible);
+    };
+
+    backToTopScrollContainer.addEventListener('scroll', (event) => {
+        const container = event.target;
+
+        toggleBackToTopBtnText(container);
     });
     backToTopBtn.addEventListener('click', () => {
         backToTopAnchor.scrollIntoView({
             behavior: 'smooth',
         });
     });
+
+    const resizeObserver = new ResizeObserver((entries) => {
+        if (currentBackToTopAnchorHeight === entries[0].target.clientHeight) {
+            return;
+        }
+
+        currentBackToTopAnchorHeight = entries[0].target.clientHeight;
+
+        toggleBackToTopBtnText(backToTopScrollContainer);
+    });
+
+    resizeObserver.observe(backToTopAnchor);
 })(window, window.document);
