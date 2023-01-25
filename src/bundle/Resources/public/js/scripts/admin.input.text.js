caa5d2410e100ab9138d098fac7bd62a49ac80e4
Robert Swoboda <53216935+Gengar-i@users.noreply.github.com>
2023-01-25 09:36:58 +0100

    IBX-4767: Input_text updated extrabutton (#682)

--- src/bundle/Resources/public/js/scripts/admin.input.text.js
+++ src/bundle/Resources/public/js/scripts/admin.input.text.js
@@ -38,9 +38,42 @@
 
         inputClearBtns.forEach((clearBtn) => clearBtn.addEventListener('click', clearText, false));
         passwordTogglerBtns.forEach((passwordTogglerBtn) => passwordTogglerBtn.addEventListener('click', togglePasswordVisibility, false));
+        recalculateStyling();
+    };
+    const handleInputChange = ({ target: { value } }, btn) => {
+        btn.disabled = value === '';
+    };
+    const recalculateStyling = () => {
+        const extraBtns = doc.querySelectorAll('.ibexa-input-text-wrapper__action-btn--extra-btn');
+
+        extraBtns.forEach((btn) => {
+            const input = btn.closest('.ibexa-input-text-wrapper').querySelector('input');
+            const clearButton = btn.previousElementSibling?.classList.contains('ibexa-input-text-wrapper__action-btn--clear')
+                ? btn.previousElementSibling
+                : null;
+
+            if (!input) {
+                return;
+            }
+
+            btn.disabled = !input.value;
+            input.addEventListener('input', (inputEvent) => handleInputChange(inputEvent, btn), false);
+
+            if (!clearButton) {
+                return;
+            }
+
+            const clearButtonStyles = global.getComputedStyle(clearButton);
+            const clearButtonMarginRight = parseInt(clearButtonStyles.getPropertyValue('margin-right'), 10);
+            const clearButtonWidth = parseInt(clearButtonStyles.getPropertyValue('width'), 10);
+            const paddingRight = `${btn.offsetWidth + clearButtonMarginRight + clearButtonWidth}px`;
+
+            input.style.paddingRight = paddingRight;
+        });
     };
 
     doc.body.addEventListener('ibexa-inputs:added', attachListenersToAllInputs, false);
+    doc.body.addEventListener('ibexa-inputs:recalculate-styling', recalculateStyling, false);
 
     attachListenersToAllInputs();
 })(window, window.document);
