67aa5a69fd6ee57843be3b9edaf8372d13dcd760
Michał Grabowski <michal.grabowski@ibexa.co>
2023-04-05 16:20:33 +0200

    IBX-5284: Date widget (using date input)

--- src/bundle/Resources/public/js/scripts/admin.picker.js
+++ src/bundle/Resources/public/js/scripts/admin.picker.js
@@ -4,13 +4,24 @@
     const SELECTOR_FORM_INPUT = '.ibexa-picker__form-input';
     const pickers = doc.querySelectorAll(SELECTOR_PICKER);
     const { formatShortDateTime } = ibexa.helpers.timezone;
+    const { shortDate: shortDateFormat } = ibexa.adminUiConfig.dateFormat;
     const pickerConfig = {
         enableTime: true,
         time_24hr: true,
         formatDate: (date) => formatShortDateTime(date, null),
     };
-    const updateInputValue = (formInput, timestamp) => {
-        formInput.value = timestamp ?? '';
+    const updateInputValue = (formInput, timestamp, { dates }) => {
+        let inputValue = timestamp;
+
+        if (formInput.type === 'date') {
+            inputValue = formatShortDateTime(dates[0], null, 'YYYY-MM-DD');
+        }
+
+        if (inputValue !== formInput.value) {
+            formInput.value = inputValue ?? '';
+
+            formInput.dispatchEvent(new Event('input'));
+        }
     };
     const initFlatPickr = (field) => {
         const formInput = field.querySelector(SELECTOR_FORM_INPUT);
@@ -19,7 +30,15 @@
         let defaultDate;
 
         if (formInput.value) {
-            defaultDate = new Date(formInput.value * 1000);
+            if (formInput.type === 'date') {
+                defaultDate = new Date(formInput.value);
+            } else {
+                defaultDate = new Date(formInput.value * 1000);
+            }
+        }
+
+        if (!customConfig.enableTime) {
+            customConfig.formatDate = (date) => formatShortDateTime(date, null, shortDateFormat);
         }
 
         const dateTimePickerWidget = new ibexa.core.DateTimePicker({
