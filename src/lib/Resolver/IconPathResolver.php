a77b10c57800bf91173eaa3117afcf1e4939251b
Paweł Niedzielski <pawel.niedzielski@ibexa.co>
2023-05-30 11:33:50 +0200

    IBX-5672: Performance Improvement of IconPathResolver::resolve()
    Co-authored-by: Vidar Langseid <vl@ibexa.co>

--- src/lib/Resolver/IconPathResolver.php
+++ src/lib/Resolver/IconPathResolver.php
@@ -10,18 +10,22 @@ namespace Ibexa\AdminUi\Resolver;
 
 use Ibexa\Contracts\AdminUi\Resolver\IconPathResolverInterface;
 use Ibexa\Contracts\Core\SiteAccess\ConfigResolverInterface;
+use Ibexa\Core\MVC\Symfony\Event\ScopeChangeEvent;
+use Ibexa\Core\MVC\Symfony\MVCEvents;
 use Symfony\Component\Asset\Packages;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
  * @internal
  */
-final class IconPathResolver implements IconPathResolverInterface
+final class IconPathResolver implements IconPathResolverInterface, EventSubscriberInterface
 {
-    /** @var \Ibexa\Contracts\Core\SiteAccess\ConfigResolverInterface */
-    private $configResolver;
+    private ConfigResolverInterface $configResolver;
 
-    /** @var \Symfony\Component\Asset\Packages */
-    private $packages;
+    private Packages $packages;
+
+    /** @var string[] */
+    private array $iconCache;
 
     public function __construct(
         ConfigResolverInterface $configResolver,
@@ -29,14 +33,34 @@ final class IconPathResolver implements IconPathResolverInterface
     ) {
         $this->configResolver = $configResolver;
         $this->packages = $packages;
+        $this->iconCache = [];
     }
 
     public function resolve(string $icon, ?string $set = null): string
     {
+        if (isset($this->iconCache[$set][$icon])) {
+            return $this->iconCache[$set][$icon];
+        }
+
         $iconSetName = $set ?? $this->configResolver->getParameter('assets.default_icon_set');
         $iconSets = $this->configResolver->getParameter('assets.icon_sets');
 
-        return sprintf('%s#%s', $this->packages->getUrl($iconSets[$iconSetName]), $icon);
+        $this->iconCache[$set][$icon] = sprintf('%s#%s', $this->packages->getUrl($iconSets[$iconSetName]), $icon);
+
+        return $this->iconCache[$set][$icon];
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            MVCEvents::CONFIG_SCOPE_CHANGE => ['onConfigScopeChange', 100],
+            MVCEvents::CONFIG_SCOPE_RESTORE => ['onConfigScopeChange', 100],
+        ];
+    }
+
+    public function onConfigScopeChange(ScopeChangeEvent $event): void
+    {
+        $this->iconCache = [];
     }
 }
 
