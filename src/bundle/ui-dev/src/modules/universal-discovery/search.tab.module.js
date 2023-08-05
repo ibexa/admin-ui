6242753d2058f93ab14f0fee8b9312e601ccff7c
Bartek Wajda <bartlomiej.wajda@ibexa.co>
2023-08-05 12:21:12 +0200

    IBX-6261: Fixed UDW Search module to set locations when unmounting

--- src/bundle/ui-dev/src/modules/universal-discovery/search.tab.module.js
+++ src/bundle/ui-dev/src/modules/universal-discovery/search.tab.module.js
@@ -1,19 +1,36 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 
 import Tab from './components/tab/tab';
 import Search from './components/search/search';
 
-import { TabsConfigContext } from './universal.discovery.module';
+import {
+    LoadedLocationsMapContext,
+    MarkedLocationIdContext,
+    TabsConfigContext
+} from './universal.discovery.module';
 
 const { ibexa, Translator } = window;
 
 const SearchTabModule = () => {
     const tabsConfig = useContext(TabsConfigContext);
+    const shouldRestorePreviousStateRef = useRef(true);
+    const [markedLocationId, setMarkedLocationId] = useContext(MarkedLocationIdContext);
+    const [loadedLocationsMap, dispatchLoadedLocationsAction] = useContext(LoadedLocationsMapContext);
+
     const actionsDisabledMap = {
         'content-create-button': false,
         'sort-switcher': true,
         'view-switcher': true,
     };
+
+    useEffect(() => {
+        return () => {
+            if (shouldRestorePreviousStateRef.current) {
+                setMarkedLocationId(markedLocationId);
+                dispatchLoadedLocationsAction({ type: 'SET_LOCATIONS', data: loadedLocationsMap });
+            }
+        };
+    }, []);
 
     return (
         <div className="m-search-tab">
