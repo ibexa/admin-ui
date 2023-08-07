f5ff94a346f7b5b16509b5e823d38e767a321410
tischsoic <symfiz@gmail.com>
2023-08-07 16:03:06 +0200

    FT: Edit Content -> ...

--- src/lib/Behat/Page/UserSettingsPage.php
+++ src/lib/Behat/Page/UserSettingsPage.php
@@ -65,7 +65,7 @@ class UserSettingsPage extends Page
     {
         $this->getHTMLPage()
             ->findAll(new VisibleCSSLocator('settingsSection', '#ibexa-tab-my-preferences .ibexa-details'))
-            ->getByCriterion(new ChildElementTextCriterion(new VisibleCSSLocator('settingHeader', '.ibexa-table-header'), 'Edit Content'))
+            ->getByCriterion(new ChildElementTextCriterion(new VisibleCSSLocator('settingHeader', '.ibexa-table-header'), 'Edit content'))
             ->find(new VisibleCSSLocator('editButton', '[data-original-title="Edit"]'))
             ->click();
     }
