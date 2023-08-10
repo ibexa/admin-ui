5af0c1fd172cc35d693e6ac2e90af8f52a274303
mikolaj <mikolaj.adamczyk@ez.no>
2023-08-10 09:27:54 +0200

    Fixed extraction of translation

--- src/lib/Menu/Admin/ContentType/ContentTypeEditRightSidebarBuilder.php
+++ src/lib/Menu/Admin/ContentType/ContentTypeEditRightSidebarBuilder.php
@@ -8,13 +8,14 @@ namespace Ibexa\AdminUi\Menu\Admin\ContentType;
 
 use Ibexa\AdminUi\Menu\Event\ConfigureMenuEvent;
 use JMS\TranslationBundle\Model\Message;
+use JMS\TranslationBundle\Translation\TranslationContainerInterface;
 
 /**
  * KnpMenuBundle Menu Builder service implementation for AdminUI Section Edit contextual sidebar menu.
  *
  * @see https://symfony.com/doc/current/bundles/KnpMenuBundle/menu_builder_service.html
  */
-class ContentTypeEditRightSidebarBuilder extends AbstractContentTypeRightSidebarBuilder
+class ContentTypeEditRightSidebarBuilder extends AbstractContentTypeRightSidebarBuilder implements TranslationContainerInterface
 {
     /* Menu items */
     public const ITEM__SAVE = 'content_type_edit__sidebar_right__save';
