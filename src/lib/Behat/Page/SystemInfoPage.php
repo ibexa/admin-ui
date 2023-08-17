1260b66b7a9638d5fcd8b381951971f3ff27314b
Michał Szołtysek <michal.szoltysek@ez.no>
2023-08-17 14:12:57 +0200

    IBX-6230: Fix incorrect CSS selectors in Behat code

--- src/lib/Behat/Page/SystemInfoPage.php
+++ src/lib/Behat/Page/SystemInfoPage.php
@@ -82,7 +82,7 @@ class SystemInfoPage extends Page
     protected function specifyLocators(): array
     {
         return [
-            new VisibleCSSLocator('packagesTable', '.tab-pane.active .ibexa-fieldgroup:nth-of-type(2)'),
+            new VisibleCSSLocator('packagesTable', '.tab-pane.active div.ibexa-fieldgroup:nth-of-type(2)'),
         ];
     }
 
