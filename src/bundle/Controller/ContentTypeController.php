c783449dad5c16fa477c6781b53b41c5cedc173e
Mateusz Dębiński <58430570+mateuszdebinski@users.noreply.github.com>
2023-08-21 13:19:21 +0200

    IBX-6214: Set default action to publishContentType (#870)
    * IBX-6327: default action set to publishContentType
    
    * moved publishContentType to const
    
    ---------
    
    Co-authored-by: Mateusz Dębiński <mateusz.debinski@ibexa.co>

--- src/bundle/Controller/ContentTypeController.php
+++ src/bundle/Controller/ContentTypeController.php
@@ -48,6 +48,8 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 
 class ContentTypeController extends Controller
 {
+    private const PRIMARY_UPDATE_ACTION = 'publishContentType';
+
     /** @var \Ibexa\Contracts\AdminUi\Notification\TranslatableNotificationHandlerInterface */
     private $notificationHandler;
 
@@ -501,10 +503,11 @@ class ContentTypeController extends Controller
                 $language,
                 $baseLanguage
             ) {
+                $action = $form->getClickedButton() ? $form->getClickedButton()->getName() : self::PRIMARY_UPDATE_ACTION;
                 $this->contentTypeActionDispatcher->dispatchFormAction(
                     $form,
                     $form->getData(),
-                    $form->getClickedButton() ? $form->getClickedButton()->getName() : null,
+                    $action,
                     ['languageCode' => $language->languageCode]
                 );
 
@@ -519,7 +522,7 @@ class ContentTypeController extends Controller
                     'content_type'
                 );
 
-                if ('publishContentType' === $form->getClickedButton()->getName()) {
+                if ($action === self::PRIMARY_UPDATE_ACTION) {
                     return $this->redirectToRoute('ibexa.content_type.view', [
                         'contentTypeGroupId' => $group->id,
                         'contentTypeId' => $contentTypeDraft->id,
