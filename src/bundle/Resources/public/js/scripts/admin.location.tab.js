95727e0e1c024cde5d2f95384c2c8f9b58c37496
Michał Grabowski <michal.grabowski@ibexa.co>
2023-08-23 16:18:56 +0200

    temp

--- src/bundle/Resources/public/js/scripts/admin.location.tab.js
+++ src/bundle/Resources/public/js/scripts/admin.location.tab.js
@@ -1,43 +1,48 @@
 (function (global, doc, bootstrap) {
-    const SELECTOR_TABS = '.ibexa-tabs';
-    const SELECTOR_TAB = '.ibexa-tabs__tab';
-    const SELECTOR_TAB_ACTIVE = '.ibexa-tabs__tab--active';
-    const CLASS_TAB_ACTIVE = 'ibexa-tabs__tab--active';
-    const switchActiveTabs = (currentTab, previousTab) => {
-        if (previousTab) {
-            previousTab.classList.remove(CLASS_TAB_ACTIVE);
-        }
+    function dupa(baseSelector) {
+        const SELECTOR_TABS = `.${baseSelector}`;
+        const SELECTOR_TAB = `.${baseSelector}__tab`;
+        const SELECTOR_TAB_ACTIVE = `.${baseSelector}__tab--active`;
+        const CLASS_TAB_ACTIVE = `${baseSelector}__tab--active`;
+        const switchActiveTabs = (currentTab, previousTab) => {
+            if (previousTab) {
+                previousTab.classList.remove(CLASS_TAB_ACTIVE);
+            }
+    
+            currentTab.classList.add(CLASS_TAB_ACTIVE);
+        };
+        const changeHashForPageReload = (hash) => {
+            global.location.hash = `${hash}#tab`;
+        };
+        const handleTabShown = (event) => {
+            const { target, relatedTarget } = event;
+            const currentTab = target.closest(SELECTOR_TAB);
+            const previousTab = relatedTarget.closest(SELECTOR_TAB);
+    
+            changeHashForPageReload(event.target.hash);
+            switchActiveTabs(currentTab, previousTab);
+        };
+        const setActiveHashTab = () => {
+            const activeHashTabLink = doc.querySelector(`.ibexa-tabs a[href="#${global.location.hash.split('#')[1]}"]`);
+    
+            if (!activeHashTabLink) {
+                return;
+            }
+    
+            const activeHashTab = activeHashTabLink.closest(SELECTOR_TAB);
+            const parentTabs = activeHashTab.closest(SELECTOR_TABS);
+            const currentActiveTab = parentTabs.querySelector(SELECTOR_TAB_ACTIVE);
+    
+            bootstrap.Tab.getOrCreateInstance(activeHashTabLink).show();
+    
+            switchActiveTabs(activeHashTab, currentActiveTab);
+        };
+    
+        setActiveHashTab();
+    
+        doc.querySelectorAll(`.${baseSelector} a`).forEach((tab) => tab.addEventListener('shown.bs.tab', handleTabShown));
+    }
 
-        currentTab.classList.add(CLASS_TAB_ACTIVE);
-    };
-    const changeHashForPageReload = (hash) => {
-        global.location.hash = `${hash}#tab`;
-    };
-    const handleTabShown = (event) => {
-        const { target, relatedTarget } = event;
-        const currentTab = target.closest(SELECTOR_TAB);
-        const previousTab = relatedTarget.closest(SELECTOR_TAB);
-
-        changeHashForPageReload(event.target.hash);
-        switchActiveTabs(currentTab, previousTab);
-    };
-    const setActiveHashTab = () => {
-        const activeHashTabLink = doc.querySelector(`.ibexa-tabs a[href="#${global.location.hash.split('#')[1]}"]`);
-
-        if (!activeHashTabLink) {
-            return;
-        }
-
-        const activeHashTab = activeHashTabLink.closest(SELECTOR_TAB);
-        const parentTabs = activeHashTab.closest(SELECTOR_TABS);
-        const currentActiveTab = parentTabs.querySelector(SELECTOR_TAB_ACTIVE);
-
-        bootstrap.Tab.getOrCreateInstance(activeHashTabLink).show();
-
-        switchActiveTabs(activeHashTab, currentActiveTab);
-    };
-
-    setActiveHashTab();
-
-    doc.querySelectorAll('.ibexa-tabs a').forEach((tab) => tab.addEventListener('shown.bs.tab', handleTabShown));
+    dupa('ibexa-tabs');
+    dupa('ibexa-tab-switcher');
 })(window, window.document, window.bootstrap);
