aa8d42c32aa61fcfcaf7fa19493f30bd7e662c2f
Jakub Wesolowski <121794185+jwibexa@users.noreply.github.com>
2023-08-25 11:01:27 +0200

    IBX-6330: [Behat] Cover "My Drafts" Page (#878)
    * Initial implementation
    
    * FInished the task of testing draft deletion
    
    * Added the test for the edition of draft, splited the tests into 2 versions.
    
    * cleanup
    
    * Improvemtn of the edit table selector
    
    * Fixes regarding editing draft selectors
    
    * code style check fix
    
    * Fixes regarding veryfiing deletion, style fixes, veryfiisloaded added
    
    * Added fix regarding not finding delete confirmation, naming fix
    
    ---------
    
    Co-authored-by: Marek Nocoń <mnocon@users.noreply.github.com>
    (cherry picked from commit b0e1549b0994d3d0225a61757307a9fe8c9e8679)

---
+++ features/standard/MyDrafts.feature
@@ -0,0 +1,26 @@
+@IbexaOSS @IbexaHeadless @IbexaExperience @IbexaCommerce @javascript
+Feature: My Drafts
+
+  Scenario: It is possible to delete a draft
+   Given I create "article" Content drafts
+      | title       | short_title   | parentPath | language |
+      | TestMyDraft | TestMyDraft   | root       | eng-GB   |
+    And I am logged as admin
+    And I open "MyDrafts" page in admin SiteAccess
+    When I delete the draft "TestMyDraft" from my draft lists
+    Then I see the draft "TestMyDraft" is deleted
+
+  Scenario: It is possible to edit a draft
+    Given I create "article" Content drafts
+      | title     | short_title | parentPath | language |
+      | TestMyDraft | TestMyDraft   | root       | eng-GB   |
+    And I am logged as admin
+    And I open "MyDrafts" page in admin SiteAccess
+    When I edit "TestMyDraft" on MyDrafts page
+    And I set content fields
+      | label       | value                  |
+      | Title       | TestMyDraftSavePublish |
+      | Short title | TestMyDraftSavePublish |
+      | Intro       | TestMyDraftIntro       |
+    And I click on the edit action bar button "Save"
+    Then I should be on Content update page for "TestMyDraftSavePublish"
---
+++ src/lib/Behat/Page/MyDraftsPage.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace Ibexa\AdminUi\Behat\Page;
+
+use Behat\Mink\Session;
+use Ibexa\AdminUi\Behat\Component\Dialog;
+use Ibexa\AdminUi\Behat\Component\Table\TableBuilder;
+use Ibexa\AdminUi\Behat\Component\Table\TableInterface;
+use Ibexa\Behat\Browser\Locator\VisibleCSSLocator;
+use Ibexa\Behat\Browser\Page\Page;
+use Ibexa\Behat\Browser\Routing\Router;
+
+final class MyDraftsPage extends Page
+{
+    private TableInterface $table;
+
+    private Dialog $dialog;
+
+    public function __construct(Session $session, Router $router, TableBuilder $tableBuilder, Dialog $dialog)
+    {
+        parent::__construct($session, $router);
+        $this->table = $tableBuilder->newTable()->build();
+        $this->dialog = $dialog;
+    }
+
+    public function verifyIsLoaded(): void
+    {
+        $this->getHTMLPage()
+            ->find($this->getLocator('pageTitle'))
+            ->assert()->textEquals('Drafts');
+    }
+
+    public function deleteDraft(string $draftName): void
+    {
+        $this->table->getTableRow(['Name' => $draftName])->select();
+        $this->getHTMLPage()->find($this->getLocator('deleteButton'))->click();
+        $this->dialog->verifyIsLoaded();
+        $this->dialog->confirm();
+    }
+
+    public function isDraftOnTheList(string $draftName): bool
+    {
+        return $this->table->hasElement(['Name' => $draftName]);
+    }
+
+    public function edit(string $draftName): void
+    {
+        $this->table->getTableRow(['Name' => $draftName])->edit();
+    }
+
+    protected function specifyLocators(): array
+    {
+        return [
+            new VisibleCSSLocator('deleteButton', '#confirm-content_remove_remove'),
+            new VisibleCSSLocator('pageTitle', '.ibexa-page-title h1'),
+        ];
+    }
+
+    public function getName(): string
+    {
+        return 'MyDrafts';
+    }
+
+    protected function getRoute(): string
+    {
+        return 'contentdraft/list';
+    }
+}
