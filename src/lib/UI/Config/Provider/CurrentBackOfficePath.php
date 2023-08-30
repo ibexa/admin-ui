98af74bd8692332cc1610c120ebcc838de63f9e7
Maciej Kobus <maciej.kobus@ez.no>
2023-08-30 12:23:04 +0200

    Fixed issue where insufficient permissions would break generating base path

--- src/lib/UI/Config/Provider/CurrentBackOfficePath.php
+++ src/lib/UI/Config/Provider/CurrentBackOfficePath.php
@@ -9,16 +9,21 @@ declare(strict_types=1);
 namespace Ibexa\AdminUi\UI\Config\Provider;
 
 use Ibexa\Contracts\AdminUi\UI\Config\ProviderInterface;
+use Ibexa\Contracts\Core\Repository\Repository;
 use Symfony\Component\Routing\RouterInterface;
 
 final class CurrentBackOfficePath implements ProviderInterface
 {
     private RouterInterface $router;
 
+    private Repository $repository;
+
     public function __construct(
-        RouterInterface $router
+        RouterInterface $router,
+        Repository $repository
     ) {
         $this->router = $router;
+        $this->repository = $repository;
     }
 
     public function getConfig(): string
@@ -29,11 +34,17 @@ final class CurrentBackOfficePath implements ProviderInterface
          * Generating route for `/` allows us to make sure that we do not have any additional parameters
          * and we get raw path to back office.
          * We are not using reverse matchers here as they work in context of current request.
+         *
+         * Sudo is used to avoid insufficient permissions on reading root Location.
          */
-        $routeInfo = $this->router->match('/');
-        $path = $this->router->generate($routeInfo['_route'], $routeInfo);
-        $pathInfo = pathinfo($path);
+        return $this->repository->sudo(
+            function (Repository $repository): string {
+                $routeInfo = $this->router->match('/');
+                $path = $this->router->generate($routeInfo['_route'], $routeInfo);
+                $pathInfo = pathinfo($path);
 
-        return $pathInfo['dirname'];
+                return $pathInfo['dirname'];
+            }
+        );
     }
 }
