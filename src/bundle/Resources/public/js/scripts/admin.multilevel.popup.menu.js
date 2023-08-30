381e8315163cf233f6142966cf1af684e7173d49
tischsoic <symfiz@gmail.com>
2023-08-30 16:10:32 +0200

    prettier; etc

--- src/bundle/Resources/public/js/scripts/admin.multilevel.popup.menu.js
+++ src/bundle/Resources/public/js/scripts/admin.multilevel.popup.menu.js
@@ -1,5 +1,7 @@
 (function (global, doc, ibexa) {
-    const multilevelPopupMenusContainers = doc.querySelectorAll('.ibexa-multilevel-popup-menu:not(.ibexa-multilevel-popup-menu--custom-init)');
+    const multilevelPopupMenusContainers = doc.querySelectorAll(
+        '.ibexa-multilevel-popup-menu:not(.ibexa-multilevel-popup-menu--custom-init)',
+    );
 
     multilevelPopupMenusContainers.forEach((container) => {
         const multilevelPopupMenu = new ibexa.core.MultilevelPopupMenu({
