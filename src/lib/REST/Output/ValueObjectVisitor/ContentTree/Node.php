b1a740336aef39528634ca1f8707210038ece717
tischsoic <symfiz@gmail.com>
2023-09-06 21:15:40 +0200

    IBX-6503: UX Improvements - preview

--- src/lib/REST/Output/ValueObjectVisitor/ContentTree/Node.php
+++ src/lib/REST/Output/ValueObjectVisitor/ContentTree/Node.php
@@ -34,6 +34,12 @@ class Node extends ValueObjectVisitor
         $generator->startValueElement('contentId', $data->contentId);
         $generator->endValueElement('contentId');
 
+        $generator->startValueElement('versionNo', $data->versionNo);
+        $generator->endValueElement('versionNo');
+
+        $generator->startValueElement('translations', implode(',', $data->translations));
+        $generator->endValueElement('translations');
+
         $generator->startValueElement('name', $data->name);
         $generator->endValueElement('name');
 
