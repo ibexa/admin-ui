c3d60d1110dabe49e50de14d34676a4a4c1e39f0
Krzysztof Słomka <kisztof@users.noreply.github.com>
2023-11-06 14:01:24 +0100

    Added Quick Preview tab

---
+++ src/lib/Tab/LocationView/QuickPreviewTab.php
@@ -0,0 +1,129 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\AdminUi\Tab\LocationView;
+
+use Ibexa\AdminUi\Util\FieldDefinitionGroupsUtil;
+use Ibexa\Contracts\AdminUi\Tab\AbstractEventDispatchingTab;
+use Ibexa\Contracts\AdminUi\Tab\OrderedTabInterface;
+use Ibexa\Contracts\Core\Repository\LanguageService;
+use Ibexa\Contracts\Core\Repository\Values\Content\Content;
+use Ibexa\Contracts\Core\Repository\Values\Content\Language;
+use Ibexa\Contracts\Core\SiteAccess\ConfigResolverInterface;
+use JMS\TranslationBundle\Annotation\Desc;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Twig\Environment;
+
+class QuickPreviewTab extends AbstractEventDispatchingTab implements OrderedTabInterface
+{
+    /** @var \Ibexa\AdminUi\Util\FieldDefinitionGroupsUtil */
+    private $fieldDefinitionGroupsUtil;
+
+    /** @var \Ibexa\Contracts\Core\Repository\LanguageService */
+    private $languageService;
+
+    /** @var \Ibexa\Contracts\Core\SiteAccess\ConfigResolverInterface */
+    private $configResolver;
+
+    public function __construct(
+        Environment $twig,
+        TranslatorInterface $translator,
+        FieldDefinitionGroupsUtil $fieldDefinitionGroupsUtil,
+        LanguageService $languageService,
+        EventDispatcherInterface $eventDispatcher,
+        ConfigResolverInterface $configResolver
+    ) {
+        parent::__construct($twig, $translator, $eventDispatcher);
+
+        $this->fieldDefinitionGroupsUtil = $fieldDefinitionGroupsUtil;
+        $this->languageService = $languageService;
+        $this->configResolver = $configResolver;
+    }
+
+    public function getIdentifier(): string
+    {
+        return 'quick_preview';
+    }
+
+    public function getName(): string
+    {
+        /** @Desc("Quick Preview") */
+        return $this->translator->trans('tab.name.quick_preview', [], 'ibexa_locationview');
+    }
+
+    public function getOrder(): int
+    {
+        return 100;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTemplate(): string
+    {
+        return '@ibexadesign/content/tab/quick_preview.html.twig';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTemplateParameters(array $contextParameters = []): array
+    {
+        /** @var \Ibexa\Contracts\Core\Repository\Values\Content\Content $content */
+        $content = $contextParameters['content'];
+        /** @var \Ibexa\Contracts\Core\Repository\Values\ContentType\ContentType $contentType */
+        $contentType = $contextParameters['contentType'];
+        $fieldDefinitions = $contentType->getFieldDefinitions();
+        $fieldDefinitionsByGroup = $this->fieldDefinitionGroupsUtil->groupFieldDefinitions($fieldDefinitions);
+
+        $languages = $this->loadContentLanguages($content);
+
+        return array_replace($contextParameters, [
+            'content' => $content,
+            'field_definitions_by_group' => $fieldDefinitionsByGroup,
+            'languages' => $languages,
+            'location' => $contextParameters['location'],
+        ]);
+    }
+
+    /**
+     * Loads system languages with filtering applied.
+     *
+     * @param \Ibexa\Contracts\Core\Repository\Values\Content\Content $content
+     *
+     * @return array
+     */
+    public function loadContentLanguages(Content $content): array
+    {
+        $contentLanguages = $content->versionInfo->languageCodes;
+
+        $filter = static function (Language $language) use ($contentLanguages) {
+            return $language->enabled && in_array($language->languageCode, $contentLanguages, true);
+        };
+
+        $languagesByCode = [];
+
+        foreach (array_filter($this->languageService->loadLanguages(), $filter) as $language) {
+            $languagesByCode[$language->languageCode] = $language;
+        }
+
+        $saLanguages = [];
+
+        foreach ($this->configResolver->getParameter('languages') as $languageCode) {
+            if (!isset($languagesByCode[$languageCode])) {
+                continue;
+            }
+
+            $saLanguages[] = $languagesByCode[$languageCode];
+            unset($languagesByCode[$languageCode]);
+        }
+
+        return array_merge($saLanguages, array_values($languagesByCode));
+    }
+}
