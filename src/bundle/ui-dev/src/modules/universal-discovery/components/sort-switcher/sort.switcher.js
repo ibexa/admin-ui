a03a8a37620817048779f99da7da4ea49e467b66
Lukasz Ostafin <ostafin.lucas@gmail.com>
2023-11-14 20:54:35 +0100

    UDW as GH package

--- src/bundle/ui-dev/src/modules/universal-discovery/components/sort-switcher/sort.switcher.js
+++ src/bundle/ui-dev/src/modules/universal-discovery/components/sort-switcher/sort.switcher.js
@@ -2,27 +2,26 @@ import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 
 import SimpleDropdown from '../../../common/simple-dropdown/simple.dropdown';
-// import { SortingContext, SortOrderContext, SORTING_OPTIONS } from '../../universal.discovery.module';
-import { SortingContext, SortOrderContext } from '../../universal.discovery.module';
+import { SortingContext, SortOrderContext, SORTING_OPTIONS } from '../../universal.discovery.module';
 
 const SortSwitcher = ({ isDisabled }) => {
     const [sorting, setSorting] = useContext(SortingContext);
     const [sortOrder, setSortOrder] = useContext(SortOrderContext);
-    // const selectedOption = SORTING_OPTIONS.find((option) => option.sortClause === sorting && option.sortOrder === sortOrder);
-    // const onOptionClick = (option) => {
-    //     setSorting(option.sortClause);
-    //     setSortOrder(option.sortOrder);
-    // };
+    const selectedOption = SORTING_OPTIONS.find((option) => option.sortClause === sorting && option.sortOrder === sortOrder);
+    const onOptionClick = (option) => {
+        setSorting(option.sortClause);
+        setSortOrder(option.sortOrder);
+    };
 
     return (
         <div className="c-sort-switcher">
-            {/* <SimpleDropdown
+            <SimpleDropdown
                 options={SORTING_OPTIONS}
                 selectedOption={selectedOption}
                 onOptionClick={onOptionClick}
                 isDisabled={isDisabled}
                 isSwitcher={true}
-            /> */}
+            />
         </div>
     );
 };
