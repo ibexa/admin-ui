4c82c46868ff1accc92beb2e70baa6c9948f1e76
Krzysztof Słomka <kisztof@users.noreply.github.com>
2023-11-14 14:50:16 +0100

    Moved subitems to separate tab

---
+++ src/lib/Tab/LocationView/SubItemsTab.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\AdminUi\Tab\LocationView;
+
+use Ibexa\Contracts\AdminUi\Tab\AbstractEventDispatchingTab;
+use Ibexa\Contracts\AdminUi\Tab\OrderedTabInterface;
+use JMS\TranslationBundle\Annotation\Desc;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Twig\Environment;
+
+class SubItemsTab extends AbstractEventDispatchingTab implements OrderedTabInterface
+{
+    public const URI_FRAGMENT = 'ibexa-tab-location-view-sub-items';
+
+    public function getIdentifier(): string
+    {
+        return 'sub_items';
+    }
+
+    public function getName(): string
+    {
+        /** @Desc("Sub Items") */
+        return $this->translator->trans('tab.name.sub_items', [], 'ibexa_locationview');
+    }
+
+    public function getOrder(): int
+    {
+        return 200;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTemplate(): string
+    {
+        return '@ibexadesign/content/tab/sub_items.html.twig';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTemplateParameters(array $contextParameters = []): array
+    {
+        /** @var \Ibexa\Contracts\Core\Repository\Values\Content\Content $content */
+        $content = $contextParameters['content'];
+
+        $versionInfo = $content->getVersionInfo();
+        $contentInfo = $versionInfo->getContentInfo();
+
+        $viewParameters = [
+            'content_info' => $contentInfo,
+            'version_info' => $versionInfo,
+        ];
+
+        return array_replace($contextParameters, $viewParameters);
+    }
+}
