17ef47921ad4146b1016c977316515f48084bd59
Krzysztof Słomka <kisztof@users.noreply.github.com>
2023-11-22 15:55:49 +0100

    Fixed choice loader

--- src/lib/Form/Type/User/UserModeChoiceType.php
+++ src/lib/Form/Type/User/UserModeChoiceType.php
@@ -10,7 +10,6 @@ namespace Ibexa\AdminUi\Form\Type\User;
 
 use Ibexa\AdminUi\UserSetting\UserMode;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\ChoiceList\ChoiceList;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -21,26 +20,16 @@ final class UserModeChoiceType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'choice_loader' => ChoiceList::lazy($this, $this->getChoiceLoader()),
             'translation_domain' => self::TRANSLATION_DOMAIN,
+            'choices' => [
+                'user.setting.mode.expert' => UserMode::EXPERT,
+                'user.setting.mode.smart' => UserMode::SMART,
+            ],
         ]);
     }
 
     public function getParent(): string
     {
         return ChoiceType::class;
-    }
-
-    /**
-     * @phpstan-return callable(): array<string, string>
-     */
-    private function getChoiceLoader(): callable
-    {
-        return static function (): array {
-            return [
-                'user.setting.mode.expert' => UserMode::EXPERT,
-                'user.setting.mode.smart' => UserMode::SMART,
-            ];
-        };
     }
 }
