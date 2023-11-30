dfbdf9986373c7df4d458bde031296115b7eae37
Adam Wójs <adam@wojs.pl>
2023-11-30 14:37:22 +0100

    fixup! Added form to switch between smart/expert mode to user menu

--- src/bundle/Resources/public/js/scripts/admin.user.mode.js
+++ src/bundle/Resources/public/js/scripts/admin.user.mode.js
@@ -2,9 +2,11 @@
     const FORM_SELECTOR = 'form[name=user_mode_change]';
     const form = doc.querySelector(FORM_SELECTOR);
 
-    form.querySelectorAll('input[type=checkbox]').forEach((input) => {
-        input.addEventListener('change', () => {
-            form.submit();
+    if (form) {
+        form.querySelectorAll('input[type=checkbox]').forEach((input) => {
+            input.addEventListener('change', () => {
+                form.submit();
+            });
         });
-    });
+    }
 })(window, window.document);
