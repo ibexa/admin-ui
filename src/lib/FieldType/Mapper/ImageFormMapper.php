5d151458f4ca0031bf4fbea4313384d42e6e201c
Tomasz Kryszan <tomasz.kryszan@ez.no>
2023-12-01 11:00:00 +0100

    Added mimeTypes field to ImageFormMapper

--- src/lib/FieldType/Mapper/ImageFormMapper.php
+++ src/lib/FieldType/Mapper/ImageFormMapper.php
@@ -9,22 +9,36 @@ namespace Ibexa\AdminUi\FieldType\Mapper;
 use Ibexa\AdminUi\FieldType\FieldDefinitionFormMapperInterface;
 use Ibexa\AdminUi\Form\Data\FieldDefinitionData;
 use Ibexa\ContentForms\ConfigResolver\MaxUploadSize;
-use Ibexa\Contracts\Core\Repository\FieldTypeService;
 use JMS\TranslationBundle\Annotation\Desc;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Mime\MimeTypesInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Range;
 
 class ImageFormMapper implements FieldDefinitionFormMapperInterface
 {
+    /** @var array<string> */
+    private array $allowedMimeTypes;
+
     /** @var \Ibexa\ContentForms\ConfigResolver\MaxUploadSize */
     private $maxUploadSize;
 
-    public function __construct(FieldTypeService $fieldTypeService, MaxUploadSize $maxUploadSize)
-    {
+    private MimeTypesInterface $mimeTypes;
+
+    /**
+     * @param string[] $allowedMimeTypes
+     */
+    public function __construct(
+        array $allowedMimeTypes,
+        MaxUploadSize $maxUploadSize,
+        MimeTypesInterface $mimeTypes
+    ) {
+        $this->allowedMimeTypes = $allowedMimeTypes;
         $this->maxUploadSize = $maxUploadSize;
+        $this->mimeTypes = $mimeTypes;
     }
 
     public function mapFieldDefinitionForm(FormInterface $fieldDefinitionForm, FieldDefinitionData $data): void
@@ -48,6 +62,13 @@ class ImageFormMapper implements FieldDefinitionFormMapperInterface
                 'disabled' => $isTranslation,
                 'scale' => 1,
             ])
+            ->add('mimeTypes', ChoiceType::class, [
+                'required' => false,
+                'multiple' => true,
+                'choices' => $this->getMimeTypesChoiceList(),
+                'property_path' => 'fieldSettings[mimeTypes]',
+                'label' => /** @Desc("Image types") */ 'field_definition.ezimage.image_types',
+            ])
             ->add('isAlternativeTextRequired', CheckboxType::class, [
                 'required' => false,
                 'property_path' => 'validatorConfiguration[AlternativeTextValidator][required]',
@@ -67,6 +88,21 @@ class ImageFormMapper implements FieldDefinitionFormMapperInterface
             ->setDefaults([
                 'translation_domain' => 'ibexa_content_type',
             ]);
+    }
+
+    /**
+     * @return array<string>
+     */
+    private function getMimeTypesChoiceList(): array
+    {
+        $mimeTypeChoiceList = [];
+        foreach ($this->allowedMimeTypes as $mimeType) {
+            $extensions = implode(', ', $this->mimeTypes->getExtensions($mimeType));
+            $label = explode('image/', $mimeType);
+            $mimeTypeChoiceList[$label[1] . " ($extensions)"] = $mimeType;
+        }
+
+        return $mimeTypeChoiceList;
     }
 }
 
