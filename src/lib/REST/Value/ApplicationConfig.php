e06d4bcc371c9c43aaa111bb782cc983059b324d
Tomasz Kryszan <tomasz.kryszan@ez.no>
2023-12-04 16:37:23 +0100

    Added ApplicationConfig REST Value

---
+++ src/lib/REST/Value/ApplicationConfig.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\AdminUi\REST\Value;
+
+use Ibexa\Rest\Value;
+
+final class ApplicationConfig extends Value
+{
+    /** @var array<string, mixed> */
+    private array $config;
+
+    /**
+     * @param array<string, mixed> $config
+     */
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function getConfig(): array
+    {
+        return $this->config;
+    }
+}
