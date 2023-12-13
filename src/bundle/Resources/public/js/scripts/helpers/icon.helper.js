c1ed331242192ca19090bea1ac95bf853557b83f
Lukasz Ostafin <ostafin.lucas@gmail.com>
2023-12-13 15:34:45 +0100

    remove old code from icon helper

--- src/bundle/Resources/public/js/scripts/helpers/icon.helper.js
+++ src/bundle/Resources/public/js/scripts/helpers/icon.helper.js
@@ -1,12 +1,7 @@
-import { getAdminUiConfig, getRestInfo } from './context.helper';
+import { getAdminUiConfig } from './context.helper';
 
 const getIconPath = (path, iconSet) => {
-    const { instanceUrl } = getRestInfo();
     const adminUiConfig = getAdminUiConfig();
-
-    if (window.origin !== instanceUrl) {
-        return `/ibexa-icons.svg#${path}`;
-    }
 
     if (!iconSet) {
         iconSet = adminUiConfig.iconPaths.defaultIconSet;
