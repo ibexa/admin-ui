a68ce3844a72d33b9b9b2863ceac2250f311ab67
Robert Swoboda <53216935+Gengar-i@users.noreply.github.com>
2024-01-04 14:12:26 +0100

    IBX-7411: Implemented datalabels for doughnut charts

--- src/bundle/Resources/public/js/scripts/core/doughnut.chart.js
+++ src/bundle/Resources/public/js/scripts/core/doughnut.chart.js
@@ -1,15 +1,49 @@
-(function (global, doc, ibexa) {
+(function (global, doc, ibexa, ChartDataLabels) {
+    const doughnutOptions = {
+        plugins: {
+            datalabels: {
+                color: '#FFFFFF',
+                font: {
+                    weight: 'bold',
+                    size: 18,
+                },
+                formatter: (value, context) => {
+                    const sum = context.dataset.data.reduce((acc, curValue) => acc + curValue, 0);
+                    const percentage = (value / sum) * 100;
+
+                    return `${Math.floor(percentage)}%`;
+                },
+            },
+        },
+    };
+    const doughnutPlugins = [ChartDataLabels];
+
     class DoughnutChart extends ibexa.core.BaseChart {
         constructor(data, options = {}, plugins = []) {
             super(data, options, plugins);
 
+            this.setOptions(options);
             this.type = 'doughnut';
         }
 
         getType() {
             return this.type;
         }
+
+        setOptions(options) {
+            super.setOptions(options);
+
+            this.options.plugins = {
+                ...this.options.plugins,
+                ...doughnutOptions.plugins,
+            };
+        }
+
+        setPlugins(plugins) {
+            super.setPlugins(plugins);
+            this.plugins = [...doughnutPlugins, ...this.plugins];
+        }
     }
 
     ibexa.addConfig('core.chart.DoughnutChart', DoughnutChart);
-})(window, window.document, window.ibexa);
+})(window, window.document, window.ibexa, window.ChartDataLabels);
