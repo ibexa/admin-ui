4a32abf2b598925e59e64898dd48cb99116947af
Robert Swoboda <53216935+Gengar-i@users.noreply.github.com>
2024-01-30 13:34:55 +0100

    Changed to isEdge with comment

--- src/bundle/Resources/public/js/scripts/helpers/browser.helper.js
+++ src/bundle/Resources/public/js/scripts/helpers/browser.helper.js
@@ -1,7 +1,7 @@
 const { userAgent } = window.navigator;
-const isEdg = () => userAgent.includes('Edg');
-const isChrome = () => userAgent.includes('Chrome') && !isEdg();
+const isEdge = () => userAgent.includes('Edg'); // Edge previously had Edge but they changed to Edg
+const isChrome = () => userAgent.includes('Chrome') && !isEdge();
 const isFirefox = () => userAgent.includes('Firefox');
-const isSafari = () => userAgent.includes('Safari') && !isChrome() && !isEdg();
+const isSafari = () => userAgent.includes('Safari') && !isChrome() && !isEdge();
 
-export { isChrome, isFirefox, isSafari, isEdg };
+export { isChrome, isFirefox, isSafari, isEdge };
