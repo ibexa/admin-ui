23760a37f72539458658a80500bc9de849171604
Robert Swoboda <53216935+Gengar-i@users.noreply.github.com>
2024-02-02 12:49:06 +0100

    Fixed setting cokkie when its not defined

--- src/bundle/Resources/public/js/scripts/sidebar/main.menu.js
+++ src/bundle/Resources/public/js/scripts/sidebar/main.menu.js
@@ -70,6 +70,12 @@
 
             doc.addEventListener('mousemove', collapseSecondLevelMenu, false);
         } else {
+            const secondLevelMenuWidth = ibexa.helpers.cookies.getCookie('ibexa-aui_menu-secondary-width');
+
+            if (!secondLevelMenuWidth) {
+                ibexa.helpers.cookies.setBackOfficeCookie('ibexa-aui_menu-secondary-width', SECOND_LEVEL_EXPANDED_WIDTH);
+            }
+
             setWidthOfSecondLevelMenu();
         }
     };
