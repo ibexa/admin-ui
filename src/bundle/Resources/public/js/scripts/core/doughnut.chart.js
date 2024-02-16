50b18e1f8c70a46ec47003ca156ce4682e128bec
Robert Swoboda <53216935+Gengar-i@users.noreply.github.com>
2024-02-16 16:39:47 +0100

    Used charNode variable

--- src/bundle/Resources/public/js/scripts/core/doughnut.chart.js
+++ src/bundle/Resources/public/js/scripts/core/doughnut.chart.js
@@ -37,7 +37,7 @@
             this.canvas = this.chartNode.querySelector('.ibexa-chart__canvas');
             this.legendNode = this.chartNode.querySelector('.ibexa-chart-legend');
 
-            this.initialize(data.ref);
+            this.initialize(chartNode);
         }
 
         initialize(ref) {
