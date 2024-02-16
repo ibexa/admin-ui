7b3a71b4dad822ef1da1cd3f7e85d4ad4cbc4f40
Robert Swoboda <53216935+Gengar-i@users.noreply.github.com>
2024-02-16 16:26:12 +0100

    IBX-7574: Hidded label in doughnut chart when unchecked

--- src/bundle/Resources/public/js/scripts/core/doughnut.chart.js
+++ src/bundle/Resources/public/js/scripts/core/doughnut.chart.js
@@ -1,4 +1,7 @@
 (function (global, doc, ibexa, ChartDataLabels) {
+    const IBEXA_WHITE = '#fff';
+    const IBEXA_COLOR_BASE_DARK = '#878b90';
+    const dataLabelsMap = new Map();
     const doughnutOptions = {
         plugins: {
             datalabels: {
@@ -13,6 +16,12 @@
 
                     return `${Math.floor(percentage)}%`;
                 },
+                display: (context) => {
+                    const { dataIndex } = context;
+                    const isVisible = dataLabelsMap.get(dataIndex);
+
+                    return isVisible;
+                },
             },
         },
     };
@@ -23,7 +32,11 @@
             super(data, options, plugins);
 
             this.setOptions(options);
+            this.data = data;
             this.type = 'doughnut';
+            this.chartNode = data.ref;
+            this.canvas = this.chartNode.querySelector('.ibexa-chart__canvas');
+            this.legendNode = this.chartNode.querySelector('.ibexa-chart-legend');
 
             this.initialize(data.ref);
         }
@@ -48,7 +61,87 @@
         setPlugins(plugins) {
             super.setPlugins(plugins);
 
-            this.plugins = [...doughnutPlugins, ...this.plugins];
+            const beforeInitPlugin = [
+                {
+                    beforeInit: (chart) => {
+                        const { itemTemplate } = this.legendNode.dataset;
+                        const fragment = doc.createDocumentFragment();
+                        const data = chart.data.datasets[0];
+                        data.legend.forEach((legendItem, index) => {
+                            dataLabelsMap.set(index, true);
+                            const container = doc.createElement('div');
+                            const renderedItemTemplate = itemTemplate
+                                .replace('{{ checked_color }}', data.backgroundColor[index])
+                                .replace('{{ dataset_index }}', index)
+                                .replace('{{ label }}', legendItem);
+
+                            container.insertAdjacentHTML('beforeend', renderedItemTemplate);
+
+                            const checkboxNode = container.querySelector('.ibexa-chart-legend__item-wrapper');
+
+                            checkboxNode.querySelector('input').checked = true;
+                            fragment.append(checkboxNode);
+                        });
+
+                        this.legendNode.appendChild(fragment);
+
+                        return fragment;
+                    },
+                },
+            ];
+
+            this.plugins = [...beforeInitPlugin, ...doughnutPlugins, ...this.plugins];
+        }
+
+        setLegendCheckboxBackground(checkbox) {
+            const { checkedColor } = checkbox.dataset;
+            const { checked } = checkbox;
+
+            if (checked) {
+                checkbox.style.backgroundColor = checkedColor;
+                checkbox.style.borderColor = checkedColor;
+            } else {
+                checkbox.style.backgroundColor = IBEXA_WHITE;
+                checkbox.style.borderColor = IBEXA_COLOR_BASE_DARK;
+            }
+        }
+
+        updateDataLabels(checkbox, datasetIndex) {
+            const { checked } = checkbox;
+
+            if (typeof datasetIndex !== 'number') {
+                return;
+            }
+
+            if (checked) {
+                dataLabelsMap.set(datasetIndex, true);
+            } else {
+                dataLabelsMap.set(datasetIndex, false);
+            }
+        }
+
+        setLegendCheckboxes() {
+            if (!this.legendNode) {
+                return;
+            }
+
+            this.legendNode.querySelectorAll('.ibexa-input--legend-item-checkbox').forEach((checkbox) => {
+                this.setLegendCheckboxBackground(checkbox);
+                checkbox.addEventListener('change', (event) => {
+                    const datasetIndex = parseInt(event.currentTarget.dataset.datasetIndex, 10);
+
+                    this.setLegendCheckboxBackground(event.currentTarget);
+                    this.updateDataLabels(event.currentTarget, datasetIndex);
+
+                    const chartMethod = event.currentTarget.checked ? 'show' : 'hide';
+
+                    this.chart[chartMethod](0, datasetIndex);
+                });
+            });
+        }
+
+        callbackAfterRender() {
+            this.setLegendCheckboxes();
         }
     }
 
