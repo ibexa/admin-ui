02c95642e6413fb23e599176fe9bc0a880a03407
Tomasz Kryszan <tomasz.kryszan@ez.no>
2024-04-05 12:50:57 +0200

    Improved folder configuration

--- src/lib/UI/Config/Provider/Module/DamWidget.php
+++ src/lib/UI/Config/Provider/Module/DamWidget.php
@@ -11,19 +11,20 @@ namespace Ibexa\AdminUi\UI\Config\Provider\Module;
 use Ibexa\Bundle\Core\ApiLoader\Exception\InvalidSearchEngine;
 use Ibexa\Bundle\Core\ApiLoader\RepositoryConfigurationProvider;
 use Ibexa\Contracts\AdminUi\UI\Config\ProviderInterface;
+use Ibexa\Contracts\Core\Repository\ContentTypeService;
+use Ibexa\Contracts\Core\Repository\NameSchema\SchemaIdentifierExtractorInterface;
+use Ibexa\Contracts\Core\Repository\Values\ContentType\ContentType;
 
 /**
- * @template TFolderConfig of array{
- *     contentTypeIdentifier: string,
- *     nameFieldIdentifier: string
- * }
  * @template TConfig of array{
  *     image: array{
  *         fieldDefinitionIdentifiers: array<string>,
  *         contentTypeIdentifiers: array<string>,
  *         aggregations: array<string, array<string, string>>,
  *     },
- *     folder: TFolderConfig
+ *     folder: array{
+ *         contentTypeIdentifier: string,
+ *     }
  *  }
  */
 final class DamWidget implements ProviderInterface
@@ -31,17 +32,25 @@ final class DamWidget implements ProviderInterface
     /** @phpstan-var TConfig */
     private array $config;
 
+    private ContentTypeService $contentTypeService;
+
     private RepositoryConfigurationProvider $repositoryConfigurationProvider;
+
+    private SchemaIdentifierExtractorInterface $schemaIdentifierExtractor;
 
     /**
      * @phpstan-param TConfig $config
      */
     public function __construct(
         array $config,
-        RepositoryConfigurationProvider $repositoryConfigurationProvider
+        ContentTypeService $contentTypeService,
+        RepositoryConfigurationProvider $repositoryConfigurationProvider,
+        SchemaIdentifierExtractorInterface $schemaIdentifierExtractor
     ) {
         $this->config = $config;
+        $this->contentTypeService = $contentTypeService;
         $this->repositoryConfigurationProvider = $repositoryConfigurationProvider;
+        $this->schemaIdentifierExtractor = $schemaIdentifierExtractor;
     }
 
     /**
@@ -52,17 +61,58 @@ final class DamWidget implements ProviderInterface
      *         aggregations: array<string, array<string, string>>,
      *         showImageFilters: bool,
      *     },
-     *     folder: TFolderConfig
+     *     folder: array{
+     *         contentTypeIdentifier: string,
+     *         nameSchemaIdentifiers: array<string>
+     *     }
      * }
      *
      * @throws \Ibexa\Bundle\Core\ApiLoader\Exception\InvalidSearchEngine
+     * @throws \Ibexa\Contracts\Core\Repository\Exceptions\NotFoundException
      */
     public function getConfig(): array
     {
         $widgetConfig = $this->config;
         $widgetConfig['image']['showImageFilters'] = $this->showImageFilters();
+        $widgetConfig['folder'] = $this->getFolderConfig();
 
         return $widgetConfig;
+    }
+
+    /**
+     * @return array{
+     *     contentTypeIdentifier: string,
+     *     nameSchemaIdentifiers: array<string>
+     * }
+     *
+     * @throws \Ibexa\Contracts\Core\Repository\Exceptions\NotFoundException
+     */
+    private function getFolderConfig(): array
+    {
+        $contentTypeIdentifier = $this->config['folder']['contentTypeIdentifier'];
+
+        return [
+            'contentTypeIdentifier' => $contentTypeIdentifier,
+            'nameSchemaIdentifiers' => $this->extractNameSchemaIdentifiers(
+                $this->loadContentType($contentTypeIdentifier)
+            ),
+        ];
+    }
+
+    /**
+     * @throws \Ibexa\Contracts\Core\Repository\Exceptions\NotFoundException
+     */
+    private function loadContentType(string $contentTypeIdentifier): ContentType
+    {
+        return $this->contentTypeService->loadContentTypeByIdentifier($contentTypeIdentifier);
+    }
+
+    /**
+     * @return array<string>
+     */
+    private function extractNameSchemaIdentifiers(ContentType $contentType): array
+    {
+        return $this->schemaIdentifierExtractor->extract($contentType->nameSchema)['field'] ?? [];
     }
 
     /**
