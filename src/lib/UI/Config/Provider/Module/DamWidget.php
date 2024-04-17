fbb2228f8c69662b3b952cbdfb2baa1c9ba67c53
Tomasz Kryszan <tomasz.kryszan@ez.no>
2024-04-17 12:24:38 +0200

    IBX-7981: Improved folder configuration (#1231)
    * Improved folder configuration
    
    * [Tests] Adjusted DamWidgetTest
    
    * IBX-8089: Added configuration for image management (#1236)
    
    * Added image mappings parameter
    
    * Adjusted DamWidget
    
    * [Tests] Adjusted DamWidgetTest
    
    * [PHPStan] Added improvements

--- src/lib/UI/Config/Provider/Module/DamWidget.php
+++ src/lib/UI/Config/Provider/Module/DamWidget.php
@@ -11,58 +11,148 @@ namespace Ibexa\AdminUi\UI\Config\Provider\Module;
 use Ibexa\Bundle\Core\ApiLoader\Exception\InvalidSearchEngine;
 use Ibexa\Bundle\Core\ApiLoader\RepositoryConfigurationProvider;
 use Ibexa\Contracts\AdminUi\UI\Config\ProviderInterface;
+use Ibexa\Contracts\Core\Repository\ContentTypeService;
+use Ibexa\Contracts\Core\Repository\NameSchema\SchemaIdentifierExtractorInterface;
+use Ibexa\Contracts\Core\Repository\Values\ContentType\ContentType;
 
 /**
- * @template TFolderConfig of array{
- *     contentTypeIdentifier: string,
- *     nameFieldIdentifier: string
- * }
- * @template TConfig of array{
+ * @phpstan-type TConfig array{
  *     image: array{
- *         fieldDefinitionIdentifiers: array<string>,
- *         contentTypeIdentifiers: array<string>,
  *         aggregations: array<string, array<string, string>>,
+ *         mappings: array<
+ *             string,
+ *             array{
+ *                 imageFieldIdentifier: string
+ *             },
+ *         >,
  *     },
- *     folder: TFolderConfig
- *  }
+ *     folder: array{
+ *         contentTypeIdentifier: string,
+ *     }
+ * }
+ * @phpstan-type TImageConfig array{
+ *     fieldDefinitionIdentifiers: array<string>,
+ *     contentTypeIdentifiers: array<string>,
+ *     aggregations: array<string, array<string, string>>,
+ *     showImageFilters: bool,
+ *     mappings: array<
+ *         string,
+ *         array{
+ *             imageFieldIdentifier: string,
+ *             nameSchemaIdentifiers: array<string>,
+ *         }
+ *     >,
+ * }
+ * @phpstan-type TFolderConfig array{
+ *     contentTypeIdentifier: string,
+ *     nameSchemaIdentifiers: array<string>
+ * }
  */
 final class DamWidget implements ProviderInterface
 {
     /** @phpstan-var TConfig */
     private array $config;
 
+    private ContentTypeService $contentTypeService;
+
     private RepositoryConfigurationProvider $repositoryConfigurationProvider;
+
+    private SchemaIdentifierExtractorInterface $schemaIdentifierExtractor;
 
     /**
      * @phpstan-param TConfig $config
      */
     public function __construct(
         array $config,
-        RepositoryConfigurationProvider $repositoryConfigurationProvider
+        ContentTypeService $contentTypeService,
+        RepositoryConfigurationProvider $repositoryConfigurationProvider,
+        SchemaIdentifierExtractorInterface $schemaIdentifierExtractor
     ) {
         $this->config = $config;
+        $this->contentTypeService = $contentTypeService;
         $this->repositoryConfigurationProvider = $repositoryConfigurationProvider;
+        $this->schemaIdentifierExtractor = $schemaIdentifierExtractor;
     }
 
     /**
      * @phpstan-return array{
-     *     image: array{
-     *         fieldDefinitionIdentifiers: array<string>,
-     *         contentTypeIdentifiers: array<string>,
-     *         aggregations: array<string, array<string, string>>,
-     *         showImageFilters: bool,
-     *     },
+     *     image: TImageConfig,
      *     folder: TFolderConfig
      * }
      *
-     * @throws \Ibexa\Bundle\Core\ApiLoader\Exception\InvalidSearchEngine
+     * @throws \Ibexa\Contracts\Core\Repository\Exceptions\NotFoundException
      */
     public function getConfig(): array
     {
-        $widgetConfig = $this->config;
-        $widgetConfig['image']['showImageFilters'] = $this->showImageFilters();
+        return [
+            'image' => $this->getImageConfig(),
+            'folder' => $this->getFolderConfig(),
+        ];
+    }
 
-        return $widgetConfig;
+    /**
+     * @phpstan-return TImageConfig
+     *
+     * @throws \Ibexa\Contracts\Core\Repository\Exceptions\NotFoundException
+     */
+    private function getImageConfig(): array
+    {
+        $imageConfig = [
+            'showImageFilters' => $this->showImageFilters(),
+            'aggregations' => $this->config['image']['aggregations'],
+        ];
+
+        $mappings = [];
+        $contentTypeIdentifiers = [];
+        $fieldDefinitionIdentifiers = [];
+
+        foreach ($this->config['image']['mappings'] as $contentTypeIdentifier => $mapping) {
+            $contentTypeIdentifiers[] = $contentTypeIdentifier;
+            $fieldDefinitionIdentifiers[] = $mapping['imageFieldIdentifier'];
+            $mappings[$contentTypeIdentifier] = $mapping;
+
+            $contentType = $this->loadContentType($contentTypeIdentifier);
+            $mappings[$contentTypeIdentifier]['nameSchemaIdentifiers'] = $this->extractNameSchemaIdentifiers($contentType);
+        }
+
+        $imageConfig['mappings'] = $mappings;
+        $imageConfig['contentTypeIdentifiers'] = $contentTypeIdentifiers;
+        $imageConfig['fieldDefinitionIdentifiers'] = $fieldDefinitionIdentifiers;
+
+        return $imageConfig;
+    }
+
+    /**
+     * @phpstan-return TFolderConfig
+     *
+     * @throws \Ibexa\Contracts\Core\Repository\Exceptions\NotFoundException
+     */
+    private function getFolderConfig(): array
+    {
+        $contentTypeIdentifier = $this->config['folder']['contentTypeIdentifier'];
+
+        return [
+            'contentTypeIdentifier' => $contentTypeIdentifier,
+            'nameSchemaIdentifiers' => $this->extractNameSchemaIdentifiers(
+                $this->loadContentType($contentTypeIdentifier)
+            ),
+        ];
+    }
+
+    /**
+     * @throws \Ibexa\Contracts\Core\Repository\Exceptions\NotFoundException
+     */
+    private function loadContentType(string $contentTypeIdentifier): ContentType
+    {
+        return $this->contentTypeService->loadContentTypeByIdentifier($contentTypeIdentifier);
+    }
+
+    /**
+     * @return array<string>
+     */
+    private function extractNameSchemaIdentifiers(ContentType $contentType): array
+    {
+        return $this->schemaIdentifierExtractor->extract($contentType->nameSchema)['field'] ?? [];
     }
 
     /**
