ba368d6a30824cce53b4d4cb2c94fe3e3ad34131
Łukasz Ostafin <ostafin.lucas@gmail.com>
2024-04-18 15:05:02 +0200

    IBX-7816: ALW files upload (#1184)
    Co-authored-by: Tomasz Kryszan <tomasz.kryszan@ez.no>

--- src/bundle/ui-dev/src/modules/common/tooltip-popup/tooltip.popup.component.js
+++ src/bundle/ui-dev/src/modules/common/tooltip-popup/tooltip.popup.component.js
@@ -1,64 +1,45 @@
-import React, { useLayoutEffect, useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
-import Icon from '../icon/icon';
 
-const { Translator } = window;
-
-const INITIAL_HEIGHT = 'initial';
-const HEADER_HEIGHT = 35;
-
-const TooltipPopupComponent = (props) => {
+const TooltipPopupComponent = ({
+    title,
+    subtitle,
+    children,
+    onConfirm,
+    confirmBtnAttrs,
+    confirmLabel,
+    onClose,
+    closeBtnAttrs,
+    closeLabel,
+    visible,
+}) => {
     const contentRef = useRef();
-    const [maxHeight, setMaxHeight] = useState(INITIAL_HEIGHT);
-
-    useLayoutEffect(() => {
-        const { top, height } = contentRef.current.getBoundingClientRect();
-        const topRounded = Math.round(top);
-
-        if (topRounded < HEADER_HEIGHT) {
-            setMaxHeight(height + topRounded - HEADER_HEIGHT);
-        } else if (topRounded > HEADER_HEIGHT) {
-            setMaxHeight(INITIAL_HEIGHT);
-        }
-    });
-
     const attrs = {
         className: 'c-tooltip-popup',
-        hidden: !props.visible,
+        hidden: !visible,
     };
-    const contentStyle =
-        maxHeight === INITIAL_HEIGHT
-            ? {}
-            : {
-                  maxHeight,
-                  overflowY: 'scroll',
-              };
-    const closeLabel = Translator.trans(/*@Desc("Close")*/ 'tooltip.close_label', {}, 'ibexa_content');
 
     return (
         <div {...attrs}>
             <div className="c-tooltip-popup__header">
-                <h1 className="c-tooltip-popup__title">{props.title}</h1>
-                <div
-                    className="btn ibexa-btn ibexa-btn--ghost ibexa-btn--no-text ibexa-btn--small c-tooltip-popup__close"
-                    title={closeLabel}
-                    onClick={props.onClose}
-                    tabIndex="-1"
-                    data-tooltip-container-selector=".c-tooltip-popup__header"
-                >
-                    <Icon name="discard" extraClasses="ibexa-icon--small-medium" />
-                </div>
+                <h1 className="c-tooltip-popup__title">{title}</h1>
+                {subtitle && <div className="c-tooltip-popup__subtitle">{subtitle}</div>}
             </div>
-            <div className="c-tooltip-popup__content" ref={contentRef} style={contentStyle}>
-                {props.children}
+            <div className="c-tooltip-popup__content" ref={contentRef}>
+                {children}
             </div>
-            {props.showFooter && (
-                <div className="c-tooltip-popup__footer">
-                    <button className="btn ibexa-btn ibexa-btn--secondary" type="button" onClick={props.onClose}>
+            <div className="c-tooltip-popup__footer">
+                {confirmLabel && (
+                    <button className="btn ibexa-btn ibexa-btn--primary" type="button" onClick={onConfirm} {...confirmBtnAttrs}>
+                        {confirmLabel}
+                    </button>
+                )}
+                {closeLabel && (
+                    <button className="btn ibexa-btn ibexa-btn--ghost" type="button" onClick={onClose} {...closeBtnAttrs}>
                         {closeLabel}
                     </button>
-                </div>
-            )}
+                )}
+            </div>
         </div>
     );
 };
@@ -67,13 +48,23 @@ TooltipPopupComponent.propTypes = {
     title: PropTypes.string.isRequired,
     children: PropTypes.node.isRequired,
     visible: PropTypes.bool.isRequired,
+    subtitle: PropTypes.string,
     onClose: PropTypes.func,
-    showFooter: PropTypes.bool,
+    onConfirm: PropTypes.func,
+    confirmLabel: PropTypes.string,
+    closeLabel: PropTypes.string,
+    confirmBtnAttrs: PropTypes.object,
+    closeBtnAttrs: PropTypes.object,
 };
 
 TooltipPopupComponent.defaultProps = {
+    subtitle: '',
     onClose: () => {},
-    showFooter: true,
+    onConfirm: () => {},
+    confirmLabel: '',
+    closeLabel: '',
+    confirmBtnAttrs: {},
+    closeBtnAttrs: {},
 };
 
 export default TooltipPopupComponent;
