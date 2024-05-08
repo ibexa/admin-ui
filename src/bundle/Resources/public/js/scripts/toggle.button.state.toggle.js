044ce4ba252e5b3087818b27e10915bc914d9b7b
Michał Grabowski <michal.grabowski@ibexa.co>
2024-05-08 13:04:01 +0200

    IBX-8144: [Rebranding] Remove compatibility layer from webpack encore

--- src/bundle/Resources/public/js/scripts/toggle.button.state.toggle.js
+++ src/bundle/Resources/public/js/scripts/toggle.button.state.toggle.js
@@ -1,7 +1,7 @@
-(function (global, doc, eZ) {
+(function (global, doc, ibexa) {
     doc.querySelectorAll('.ibexa-toggle').forEach((toggleNode) => {
-        const toggleButton = new eZ.core.ToggleButton({ toggleNode });
+        const toggleButton = new ibexa.core.ToggleButton({ toggleNode });
 
         toggleButton.init();
     });
-})(window, window.document, window.eZ);
+})(window, window.document, window.ibexa);
