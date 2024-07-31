349a6c339c27d3a6409ca578c49faccdec202a79
Bartek Wajda <bartlomiej.wajda@ibexa.co>
2024-07-31 10:04:03 +0200

    IBX-8429: Simplified code

--- src/lib/Util/FieldDefinitionGroupsUtil.php
+++ src/lib/Util/FieldDefinitionGroupsUtil.php
@@ -31,17 +31,15 @@ class FieldDefinitionGroupsUtil
     public function groupFieldDefinitions(iterable $fieldDefinitions): array
     {
         $fieldsGroups = $this->fieldsGroupsListHelper->getGroups();
+        $defaultGroup = $this->fieldsGroupsListHelper->getDefaultGroup();
 
         $fieldDefinitionsByGroup = [];
         foreach ($fieldDefinitions as $fieldDefinition) {
-            $groupId = $fieldDefinition->fieldGroup;
-            if (!$groupId) {
-                $groupId = $this->fieldsGroupsListHelper->getDefaultGroup();
-            }
+            $groupId = $fieldDefinition->fieldGroup ?: $defaultGroup;
 
             $fieldDefinitionsByGroup[$groupId]['name'] = $fieldDefinitionsByGroup[$groupId]['name']
                 ?? $fieldsGroups[$groupId]
-                ?? $groupId;
+                ?? $defaultGroup;
             $fieldDefinitionsByGroup[$groupId]['fieldDefinitions'][] = $fieldDefinition;
         }
 
