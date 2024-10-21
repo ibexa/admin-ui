704742358e3acb71a1f9a58b291041bc66fcf2d8
Lukasz Ostafin <ostafin.lucas@gmail.com>
2024-10-21 14:58:16 +0200

    dependecies

---
+++ src/bundle/ui-dev/src/modules/common/icon/icon.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { isExternalInstance } from '@ibexa-admin-ui/src/bundle/Resources/public/js/scripts/helpers/context.helper';
+import { createCssClassNames } from '../helpers/css.class.names';
+
+import UrlIcon from './urlIcon';
+import InculdedIcon from './inculdedIcon';
+
+const Icon = (props) => {
+    const cssClass = createCssClassNames({
+        'ibexa-icon': true,
+        [props.extraClasses]: true,
+    });
+
+    const isIconIncluded = props.useIncludedIcon || isExternalInstance();
+
+    return (
+        <>
+            {isIconIncluded ? (
+                <InculdedIcon cssClass={cssClass} name={props.name} defaultIconName={props.defaultIconName} />
+            ) : (
+                <UrlIcon cssClass={cssClass} name={props.name} customPath={props.customPath} />
+            )}
+        </>
+    );
+};
+
+Icon.propTypes = {
+    extraClasses: PropTypes.string,
+    name: PropTypes.string,
+    customPath: PropTypes.string,
+    useIncludedIcon: PropTypes.bool,
+    defaultIconName: PropTypes.string,
+};
+
+Icon.defaultProps = {
+    customPath: null,
+    name: null,
+    extraClasses: null,
+    useIncludedIcon: false,
+    defaultIconName: 'about-info',
+};
+
+export default Icon;
---
+++ src/bundle/ui-dev/src/modules/common/icon/urlIcon.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { getIconPath } from '@ibexa-admin-ui/src/bundle/Resources/public/js/scripts/helpers/icon.helper';
+
+const UrlIcon = (props) => {
+    const linkHref = props.customPath ?? getIconPath(props.name);
+
+    return (
+        <svg className={props.cssClass}>
+            <use xlinkHref={linkHref} />
+        </svg>
+    );
+};
+
+UrlIcon.propTypes = {
+    cssClass: PropTypes.string,
+    name: PropTypes.string,
+    customPath: PropTypes.string,
+};
+
+UrlIcon.defaultProps = {
+    customPath: null,
+    name: null,
+    cssClass: '',
+};
+
+export default UrlIcon;
