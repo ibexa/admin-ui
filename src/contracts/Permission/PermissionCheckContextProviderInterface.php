1f0af0466229aaa6d382dda9ae149337c3594aa6
Tomasz Kryszan <tomasz.kryszan@ibexa.co>
2024-10-31 09:47:58 +0100

    Added PermissionCheckContextProviderInterface

---
+++ src/contracts/Permission/PermissionCheckContextProviderInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+
+namespace Ibexa\Contracts\AdminUi\Permission;
+
+use Ibexa\AdminUi\Values\PermissionCheckContext;
+use Symfony\Component\HttpFoundation\Request;
+
+interface PermissionCheckContextProviderInterface
+{
+    public function supports(string $module, string $function): bool;
+
+    public function getPermissionCheckContext(string $module, string $function, Request $request): PermissionCheckContext;
+}
