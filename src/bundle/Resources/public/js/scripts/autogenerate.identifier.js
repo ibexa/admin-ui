27ff2b2ff6fd821cd88cfa4c8b155964c1c3b926
Aleksandra Bozek <aleksandra.m.bozek@gmail.com>
2024-11-06 09:58:07 +0100

    review fixes pt 3

--- src/bundle/Resources/public/js/scripts/autogenerate.identifier.js
+++ src/bundle/Resources/public/js/scripts/autogenerate.identifier.js
@@ -1,14 +1,14 @@
 (function (doc, ibexa) {
     const sourceInputs = doc.querySelectorAll('[data-autogenerate-identifier-target-selector]');
 
-    const initAutogenerator = (elements, shouldAutogenerateValue = false) => {
+    const initAutogenerator = (elements, shouldAutogenerateValue) => {
         elements.forEach((sourceInput) => {
             const { autogenerateIdentifierTargetSelector } = sourceInput.dataset;
             const targetInput = doc.querySelector(autogenerateIdentifierTargetSelector);
             const identifierAutogenerator = new ibexa.core.SlugValueInputAutogenerator({
                 sourceInput,
                 targetInput,
-                shouldAutogenerateValue,
+                shouldAutogenerateValue: shouldAutogenerateValue || !targetInput.value,
             });
 
             identifierAutogenerator.init();
@@ -20,9 +20,9 @@
             'ibexa-autogenerate-identifier:init',
             (event) => {
                 const { fieldNode, shouldAutogenerateValue } = event.detail;
-                const sourceInputs = fieldNode.querySelectorAll('[data-autogenerate-identifier-target-selector]');
+                const sourceFields = fieldNode.querySelectorAll('[data-autogenerate-identifier-target-selector]');
 
-                initAutogenerator(sourceInputs, shouldAutogenerateValue);
+                initAutogenerator(sourceFields, shouldAutogenerateValue);
             },
             false,
         );
