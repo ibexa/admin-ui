05e1a563d4c88d59fe142ab7dd86443a80675ea7
Aleksandra Bozek <aleksandra.m.bozek@gmail.com>
2024-11-18 12:43:41 +0100

    IBX-3456 Collapse all button support

--- src/bundle/Resources/public/js/scripts/core/collapse.js
+++ src/bundle/Resources/public/js/scripts/core/collapse.js
@@ -1,10 +1,57 @@
 (function (global, doc) {
+    let toggleAllTimeout;
+    const toggleAllBtn = doc.querySelector('.multi-collapse-btn');
+    const expandAll = toggleAllBtn?.querySelector('.ibexa-attribute-group__toggler-expand');
+    const collapseAll = toggleAllBtn?.querySelector('.ibexa-attribute-group__toggler-collapse');
+    MULTI_COLLAPSE_BODY_CLASS = '.multi-collapse';
+    let singleElementClicked = [];
+    const checkIfChangeText = () => {
+        const allGroups = doc.querySelectorAll(MULTI_COLLAPSE_BODY_CLASS);
+        if (singleElementClicked.length === allGroups.length || singleElementClicked.length === 0) {
+            collapseAll.classList.toggle('d-none');
+            expandAll.classList.toggle('d-none');
+        }
+    };
+    const toggleCollapseAllButton = () => {
+        singleElementClicked = [];
+        collapseAll.classList.toggle('d-none');
+        expandAll.classList.toggle('d-none');
+    };
+    const handleCollapseAction = (expandAction) => {
+        singleElementClicked = [];
+        doc.querySelectorAll('.ibexa-collapse').forEach((collapseNode) => {
+            const isElementCollapsed = collapseNode.classList.contains('ibexa-collapse--collapsed');
+
+            if (expandAction === isElementCollapsed) {
+                bootstrap.Collapse.getOrCreateInstance(collapseNode.querySelector('.multi-collapse')).toggle();
+            }
+        });
+    };
+
     doc.querySelectorAll('.ibexa-collapse').forEach((collapseNode) => {
         const toggleButton = collapseNode.querySelector('.ibexa-collapse__toggle-btn');
         const isCollapsed = toggleButton.classList.contains('collapsed');
 
         collapseNode.classList.toggle('ibexa-collapse--collapsed', isCollapsed);
         collapseNode.dataset.collapsed = isCollapsed;
+
+        if (toggleAllBtn) {
+            const uniqeName = toggleButton.getAttribute('data-bs-target');
+            collapseNode.addEventListener('click', (event) => {
+                event.stopPropagation();
+                const toggleIndex = singleElementClicked.findIndex((el) => el === uniqeName);
+                window.clearTimeout(toggleAllTimeout);
+                toggleAllTimeout = window.setTimeout(() => {
+                    if (toggleIndex !== -1) {
+                        singleElementClicked.splice(toggleIndex, 1);
+                        checkIfChangeText();
+                        return;
+                    }
+                    singleElementClicked.push(uniqeName);
+                    checkIfChangeText();
+                }, 200);
+            });
+        }
 
         collapseNode.addEventListener('hide.bs.collapse', (event) => {
             event.stopPropagation();
@@ -18,4 +65,17 @@
             collapseNode.dataset.collapsed = false;
         });
     });
+
+    if (toggleAllBtn) {
+        const collapseAll = toggleAllBtn.querySelector('.ibexa-attribute-group__toggler-collapse');
+        toggleAllBtn.addEventListener('click', (event) => {
+            event.stopPropagation();
+            window.clearTimeout(toggleAllTimeout);
+            toggleAllTimeout = window.setTimeout(() => {
+                isExpanding = collapseAll.classList.contains('d-none');
+                handleCollapseAction(isExpanding);
+                toggleCollapseAllButton();
+            }, 200);
+        });
+    }
 })(window, window.document);
