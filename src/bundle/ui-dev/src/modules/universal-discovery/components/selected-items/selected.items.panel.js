bd15cffeb315b74ef3dacf863a6ef0b752c258d3
tischsoic <symfiz@gmail.com>
2024-11-22 11:42:00 +0100

    IBX-9069: Initial Product Tab

--- src/bundle/ui-dev/src/modules/universal-discovery/components/selected-items/selected.items.panel.js
+++ src/bundle/ui-dev/src/modules/universal-discovery/components/selected-items/selected.items.panel.js
@@ -1,26 +1,44 @@
-import React, { useContext, useState, useEffect, useRef } from 'react';
+import React, { useContext, useState, useEffect, useRef, useMemo } from 'react';
 
 import {
     parse as parseTooltip,
     hideAll as hideAllTooltips,
 } from '@ibexa-admin-ui/src/bundle/Resources/public/js/scripts/helpers/tooltips.helper';
-import { getBootstrap, getTranslator } from '@ibexa-admin-ui/src/bundle/Resources/public/js/scripts/helpers/context.helper';
+import {
+    getBootstrap,
+    getAdminUiConfig,
+    getTranslator,
+} from '@ibexa-admin-ui/src/bundle/Resources/public/js/scripts/helpers/context.helper';
 
 import Icon from '../../../common/icon/icon';
-import SelectedLocationsItem from './selected.locations.item';
 import { createCssClassNames } from '../../../common/helpers/css.class.names';
 
-import { SelectedLocationsContext, AllowConfirmationContext } from '../../universal.discovery.module';
+import { AllowConfirmationContext, SelectedItemsContext } from '../../universal.discovery.module';
+import { CLEAR_SELECTED_ITEMS } from '../../hooks/useSelectedItemsReducer';
 
-const SelectedLocations = () => {
+const SelectedItemsPanel = () => {
     const Translator = getTranslator();
+    const adminUiConfig = getAdminUiConfig();
+    // TODO: fill dependency array
+    const itemsComponentsMap = useMemo(() => {
+        const { universalSelectItemsComponentsConfigs } = adminUiConfig.universalDiscoveryWidget;
+        const configsArray = universalSelectItemsComponentsConfigs ? [...universalSelectItemsComponentsConfigs] : [];
+
+        return configsArray.reduce((configsMap, config) => {
+            configsMap[config.itemType] = config;
+
+            return configsMap;
+        }, {});
+    }, [adminUiConfig]);
+
     const refSelectedLocations = useRef(null);
-    const [selectedLocations, dispatchSelectedLocationsAction] = useContext(SelectedLocationsContext);
+
+    const { selectedItems, dispatchSelectedItemsAction } = useContext(SelectedItemsContext);
     const allowConfirmation = useContext(AllowConfirmationContext);
     const [isExpanded, setIsExpanded] = useState(false);
     const className = createCssClassNames({
-        'c-selected-locations': true,
-        'c-selected-locations--expanded': isExpanded,
+        'c-selected-items-panel': true,
+        'c-selected-items-panel--expanded': isExpanded,
     });
     const expandLabel = Translator.trans(
         /*@Desc("Expand sidebar")*/ 'selected_locations.expand.sidebar',
@@ -35,7 +53,7 @@ const SelectedLocations = () => {
     const togglerLabel = isExpanded ? collapseLabel : expandLabel;
     const clearSelection = () => {
         hideAllTooltips(refSelectedLocations.current);
-        dispatchSelectedLocationsAction({ type: 'CLEAR_SELECTED_LOCATIONS' });
+        dispatchSelectedItemsAction({ type: CLEAR_SELECTED_ITEMS });
     };
     const toggleExpanded = () => {
         setIsExpanded(!isExpanded);
@@ -43,39 +61,39 @@ const SelectedLocations = () => {
     const renderSelectionCounter = () => {
         const selectedLabel = Translator.transChoice(
             /*@Desc("{1}%count% selected item|[2,Inf]%count% selected items")*/ 'selected_locations.selected_items',
-            selectedLocations.length,
-            { count: selectedLocations.length },
+            selectedItems.length,
+            { count: selectedItems.length },
             'ibexa_universal_discovery_widget',
         );
 
-        return <div className="c-selected-locations__selection-counter">{selectedLabel}</div>;
+        return <div className="c-selected-items-panel__selection-counter">{selectedLabel}</div>;
     };
     const renderToggleButton = () => {
         return (
             <button
                 type="button"
-                className="c-selected-locations__toggle-button btn ibexa-btn ibexa-btn--tertiary ibexa-btn--small ibexa-btn--no-text"
+                className="c-selected-items-panel__toggle-button btn ibexa-btn ibexa-btn--tertiary ibexa-btn--small ibexa-btn--no-text"
                 onClick={toggleExpanded}
                 title={togglerLabel}
                 data-tooltip-container-selector=".c-udw-tab"
             >
-                <Icon name="expand-left" extraClasses="c-selected-locations__toggle-button-icon ibexa-icon--tiny-small" />
+                <Icon name="expand-left" extraClasses="c-selected-items-panel__toggle-button-icon ibexa-icon--tiny-small" />
             </button>
         );
     };
     const renderActionButtons = () => {
         const removeLabel = Translator.transChoice(
             /*@Desc("{1}Deselect|[2,Inf]Deselect all")*/ 'selected_locations.deselect_all',
-            selectedLocations.length,
+            selectedItems.length,
             {},
             'ibexa_universal_discovery_widget',
         );
 
         return (
-            <div className="c-selected-locations__actions">
+            <div className="c-selected-items-panel__actions">
                 <button
                     type="button"
-                    className="c-selected-locations__clear-selection-button btn ibexa-btn ibexa-btn--small ibexa-btn--secondary"
+                    className="c-selected-items-panel__clear-selection-button btn ibexa-btn ibexa-btn--small ibexa-btn--secondary"
                     onClick={clearSelection}
                 >
                     {removeLabel}
@@ -89,16 +107,14 @@ const SelectedLocations = () => {
         }
 
         return (
-            <div className="c-selected-locations__items-wrapper">
+            <div className="c-selected-items-panel__items-wrapper">
                 {renderActionButtons()}
-                <div className="c-selected-locations__items-list">
-                    {selectedLocations.map((selectedLocation) => (
-                        <SelectedLocationsItem
-                            key={selectedLocation.location.id}
-                            location={selectedLocation.location}
-                            permissions={selectedLocation.permissions}
-                        />
-                    ))}
+                <div className="c-selected-items-panel__items-list">
+                    {selectedItems.map((selectedItem) => {
+                        const ItemComponent = itemsComponentsMap[selectedItem.type].component;
+
+                        return ItemComponent && <ItemComponent key={`${selectedItem.type}-${selectedItem.id}`} item={selectedItem} />;
+                    })}
                 </div>
             </div>
         );
@@ -113,7 +129,7 @@ const SelectedLocations = () => {
         hideAllTooltips();
 
         const bootstrap = getBootstrap();
-        const toggleButtonTooltip = bootstrap.Tooltip.getOrCreateInstance('.c-selected-locations__toggle-button');
+        const toggleButtonTooltip = bootstrap.Tooltip.getOrCreateInstance('.c-selected-items-panel__toggle-button');
 
         toggleButtonTooltip.setContent({ '.tooltip-inner': togglerLabel });
     }, [isExpanded]);
@@ -124,7 +140,7 @@ const SelectedLocations = () => {
 
     return (
         <div className={className} ref={refSelectedLocations}>
-            <div className="c-selected-locations__header">
+            <div className="c-selected-items-panel__header">
                 {renderToggleButton()}
                 {renderSelectionCounter()}
             </div>
@@ -133,4 +149,4 @@ const SelectedLocations = () => {
     );
 };
 
-export default SelectedLocations;
+export default SelectedItemsPanel;
