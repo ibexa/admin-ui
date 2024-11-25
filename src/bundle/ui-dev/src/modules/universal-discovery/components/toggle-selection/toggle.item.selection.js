169521a17d61b0d67c9af8d5af61f8e17541bcbb
tischsoic <symfiz@gmail.com>
2024-11-25 09:38:08 +0100

    Items selection adjustment

--- src/bundle/ui-dev/src/modules/universal-discovery/components/toggle-selection/toggle.item.selection.js
+++ src/bundle/ui-dev/src/modules/universal-discovery/components/toggle-selection/toggle.item.selection.js
@@ -2,29 +2,41 @@ import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 
 import { createCssClassNames } from '../../../common/helpers/css.class.names';
-import { SelectedItemsContext } from '../../universal.discovery.module';
+import { MultipleConfigContext, SelectedItemsContext } from '../../universal.discovery.module';
 
-const ToggleItemSelection = ({ multiple, item, isHidden }) => {
+const ToggleItemSelection = ({ item, isDisabled, isHidden }) => {
     const { selectedItems } = useContext(SelectedItemsContext);
+    const [multiple, multipleItemsLimit] = useContext(MultipleConfigContext);
     const isSelected = selectedItems.some((selectedItem) => selectedItem.type === item.type && selectedItem.id === item.id);
+    const isSelectionBlocked = multipleItemsLimit !== 0 && selectedItems.length >= multipleItemsLimit && !isSelected;
     const className = createCssClassNames({
         'c-udw-toggle-selection ibexa-input': true,
         'ibexa-input--checkbox': multiple,
+        'ibexa-input--radio': !multiple,
         'c-udw-toggle-selection--hidden': isHidden,
     });
     const inputType = multiple ? 'checkbox' : 'radio';
 
-    return <input type={inputType} className={className} checked={isSelected} disabled={isHidden} readOnly={true} />;
+    return (
+        <input
+            type={inputType}
+            className={className}
+            checked={isSelected}
+            disabled={isSelectionBlocked || isDisabled || isHidden}
+            readOnly={true}
+        />
+    );
 };
 
 ToggleItemSelection.propTypes = {
     item: PropTypes.object.isRequired,
-    multiple: PropTypes.bool.isRequired,
     isHidden: PropTypes.bool,
+    isDisabled: PropTypes.bool,
 };
 
 ToggleItemSelection.defaultProps = {
     isHidden: false,
+    isDisabled: false,
 };
 
 export default ToggleItemSelection;
