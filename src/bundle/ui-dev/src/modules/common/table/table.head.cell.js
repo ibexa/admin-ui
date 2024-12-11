8126e07c9a918d76700dc34ff349cc748cf6d19b
tischsoic <symfiz@gmail.com>
2024-12-11 10:28:09 +0100

    Add common Table components in React

---
+++ src/bundle/ui-dev/src/modules/common/table/table.head.cell.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { createCssClassNames } from '../helpers/css.class.names';
+
+const TableHeadCell = ({
+    extraClasses,
+    wrapperExtraClasses,
+    children,
+    sortColumnName,
+    hasCheckbox,
+    hasIcon,
+    isCloseLeft,
+    isCenterContent,
+}) => {
+    const className = createCssClassNames({
+        'ibexa-table__header-cell': true,
+        'ibexa-table__header-cell--has-checkbox': hasCheckbox,
+        'ibexa-table__header-cell--has-icon': hasIcon,
+        'ibexa-table__header-cell--close-left': isCloseLeft,
+        'ibexa-table__header-cell--content-center': isCenterContent,
+        [extraClasses]: true,
+    });
+    const cellTextWrapperClassName = createCssClassNames({
+        'ibexa-table__header-cell-text-wrapper': true,
+        [`ibexa-table__sort-column--${sortColumnName}`]: sortColumnName,
+        [wrapperExtraClasses]: true,
+    });
+
+    return (
+        <th className={className}>
+            <span className={cellTextWrapperClassName}>{children}</span>
+        </th>
+    );
+};
+
+TableHeadCell.propTypes = {
+    extraClasses: PropTypes.string,
+    wrapperExtraClasses: PropTypes.string,
+    children: PropTypes.element,
+    sortColumnName: PropTypes.string,
+    hasCheckbox: PropTypes.bool,
+    hasIcon: PropTypes.bool,
+    isCloseLeft: PropTypes.bool,
+    isCenterContent: PropTypes.bool,
+};
+
+TableHeadCell.defaultProps = {
+    extraClasses: '',
+    wrapperExtraClasses: '',
+    children: null,
+    sortColumnName: null,
+    hasCheckbox: false,
+    hasIcon: false,
+    isCloseLeft: false,
+    isCenterContent: false,
+};
+
+export default TableHeadCell;
