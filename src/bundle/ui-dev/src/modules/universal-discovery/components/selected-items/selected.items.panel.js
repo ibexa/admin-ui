c86d1662f47ac9b85f22f20302807cc45e6d5e78
Jakub Brzegowski <symfiz@gmail.com>
2024-12-20 15:17:10 +0100

    IBX-9069: Initial Product Tab (#1397)

---
+++ src/bundle/ui-dev/src/modules/universal-discovery/components/selected-items/selected.items.panel.js
@@ -0,0 +1,154 @@
+import React, { useContext, useState, useEffect, useRef, useMemo } from 'react';
+
+import {
+    parse as parseTooltip,
+    hideAll as hideAllTooltips,
+} from '@ibexa-admin-ui/src/bundle/Resources/public/js/scripts/helpers/tooltips.helper';
+import {
+    getBootstrap,
+    getAdminUiConfig,
+    getTranslator,
+} from '@ibexa-admin-ui/src/bundle/Resources/public/js/scripts/helpers/context.helper';
+
+import Icon from '../../../common/icon/icon';
+import { createCssClassNames } from '../../../common/helpers/css.class.names';
+
+import { AllowConfirmationContext, SelectedItemsContext } from '../../universal.discovery.module';
+
+import { CLEAR_SELECTED_ITEMS } from '../../hooks/useSelectedItemsReducer';
+
+const SelectedItemsPanel = () => {
+    const Translator = getTranslator();
+    const adminUiConfig = getAdminUiConfig();
+    const itemsComponentsMap = useMemo(() => {
+        const { universalSelectItemsComponentsConfigs } = adminUiConfig.universalDiscoveryWidget;
+        const configsArray = universalSelectItemsComponentsConfigs ? [...universalSelectItemsComponentsConfigs] : [];
+
+        return configsArray.reduce((configsMap, config) => {
+            configsMap[config.itemType] = config;
+
+            return configsMap;
+        }, {});
+    }, [adminUiConfig]);
+    const refSelectedLocations = useRef(null);
+    const { selectedItems, dispatchSelectedItemsAction } = useContext(SelectedItemsContext);
+    const allowConfirmation = useContext(AllowConfirmationContext);
+    const [isExpanded, setIsExpanded] = useState(false);
+    const className = createCssClassNames({
+        'c-selected-items-panel': true,
+        'c-selected-items-panel--expanded': isExpanded,
+    });
+    const expandLabel = Translator.trans(
+        /*@Desc("Expand sidebar")*/ 'selected_items.expand.sidebar',
+        {},
+        'ibexa_universal_discovery_widget',
+    );
+    const collapseLabel = Translator.trans(
+        /*@Desc("Collapse sidebar")*/ 'selected_items.collapse.sidebar',
+        {},
+        'ibexa_universal_discovery_widget',
+    );
+    const togglerLabel = isExpanded ? collapseLabel : expandLabel;
+    const clearSelection = () => {
+        hideAllTooltips(refSelectedLocations.current);
+        dispatchSelectedItemsAction({ type: CLEAR_SELECTED_ITEMS });
+    };
+    const toggleExpanded = () => {
+        setIsExpanded(!isExpanded);
+    };
+    const renderSelectionCounter = () => {
+        const selectedLabel = Translator.transChoice(
+            /*@Desc("{1}%count% selected item|[2,Inf]%count% selected items")*/ 'selected_items.selection_info',
+            selectedItems.length,
+            { count: selectedItems.length },
+            'ibexa_universal_discovery_widget',
+        );
+
+        return <div className="c-selected-items-panel__selection-counter">{selectedLabel}</div>;
+    };
+    const renderToggleBtn = () => {
+        return (
+            <button
+                type="button"
+                className="c-selected-items-panel__toggle-button btn ibexa-btn ibexa-btn--tertiary ibexa-btn--small ibexa-btn--no-text"
+                onClick={toggleExpanded}
+                title={togglerLabel}
+                data-tooltip-container-selector=".c-udw-tab"
+            >
+                <Icon name="expand-left" extraClasses="c-selected-items-panel__toggle-button-icon ibexa-icon--tiny-small" />
+            </button>
+        );
+    };
+    const renderActionBtns = () => {
+        const removeLabel = Translator.transChoice(
+            /*@Desc("{1}Deselect|[2,Inf]Deselect all")*/ 'selected_items.deselect_all',
+            selectedItems.length,
+            {},
+            'ibexa_universal_discovery_widget',
+        );
+
+        return (
+            <div className="c-selected-items-panel__actions">
+                <button
+                    type="button"
+                    className="c-selected-items-panel__clear-selection-button btn ibexa-btn ibexa-btn--small ibexa-btn--secondary"
+                    onClick={clearSelection}
+                >
+                    {removeLabel}
+                </button>
+            </div>
+        );
+    };
+    const renderLocationsList = () => {
+        if (!isExpanded) {
+            return null;
+        }
+
+        return (
+            <div className="c-selected-items-panel__items-wrapper">
+                {renderActionBtns()}
+                <div className="c-selected-items-panel__items-list">
+                    {selectedItems.map((selectedItem) => {
+                        const ItemComponent = itemsComponentsMap[selectedItem.type].component;
+
+                        if (!ItemComponent) {
+                            throw new Error(`SelectedItemsPanel: component for ${selectedItem.type} not provided in configuration.`);
+                        }
+
+                        return <ItemComponent key={`${selectedItem.type}-${selectedItem.id}`} item={selectedItem} />;
+                    })}
+                </div>
+            </div>
+        );
+    };
+
+    useEffect(() => {
+        if (!allowConfirmation) {
+            return;
+        }
+
+        parseTooltip(refSelectedLocations.current);
+        hideAllTooltips();
+
+        const bootstrap = getBootstrap();
+        const toggleBtnTooltip = bootstrap.Tooltip.getOrCreateInstance('.c-selected-items-panel__toggle-button');
+
+        toggleBtnTooltip.setContent({ '.tooltip-inner': togglerLabel });
+    }, [isExpanded]);
+
+    if (!allowConfirmation) {
+        return null;
+    }
+
+    return (
+        <div className={className} ref={refSelectedLocations}>
+            <div className="c-selected-items-panel__header">
+                {renderToggleBtn()}
+                {renderSelectionCounter()}
+            </div>
+            {renderLocationsList()}
+        </div>
+    );
+};
+
+export default SelectedItemsPanel;
