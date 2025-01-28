d7a33fe4eb2571d9e53986f830fd2f4b9e777312
Michał Grabowski <michal.grabowski@ibexa.co>
2025-01-28 14:20:53 +0100

    fix scrollbar issues

--- src/bundle/Resources/public/js/scripts/admin.input.text.js
+++ src/bundle/Resources/public/js/scripts/admin.input.text.js
@@ -1,5 +1,6 @@
 (function (global, doc) {
     const INPUT_PADDING = 12;
+    const TEXTAREA_SCROLLBAR_PADDING = 6;
     const togglePasswordVisibility = (event) => {
         const passwordTogglerBtn = event.currentTarget;
         const passwordShowIcon = passwordTogglerBtn.querySelector('.ibexa-input-text-wrapper__password-show');
@@ -40,6 +41,7 @@
         inputClearBtns.forEach((clearBtn) => clearBtn.addEventListener('click', clearText, false));
         passwordTogglerBtns.forEach((passwordTogglerBtn) => passwordTogglerBtn.addEventListener('click', togglePasswordVisibility, false));
         recalculateStyling();
+        attachListenersToMultilineInputs();
     };
     const recalculateInputStyling = (inputActionsContainer) => {
         const textWrapper = inputActionsContainer.closest('.ibexa-input-text-wrapper');
@@ -65,6 +67,30 @@
             inputActionsContainerObserver.observe(inputActionsContainer);
             recalculateInputStyling(inputActionsContainer);
         });
+    };
+    const attachListenersToMultilineInputs = () => {
+        const multilineInputWrappers = doc.querySelectorAll('.ibexa-input-text-wrapper.ibexa-input-text-wrapper--multiline');
+
+        multilineInputWrappers.forEach((multilineInputWrapper) => {
+            const textareaComponent = multilineInputWrapper.querySelector('.ibexa-input--textarea');
+
+            const textareaComponentObserver = new ResizeObserver(() => {
+                toggleScrollbarStyles(multilineInputWrapper);
+            });
+
+            textareaComponentObserver.observe(textareaComponent);
+            toggleScrollbarStyles(multilineInputWrapper);
+        });
+    };
+    const toggleScrollbarStyles = (multilineInputWrapper) => {
+        const textareaComponent = multilineInputWrapper.querySelector('textarea');
+        const { scrollHeight, clientHeight, offsetWidth, clientWidth } = textareaComponent;
+        const { overflowY } = global.getComputedStyle(textareaComponent);
+        const hasScrollbar = overflowY !== 'hidden' && scrollHeight > clientHeight;
+        const scrollbarWidth = offsetWidth - clientWidth;
+
+        multilineInputWrapper.classList.toggle('ibexa-input-text-wrapper--scrollbar-visible', hasScrollbar);
+        multilineInputWrapper.style.setProperty('--scrollbar-width', `${scrollbarWidth + TEXTAREA_SCROLLBAR_PADDING}px`);
     };
 
     doc.body.addEventListener('ibexa-inputs:added', attachListenersToAllInputs, false);
