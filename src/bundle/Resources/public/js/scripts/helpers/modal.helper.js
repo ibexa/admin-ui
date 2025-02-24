8fa59faf5bf54080aeec095da306072ff4837f39
Michał Grabowski <michal.grabowski@ibexa.co>
2025-02-24 13:03:16 +0100

    IBX-8316: Modal for taxonomy entry assignment field displaying not perfectly with page builder

--- src/bundle/Resources/public/js/scripts/helpers/modal.helper.js
+++ src/bundle/Resources/public/js/scripts/helpers/modal.helper.js
@@ -1,14 +1,42 @@
-const controlZIndex = (container) => {
+const controlZIndex = (container, listenerContainer, resetedZIndex = 'initial') => {
     const initialZIndex = container.style.zIndex;
-
-    container.addEventListener('show.bs.modal', () => {
-        container.style.zIndex = 'initial';
-    });
-    container.addEventListener('hide.bs.modal', () => {
+    const finalListenerContainer = listenerContainer ?? container;
+    const handleShowModal = () => {
+        container.style.zIndex = resetedZIndex;
+    };
+    const handleCloseModal = () => {
         container.style.zIndex = initialZIndex;
-    });
+    };
+    const removeControlZIndexListeners = () => {
+        finalListenerContainer.removeEventListener('show.bs.modal', handleShowModal, false);
+        finalListenerContainer.removeEventListener('hidden.bs.modal', handleCloseModal, false);
+
+        document.body.dispatchEvent(new CustomEvent('ibexa-control-z-index:events-detached'));
+    };
+
+    finalListenerContainer.addEventListener('show.bs.modal', handleShowModal, false);
+    finalListenerContainer.addEventListener('hidden.bs.modal', handleCloseModal, false);
 
     document.body.dispatchEvent(new CustomEvent('ibexa-control-z-index:events-attached'));
+
+    return {
+        removeControlZIndexListeners,
+    };
 };
 
-export { controlZIndex };
+const controlZIndexBulk = (items) => {
+    const storedControlZIndex = items.map((item) => {
+        return controlZIndex(...item);
+    });
+    const removeControlZIndexListeners = () => {
+        storedControlZIndex.forEach((item) => {
+            item.removeControlZIndexListeners();
+        });
+    };
+
+    return {
+        removeControlZIndexListeners,
+    };
+};
+
+export { controlZIndex, controlZIndexBulk };
