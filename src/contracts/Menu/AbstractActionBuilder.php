2dcc8d9139bea05262a8daf46ea1ea61cb378285
Tomasz Kryszan <tomasz.kryszan@ibexa.co>
2025-03-10 12:54:41 +0100

    Reworked AbstractActionBuilder

--- src/contracts/Menu/AbstractActionBuilder.php
+++ src/contracts/Menu/AbstractActionBuilder.php
@@ -7,30 +7,48 @@
 
 namespace Ibexa\Contracts\AdminUi\Menu;
 
+use Ibexa\AdminUi\Specification\ContentIsUser;
+use Ibexa\Contracts\Core\Repository\ContentService;
+use Ibexa\Contracts\Core\Repository\UserService;
+use Ibexa\Contracts\Core\Repository\Values\Content\VersionInfo;
 use Knp\Menu\FactoryInterface;
 use Knp\Menu\ItemInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 abstract class AbstractActionBuilder extends AbstractBuilder
 {
+    protected const TRANSLATION_DOMAIN = 'ibexa_action_menu';
+    private const ICON_EDIT = 'edit';
+    private const ORDER_NUMBER = 10;
+
+    protected TranslatorInterface $translator;
+
+    private ContentService $contentService;
+
     private FactoryInterface $menuItemFactory;
 
-    private TranslatorInterface $translator;
+    private UrlGeneratorInterface $urlGenerator;
 
-    protected ContentAwareActionItemFactoryInterface $contentAwareActionItemFactory;
+    private UserService $userService;
 
     public function __construct(
-        MenuItemFactoryInterface $menuItemFactory,
+        MenuItemFactoryInterface $factory,
         EventDispatcherInterface $eventDispatcher,
-        ContentAwareActionItemFactoryInterface $contentAwareActionItemFactory,
-        TranslatorInterface $translator
+        ContentService $contentService,
+        FactoryInterface $menuFactory,
+        TranslatorInterface $translator,
+        UrlGeneratorInterface $urlGenerator,
+        UserService $userService
     ) {
-        parent::__construct($menuItemFactory, $eventDispatcher);
+        parent::__construct($factory, $eventDispatcher);
 
-        $this->menuItemFactory = $menuItemFactory;
+        $this->contentService = $contentService;
+        $this->menuItemFactory = $menuFactory;
         $this->translator = $translator;
-        $this->contentAwareActionItemFactory = $contentAwareActionItemFactory;
+        $this->urlGenerator = $urlGenerator;
+        $this->userService = $userService;
     }
 
     /**
@@ -39,22 +57,78 @@ abstract class AbstractActionBuilder extends AbstractBuilder
     protected function createActionItem(string $name, array $options = []): ItemInterface
     {
         if (empty($options['extras']['translation_domain'])) {
-            $options['extras']['translation_domain'] = 'ibexa_action_menu';
-        }
-
-        if (empty($options['label'])) {
-            $options['label'] = $this->createLabel($name);
+            $options['extras']['translation_domain'] = self::TRANSLATION_DOMAIN;
         }
 
         return $this->menuItemFactory->createItem($name, $options);
     }
 
-    protected function createLabel(string $name): string
+    /**
+     * @param array<string, mixed> $parameters
+     */
+    public function createEditDraftAction(
+        VersionInfo $versionInfo,
+        string $name,
+        array $parameters = [],
+        bool $isDraftConflict = false,
+        ?int $locationId = null
+    ): ItemInterface {
+        $btnClass = 'ibexa-btn--content-draft-edit';
+
+        if (isset($parameters['attributes']['class'])) {
+            $btnClass .= ' ' . $parameters['attributes']['class'];
+        }
+
+        $parameters['uri'] = $isDraftConflict ? $this->generateDraftEditUrl($versionInfo, $locationId) : null;
+        $parameters['attributes']['class'] = $btnClass;
+        $parameters['attributes']['data-content-id'] = $versionInfo->getContentInfo()->getId();
+        $parameters['attributes']['data-language-code'] = $versionInfo->getInitialLanguage()->getLanguageCode();
+        $parameters['attributes']['data-version-has-conflict-url'] = $this->generateVersionHasConflictUrl($versionInfo);
+        $parameters['attributes']['data-content-draft-edit-url'] = $this->generateDraftEditUrl($versionInfo);
+
+        $parameters['extras']['icon'] = $parameters['extras']['icon'] ?? self::ICON_EDIT;
+        $parameters['extras']['orderNumber'] = $parameters['extras']['orderNumber'] ?? self::ORDER_NUMBER;
+
+        return $this->createActionItem($name, $parameters);
+    }
+
+    private function generateVersionHasConflictUrl(VersionInfo $versionInfo): string
     {
-        return $this->translator->trans(
-            $name,
-            [],
-            'ibexa_action_menu'
+        return $this->urlGenerator->generate(
+            'ibexa.version.has_no_conflict',
+            [
+                'contentId' => $versionInfo->getContentInfo()->getId(),
+                'versionNo' => $versionInfo->getVersionNo(),
+                'languageCode' => $versionInfo->getInitialLanguage()->getLanguageCode(),
+            ]
         );
+    }
+
+    private function generateDraftEditUrl(
+        VersionInfo $versionInfo,
+        ?int $locationId = null
+    ): string {
+        $routeName = 'ibexa.content.draft.edit';
+
+        if ($this->isUserBased($versionInfo)) {
+            $routeName = 'ibexa.user.update';
+        }
+
+        return $this->urlGenerator->generate(
+            $routeName,
+            [
+                'contentId' => $versionInfo->getContentInfo()->getId(),
+                'versionNo' => $versionInfo->getVersionNo(),
+                'language' => $versionInfo->getInitialLanguage()->getLanguageCode(),
+                'locationId' => $locationId,
+            ]
+        );
+    }
+
+    private function isUserBased(VersionInfo $versionInfo): bool
+    {
+        $content = $this->contentService->loadContentByVersionInfo($versionInfo);
+
+        return (new ContentIsUser($this->userService))->isSatisfiedBy($content);
     }
 }
