d90f49226f6245030c0d95c167a023f88b1ecee5
Dawid Parafinski <dawid.parafinski@ibexa.co>
2025-03-20 12:12:23 +0100

    Implemented Sorting by moving to location filtering API

--- src/bundle/Controller/Location/LoadSubItemsController.php
+++ src/bundle/Controller/Location/LoadSubItemsController.php
@@ -13,30 +13,90 @@ use Ibexa\AdminUi\REST\Value\SubItems\Owner;
 use Ibexa\AdminUi\REST\Value\SubItems\SubItem;
 use Ibexa\AdminUi\REST\Value\SubItems\SubItemList;
 use Ibexa\AdminUi\REST\Value\SubItems\Thumbnail;
+use Ibexa\Contracts\Core\Repository\Exceptions\NotImplementedException;
 use Ibexa\Contracts\Core\Repository\LocationService;
 use Ibexa\Contracts\Core\Repository\Values\Content\Location;
 use Ibexa\Contracts\Core\Repository\Values\Content\LocationList;
+use Ibexa\Contracts\Core\Repository\Values\Content\Query;
+use Ibexa\Contracts\Core\Repository\Values\Content\Query\Criterion\ParentLocationId;
+use Ibexa\Contracts\Core\Repository\Values\Content\Query\SortClause;
+use Ibexa\Contracts\Core\Repository\Values\Filter\Filter;
+use Ibexa\Contracts\Core\Repository\Values\Filter\FilteringSortClause;
+use Ibexa\Core\Base\Exceptions\InvalidArgumentException;
 use Ibexa\Rest\Server\Controller as RestController;
+use Symfony\Component\HttpFoundation\Request;
 
 final class LoadSubItemsController extends RestController
 {
+    private const SORT_CLAUSE_MAP = [
+        'ContentId' => SortClause\ContentId::class,
+        'DateModified' => SortClause\DateModified::class,
+        'LocationDepth' => SortClause\Location\Depth::class,
+        'LocationPath' => SortClause\Location\Path::class,
+        'LocationPriority' => SortClause\Location\Priority::class,
+        'SectionIdentifier' => SortClause\SectionIdentifier::class,
+        'SectionName' => SortClause\SectionName::class,
+        'DatePublished' => SortClause\DatePublished::class,
+        'ContentName' => SortClause\ContentName::class,
+    ];
+
     public function __construct(readonly private LocationService $locationService)
     {
     }
 
     public function loadAction(
+        Request $request,
         Location $location,
         int $limit,
         int $offset
     ): SubItemList {
-        $count = $this->locationService->getLocationChildCount($location);
+        $sortOrder = $request->query->getAlpha('sortOrder', Query::SORT_ASC);
 
-        $children = $this->locationService->loadLocationChildren($location, $offset, $limit);
+        $filter = new Filter(new ParentLocationId($location->getId()));
+        $filter->withLimit($limit);
+        $filter->withOffset($offset);
+
+        $sortClauses = $request->query->get('sortClause') ? [$this->buildSortClause($request->query->get('sortClause'), $sortOrder)] : $this->getDefaultSortClause($location);
+
+        foreach ($sortClauses as $sortClause) {
+            $filter->withSortClause($sortClause);
+        }
+
+        $count = $this->locationService->count($filter);
+        $children = $this->locationService->find($filter);
 
         return $this->buildSubItemsList(
             $count,
             $children
         );
+    }
+
+    /**
+     * @return \Ibexa\Contracts\Core\Repository\Values\Filter\FilteringSortClause[]
+     */
+    private function getDefaultSortClause(Location $location): array
+    {
+        try {
+            $sortClauses = $location->getSortClauses();
+        } catch (NotImplementedException $e) {
+            return [];
+        }
+
+        return array_filter($sortClauses, static fn ($sortClause) => $sortClause instanceof FilteringSortClause);
+    }
+
+    private function buildSortClause(string $sortClause, string $sortOrder): FilteringSortClause
+    {
+        if (!isset(static::SORT_CLAUSE_MAP[$sortClause])) {
+            throw new InvalidArgumentException('$sortClause', 'Invalid sort clause');
+        }
+
+        $map = static::SORT_CLAUSE_MAP;
+
+        $sortClauseInstance = new $map[$sortClause]();
+        $sortClauseInstance->direction = $sortOrder;
+
+        return $sortClauseInstance;
     }
 
     private function buildSubItemsList(int $totalCount, LocationList $childrenList): SubItemList
