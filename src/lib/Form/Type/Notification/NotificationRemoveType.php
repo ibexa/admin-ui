f0c79b058cefc5c5859b0f711360f5c0a50f8022
Tomasz Białczak <tomasz.bialczak@ibexa.co>
2025-03-26 07:03:37 +0100

    IBX-9060: Added delete action and delete multiple notifications

---
+++ src/lib/Form/Type/Notification/NotificationRemoveType.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\AdminUi\Form\Type\Notification;
+
+use Ibexa\AdminUi\Form\Data\Notification\NotificationRemoveData;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NotificationRemoveType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add(
+            'notifications',
+            CollectionType::class,
+            [
+                'entry_type' => CheckboxType::class,
+                'required' => false,
+                'allow_add' => true,
+                'entry_options' => ['label' => false],
+                'label' => false,
+            ]
+        );
+
+        $builder->add(
+            'remove',
+            SubmitType::class
+        );
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => NotificationRemoveData::class,
+        ]);
+    }
+}
