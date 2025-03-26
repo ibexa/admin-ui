6ce7874ada2f5a61807e21a50a7002ed74127467
Tomasz Białczak <tomasz.bialczak@ibexa.co>
2025-03-26 07:03:37 +0100

    IBX-9060: Added delete action and delete multiple notifications

--- src/bundle/Controller/NotificationController.php
+++ src/bundle/Controller/NotificationController.php
@@ -8,9 +8,10 @@ declare(strict_types=1);
 
 namespace Ibexa\Bundle\AdminUi\Controller;
 
+use Ibexa\AdminUi\Form\Data\Notification\NotificationRemoveData;
+use Ibexa\AdminUi\Form\Factory\FormFactory;
+use Ibexa\AdminUi\Form\SubmitHandler;
 use Ibexa\AdminUi\Pagination\Pagerfanta\NotificationAdapter;
-use Ibexa\Bundle\AdminUi\View\EzPagerfantaView;
-use Ibexa\Bundle\AdminUi\View\Template\EzPagerfantaTemplate;
 use Ibexa\Contracts\AdminUi\Controller\Controller;
 use Ibexa\Contracts\Core\Repository\NotificationService;
 use Ibexa\Contracts\Core\SiteAccess\ConfigResolverInterface;
@@ -35,16 +36,26 @@ class NotificationController extends Controller
     /** @var \Ibexa\Contracts\Core\SiteAccess\ConfigResolverInterface */
     private $configResolver;
 
+    /** @var \Ibexa\AdminUi\Form\Factory\FormFactory */
+    private $formFactory;
+
+    /** @var \Ibexa\AdminUi\Form\SubmitHandler */
+    private $submitHandler;
+
     public function __construct(
         NotificationService $notificationService,
         Registry $registry,
         TranslatorInterface $translator,
-        ConfigResolverInterface $configResolver
+        ConfigResolverInterface $configResolver,
+        FormFactory $formFactory,
+        SubmitHandler $submitHandler
     ) {
         $this->notificationService = $notificationService;
         $this->registry = $registry;
         $this->translator = $translator;
         $this->configResolver = $configResolver;
+        $this->formFactory = $formFactory;
+        $this->submitHandler = $submitHandler;
     }
 
     public function getNotificationsAction(Request $request, int $offset, int $limit): JsonResponse
@@ -76,22 +87,37 @@ class NotificationController extends Controller
         $pagerfanta->setMaxPerPage($this->configResolver->getParameter('pagination.notification_limit'));
         $pagerfanta->setCurrentPage(min($page, $pagerfanta->getNbPages()));
 
-        $notifications = '';
+        $notifications = [];
         foreach ($pagerfanta->getCurrentPageResults() as $notification) {
             if ($this->registry->hasRenderer($notification->type)) {
                 $renderer = $this->registry->getRenderer($notification->type);
-                $notifications .= $renderer->render($notification);
+                $notifications[] = $renderer->render($notification);
             }
         }
-        $notifications = $request->attributes->get('notifications', $notifications);
 
         $template = $request->attributes->get('template', '@ibexadesign/account/notifications/list.html.twig');
+
+        $deleteNotificationsForm = $this->formFactory->deleteNotification(
+            $this->createNotificationRemoveData($pagerfanta)
+        );
 
         return $this->render($template, [
             'notifications' => $notifications,
             'notifications_count_interval' => $this->configResolver->getParameter('notification_count.interval'),
             'pager' => $pagerfanta,
+            'form_remove' => $deleteNotificationsForm->createView(),
         ]);
+    }
+
+    private function createNotificationRemoveData(Pagerfanta $pagerfanta): NotificationRemoveData
+    {
+        $notificationIds = [];
+
+        foreach ($pagerfanta->getCurrentPageResults() as $notification) {
+            $notificationIds[$notification->id] = false;
+        }
+
+        return new NotificationRemoveData($notificationIds);
     }
 
     /**
@@ -164,14 +190,6 @@ class NotificationController extends Controller
 
             $data = ['status' => 'success'];
 
-            if ($this->registry->hasRenderer($notification->type)) {
-                $url = $this->registry->getRenderer($notification->type)->generateUrl($notification);
-
-                if ($url) {
-                    $data['redirect'] = $url;
-                }
-            }
-
             $response->setData($data);
         } catch (\Exception $exception) {
             $response->setData([
@@ -183,6 +201,51 @@ class NotificationController extends Controller
         }
 
         return $response;
+    }
+
+    public function deleteNotificationAction(Request $request, mixed $notificationId): JsonResponse
+    {
+        $response = new JsonResponse();
+
+        try {
+            $notification = $this->notificationService->getNotification((int)$notificationId);
+
+            $this->notificationService->deleteNotification($notification);
+
+            $response->setData(['status' => 'success']);
+        } catch (\Exception $exception) {
+            $response->setData([
+                'status' => 'failed',
+                'error' => $exception->getMessage(),
+            ]);
+
+            $response->setStatusCode(404);
+        }
+
+        return $response;
+    }
+
+    public function deleteNotificationsAction(Request $request): Response
+    {
+        $form = $this->formFactory->deleteNotification();
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $result = $this->submitHandler->handle($form, function (NotificationRemoveData $data) {
+                foreach (array_keys($data->getNotifications()) as $id) {
+                    $notification = $this->notificationService->getNotification((int)$id);
+                    $this->notificationService->deleteNotification($notification);
+                }
+
+                return $this->redirectToRoute('ibexa.notifications.render.all');
+            });
+
+            if ($result instanceof Response) {
+                return $result;
+            }
+        }
+
+        return $this->redirectToRoute('ibexa.notifications.render.all');
     }
 }
 
---
+++ src/lib/Form/Data/Notification/NotificationRemoveData.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\AdminUi\Form\Data\Notification;
+
+class NotificationRemoveData
+{
+    private array $notifications;
+
+    public function __construct(array $notifications = [])
+    {
+        $this->notifications = $notifications;
+    }
+
+    public function getNotifications(): array
+    {
+        return $this->notifications;
+    }
+
+    public function setNotifications(array $notifications): void
+    {
+        $this->notifications = $notifications;
+    }
+}
