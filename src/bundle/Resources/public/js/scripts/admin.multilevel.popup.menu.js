acd3629a25a673613c8e2b5cfae631651a43a3a7
Dariusz Szut <dew326@gmail.com>
2025-03-31 15:33:52 +0200

    IBX-9060: Fixed all list

--- src/bundle/Resources/public/js/scripts/admin.multilevel.popup.menu.js
+++ src/bundle/Resources/public/js/scripts/admin.multilevel.popup.menu.js
@@ -1,15 +1,29 @@
 (function (global, doc, ibexa) {
-    const multilevelPopupMenusContainers = doc.querySelectorAll(
-        '.ibexa-multilevel-popup-menu:not(.ibexa-multilevel-popup-menu--custom-init)',
-    );
+    const initMutlilevelPopupMenus = (container) => {
+        const multilevelPopupMenusContainers = container.querySelectorAll(
+            '.ibexa-multilevel-popup-menu:not(.ibexa-multilevel-popup-menu--custom-init)',
+        );
 
-    multilevelPopupMenusContainers.forEach((container) => {
-        const multilevelPopupMenu = new ibexa.core.MultilevelPopupMenu({
-            container,
-            triggerElement: doc.querySelector(container.dataset.triggerElementSelector),
-            initialBranchPlacement: container.dataset.initialBranchPlacement,
+        multilevelPopupMenusContainers.forEach((multilevelPopupMenusContainer) => {
+            const multilevelPopupMenu = new ibexa.core.MultilevelPopupMenu({
+                container: multilevelPopupMenusContainer,
+                triggerElement: doc.querySelector(multilevelPopupMenusContainer.dataset.triggerElementSelector),
+                initialBranchPlacement: multilevelPopupMenusContainer.dataset.initialBranchPlacement,
+            });
+
+            multilevelPopupMenu.init();
         });
+    };
 
-        multilevelPopupMenu.init();
-    });
+    initMutlilevelPopupMenus(doc);
+
+    doc.body.addEventListener(
+        'ibexa-multilevel-popup-menu:init',
+        (event) => {
+            const { container } = event.detail;
+
+            initMutlilevelPopupMenus(container);
+        },
+        false,
+    );
 })(window, window.document, window.ibexa);
