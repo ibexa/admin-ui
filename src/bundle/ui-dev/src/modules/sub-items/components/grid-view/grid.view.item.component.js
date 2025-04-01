319f3bd6d126bb3347a129798d3d14f7d7e76eb1
Lukasz Ostafin <ostafin.lucas@gmail.com>
2025-04-01 10:29:55 +0200

    Grid view adjustments

--- src/bundle/ui-dev/src/modules/sub-items/components/grid-view/grid.view.item.component.js
+++ src/bundle/ui-dev/src/modules/sub-items/components/grid-view/grid.view.item.component.js
@@ -1,31 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Icon from '../../../common/icon/icon';
+import { createCssClassNames } from '../../../common/helpers/css.class.names';
+
 import { getContentTypeIconUrl, getContentTypeName } from '@ibexa-admin-ui-helpers/content.type.helper';
 
 const GridViewItemComponent = ({ item, generateLink }) => {
     const { id: locationId, contentInfo, contentThumbnail, contentType } = item;
-    const imageClassName = 'ibexa-grid-view-item__image';
     const contentTypeIdentifier = contentType.ContentType.identifier;
     const contentTypeIconUrl = getContentTypeIconUrl(contentTypeIdentifier);
     const contentTypeName = getContentTypeName(contentTypeIdentifier);
-    let image = null;
+    const isSVGThumbnail = !contentThumbnail.Thumbnail || contentThumbnail.Thumbnail.mimeType === 'image/svg+xml';
+    const thumbnailClassName = createCssClassNames({
+        'ibexa-grid-view-item__image': true,
+        'ibexa-grid-view-item__image--none': isSVGThumbnail,
+    });
+    const renderThumbnail = () => {
+        if (isSVGThumbnail) {
+            return (
+                <div className={thumbnailClassName}>
+                    <Icon customPath={contentTypeIconUrl} extraClasses="ibexa-icon--extra-large" />
+                </div>
+            );
+        }
 
-    if (!contentThumbnail.Thumbnail || contentThumbnail.Thumbnail.mimeType === 'image/svg+xml') {
-        image = (
-            <div className={`${imageClassName} ${imageClassName}--none`}>
-                <Icon customPath={contentTypeIconUrl} extraClasses="ibexa-icon--extra-large" />
-            </div>
-        );
-    } else {
-        const { uri, alternativeText } = contentThumbnail.Thumbnail;
-
-        image = <img className={imageClassName} src={uri} alt={alternativeText} />;
+        return <img className={contentThumbnail.Thumbnail.uri} src={uri} alt={contentInfo.ContentInfo.name} />;
     }
 
     return (
         <a className="ibexa-grid-view-item" href={generateLink(locationId, contentInfo.ContentInfo.id)}>
-            <div className="ibexa-grid-view-item__image-wrapper">{image}</div>
+            <div className="ibexa-grid-view-item__image-wrapper">{renderThumbnail()}</div>
             <div className="ibexa-grid-view-item__footer">
                 <div className="ibexa-grid-view-item__title" title={contentInfo.ContentInfo.name}>
                     {contentInfo.ContentInfo.name}
