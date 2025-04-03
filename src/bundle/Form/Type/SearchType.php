b86a717e070016560839bc5196aed2c570c04f0b
Tomasz Białczak <tomasz.bialczak@ibexa.co>
2025-04-03 13:46:03 +0200

    IBX-9060: Added search form and query support to notifications page

---
+++ src/bundle/Form/Type/SearchType.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Bundle\AdminUi\Form\Type;
+
+use Ibexa\Bundle\AdminUi\Form\Data\SearchQueryData;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+final class SearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('query', TextType::class, [
+            'required' => false,
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'method' => 'GET',
+            'csrf_protection' => false,
+            'data_class' => SearchQueryData::class,
+        ]);
+    }
+}
