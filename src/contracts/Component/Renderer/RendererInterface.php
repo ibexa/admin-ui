9411c5abc7c44e4ad7184114ad8c5001135c798c
MateuszKolankowski <wiewiurdp@tlen. pl>
2025-04-09 14:17:48 +0200

    Refactored components to align with new twig-components package

--- src/contracts/Component/Renderer/RendererInterface.php
+++ src/contracts/Component/Renderer/RendererInterface.php
@@ -8,6 +8,9 @@ declare(strict_types=1);
 
 namespace Ibexa\Contracts\AdminUi\Component\Renderer;
 
+/**
+ * @deprecated use {@see \Ibexa\Contracts\TwigComponents\Renderer\RendererInterface}
+ */
 interface RendererInterface
 {
     public function renderGroup(string $groupName, array $parameters = []): array;
--- src/lib/Component/LinkComponent.php
+++ src/lib/Component/LinkComponent.php
@@ -8,10 +8,13 @@ declare(strict_types=1);
 
 namespace Ibexa\AdminUi\Component;
 
-use Ibexa\Contracts\AdminUi\Component\Renderable;
+use Ibexa\Contracts\TwigComponents\ComponentInterface;
 use Twig\Environment;
 
-class LinkComponent implements Renderable
+/**
+ * @deprecated use {@see \Ibexa\TwigComponents\Component\LinkComponent}
+ */
+class LinkComponent implements ComponentInterface
 {
     /** @var \Twig\Environment */
     protected $twig;
