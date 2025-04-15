bfcd61e7a56d816e7d9256a3bcdbe93619b804c6
Sztig <sebastianjamula@gmail.com>
2025-04-15 15:19:14 +0200

    fixed edit permission check

--- src/bundle/Controller/Content/ContentTreeController.php
+++ src/bundle/Controller/Content/ContentTreeController.php
@@ -226,7 +226,7 @@ class ContentTreeController extends RestController
                 'restrictedLanguageCodes' => $createLimitationsValues[Limitation::LANGUAGE],
             ],
             'edit' => [
-                'hasAccess' => $lookupUpdateLimitationsResult->hasAccess(),
+                'hasAccess' => $this->canUserEditContent($location),
                 // skipped content type limitation values as in this case it can be inferred from "hasAccess" above
                 'restrictedLanguageCodes' => $updateLimitationsValues[Limitation::LANGUAGE],
             ],
@@ -324,6 +324,21 @@ class ContentTreeController extends RestController
         );
 
         return !empty($siteAccesses);
+    }
+
+    /**
+     * @throws \Ibexa\Contracts\Core\Repository\Exceptions\BadStateException
+     * @throws \Ibexa\Contracts\Core\Repository\Exceptions\InvalidArgumentException
+     */
+    private function canUserEditContent(Location $location): bool
+    {
+        $content = $location->getContent();
+
+        return $this->permissionResolver->canUser(
+            'content',
+            'edit',
+            $content
+        );
     }
 }
 
