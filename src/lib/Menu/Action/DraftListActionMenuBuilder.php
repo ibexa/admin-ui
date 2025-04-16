597110522fade1250c2078cda5fc7c8b50543ba5
Tomasz Kryszan <tomasz.kryszan@ez.no>
2025-04-16 11:11:52 +0200

    IBX-9619: Reworked action list (#1480)
    * Added DraftListActionMenuBuilder
    
    * [DI] Added service definition
    
    * Added const with event identifier
    
    * [Translations] Extracted translations
    
    * Added action_list.html.twig
    
    * Used KNP menu to render actions for draft list
    
    * Fixed cs
    
    * Updated action_list twig
    
    * Fixed cs
    
    * Added ContentAwareActionItemFactory
    
    * Added AbstractActionBuilder
    
    * Removed CONTENT_DRAFT_LIST_ACTION const from ConfigureMenuEvent
    
    * Reworked DraftListActionMenuBuilder
    
    * Added VersionListActionMenuBuilder
    
    * Reworked table template for version list
    
    * Added support for own classess in action_list.html.twig
    
    * Dropped extra comma from AbstracActionBuilder constructor
    
    * Dropped ContentAwareActionItemFactory
    
    * Reworked AbstractActionBuilder
    
    * [DI] dropped service definition
    
    * Extracted translations
    
    * Fixed rendering action list for published content
    
    * Marked AbstractActionBuilder::createEditDraftAction as protected
    
    * Fixed action items order
    
    * Added missing declare strict_types
    
    * Dropped Traslator property and added locationId to edit route
    
    * Fixed exception message - added debug type
    
    * [Tests] Added unit tests
    
    * CR remarks
    
    * [Behat] Changed pageTitle locator
    
    * Extracted draftEditUrl to variable
    
    * Reworked AbstractActionBuilder::createEditDraftAction
    
    * Reworked AbstractActionBuilder
    
    * Reordered properties in AbstractActionBuilder
    
    ---------
    
    Co-authored-by: Lukasz Ostafin <ostafin.lucas@gmail.com>
    Co-authored-by: barbaragr <barbara.grajczyk@ez.no>

---
+++ src/lib/Menu/Action/DraftListActionMenuBuilder.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\AdminUi\Menu\Action;
+
+use Ibexa\Contracts\AdminUi\Menu\AbstractActionBuilder;
+use Ibexa\Contracts\Core\Exception\InvalidArgumentException;
+use Ibexa\Contracts\Core\Repository\Values\Content\VersionInfo;
+use JMS\TranslationBundle\Model\Message;
+use JMS\TranslationBundle\Translation\TranslationContainerInterface;
+use Knp\Menu\ItemInterface;
+
+final class DraftListActionMenuBuilder extends AbstractActionBuilder implements TranslationContainerInterface
+{
+    public const ITEM_EDIT_DRAFT = 'draft_list__action__content_edit';
+
+    protected function getConfigureEventName(): string
+    {
+        return self::class;
+    }
+
+    /**
+     * @param array<string, mixed> $options
+     *
+     * @throws \Ibexa\Contracts\Core\Repository\Exceptions\InvalidArgumentException
+     */
+    protected function createStructure(array $options): ItemInterface
+    {
+        $versionInfo = $options['versionInfo'];
+        if (!$versionInfo instanceof VersionInfo) {
+            throw new InvalidArgumentException(
+                '$versionInfo',
+                sprintf(
+                    'Version info expected to be of type "%s" but got "%s"',
+                    VersionInfo::class,
+                    get_debug_type($versionInfo)
+                )
+            );
+        }
+
+        $menu = $this->createActionItem('root_action_list');
+
+        $parameters['label'] = $this->translator->trans(
+            self::ITEM_EDIT_DRAFT,
+            [],
+            self::TRANSLATION_DOMAIN
+        );
+        $parameters['attributes']['class'] = self::IBEXA_BTN_CONTENT_DRAFT_EDIT_CLASS;
+
+        $contentEditDraftAction = $this->createEditDraftButtonAction(
+            $versionInfo,
+            self::ITEM_EDIT_DRAFT,
+            $parameters
+        );
+
+        $menu->addChild($contentEditDraftAction);
+
+        return $menu;
+    }
+
+    public static function getTranslationMessages(): array
+    {
+        return [
+            Message::create(self::ITEM_EDIT_DRAFT, 'ibexa_action_menu')->setDesc('Edit'),
+        ];
+    }
+}
