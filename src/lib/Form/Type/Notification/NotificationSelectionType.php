566e5bee3383becf8b44fa45a9a271f5bde0c7af
Tomasz Białczak <tomasz.bialczak@ibexa.co>
2025-04-29 13:08:24 +0200

    IBX-9060: Added submit buttons to notification selection forms

--- src/lib/Form/Type/Notification/NotificationSelectionType.php
+++ src/lib/Form/Type/Notification/NotificationSelectionType.php
@@ -12,7 +12,6 @@ use Ibexa\AdminUi\Form\Data\Notification\NotificationSelectionData;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
