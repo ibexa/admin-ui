51142720d1330b8d3bef71bd7e58e4f333a65269
Tomasz Białczak <tomasz.bialczak@ibexa.co>
2025-05-20 13:18:48 +0200

    IBX-9060: Specified iterable value types and generic return types to satisfy PHPStan

--- src/bundle/Form/Type/SearchType.php
+++ src/bundle/Form/Type/SearchType.php
@@ -15,6 +15,9 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+/**
+ * @extends AbstractType<SearchQueryData>
+ */
 final class SearchType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
