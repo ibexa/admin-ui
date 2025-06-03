3a3ffa2232a225748de000fcde5360f0f362c7ce
MateuszKolankowski <wiewiurdp@tlen. pl>
2025-06-03 15:19:01 +0200

    Add normalization of keys for icon_aliases configuration

--- src/bundle/DependencyInjection/Configuration/Parser/Assets.php
+++ src/bundle/DependencyInjection/Configuration/Parser/Assets.php
@@ -50,6 +50,7 @@ class Assets extends AbstractParser
                     ->end()
                     ->arrayNode('icon_aliases')
                         ->useAttributeAsKey('name')
+                        ->normalizeKeys(false)
                         ->scalarPrototype()->end()
                     ->end()
                 ->end()
