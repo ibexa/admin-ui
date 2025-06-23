9b18ae8a7c73fe463a08c5e3c60301f2a2448cca
MateuszKolankowski <wiewiurdp@tlen. pl>
2025-06-23 12:55:20 +0200

    Adding unit tests for TrashItemData

---
+++ tests/lib/Form/Data/TrashItemDataTest.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+use Ibexa\AdminUi\Form\Data\TrashItemData;
+use Ibexa\Core\Repository\Values\Content\Location;
+use Ibexa\Core\Repository\Values\Content\TrashItem;
+use PHPUnit\Framework\TestCase;
+
+class TrashItemDataTest extends TestCase
+{
+    public function testIsParentInTrashReturnsFalseWhenNoAncestors(): void
+    {
+        $trashItem = new TrashItem(['path' => ['1', '2', '3'], 'id' => 3]);
+        $data = new TrashItemData($trashItem, null, []);
+
+        self::assertFalse($data->isParentInTrash());
+    }
+
+    public function testIsParentInTrashReturnsFalseWhenPathsMatch(): void
+    {
+        $trashItem = new TrashItem(['path' => ['1', '2', '3'], 'id' => 3]);
+
+        $ancestor = new Location(['path' => ['1', '2']]);
+        $data = new TrashItemData($trashItem, null, [$ancestor]);
+
+        self::assertFalse($data->isParentInTrash());
+    }
+
+    public function testIsParentInTrashReturnsTrueWhenPathsDoNotMatch(): void
+    {
+        $trashItem = new TrashItem(['path' => ['1', '2', '3'], 'id' => 3]);
+        $ancestor = new Location(['path' => ['1']]);
+
+        $data = new TrashItemData($trashItem, null, [$ancestor]);
+
+        self::assertTrue($data->isParentInTrash());
+    }
+
+    public function testIsParentInTrashWithMultipleAncestors(): void
+    {
+        $trashItem = new TrashItem(['path' => ['1', '2', '3', '4'], 'id' => 4]);
+
+        $ancestor1 = new Location(['path' => ['1']]);
+        $ancestor2 = new Location(['path' => ['1', '2', '3']]);
+
+        $data = new TrashItemData($trashItem, null, [$ancestor1, $ancestor2]);
+
+        self::assertFalse($data->isParentInTrash());
+    }
+}
