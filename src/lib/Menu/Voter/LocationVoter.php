21cf2e6f88d45c65d45f970bd892c86c69612448
Tomasz Białczak <tomasz.bialczak@ibexa.co>
2025-06-25 20:06:39 +0200

    IBX-6554: Enhanced null safety and refactored location ID checks in LocationVoter

--- src/lib/Menu/Voter/LocationVoter.php
+++ src/lib/Menu/Voter/LocationVoter.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
 namespace Ibexa\AdminUi\Menu\Voter;
 
 use Ibexa\Core\MVC\Symfony\View\ContentView;
+use Ibexa\Core\Repository\Values\Content\Location;
 use Knp\Menu\ItemInterface;
 use Knp\Menu\Matcher\Voter\VoterInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -26,20 +27,24 @@ class LocationVoter implements VoterInterface
 
     public function matchItem(ItemInterface $item): ?bool
     {
-        $routes = $item->getExtra('routes', []);
+        foreach ($item->getExtra('routes', []) as $route) {
+            $routeName = $route['route'] ?? null;
+            $locationId = isset($route['parameters']['locationId']) ? (int)$route['parameters']['locationId'] : null;
 
-        foreach ($routes as $route) {
-            if (isset($route['route']) && $route['route'] === self::CONTENT_VIEW_ROUTE_NAME) {
-                $request = $this->requestStack->getCurrentRequest();
-                $contentView = $request->attributes->get('view');
-                $locationId = $route['parameters']['locationId'];
+            if ($routeName !== self::CONTENT_VIEW_ROUTE_NAME || $locationId === null) {
+                continue;
+            }
 
-                if ($contentView instanceof ContentView) {
-                    $location = $contentView->getLocation();
-                    if ($location !== null && in_array($locationId, $location->path ?? [$location->id])) {
-                        return true;
-                    }
-                }
+            $request = $this->requestStack->getCurrentRequest();
+            $contentView = $request ? $request->attributes->get('view') : null;
+            $location = $contentView instanceof ContentView ? $contentView->getLocation() : null;
+
+            if (!$location instanceof Location) {
+                continue;
+            }
+
+            if (in_array($locationId, array_map('intval', $location->getPath()), true)) {
+                return true;
             }
         }
 
