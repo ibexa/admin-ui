48e0c2b93de82292e565e20fb6b2d0cfa57d2773
Tomasz Białczak <tomasz.bialczak@ibexa.co>
2025-07-09 11:31:29 +0200

    IBX-10221: Simplified Location mock creation in tests and added unit tests for IsRoot specification

---
+++ tests/lib/Specification/Location/IsRootTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Tests\AdminUi\Specification\Location;
+
+use Ibexa\AdminUi\Specification\Location\IsRoot;
+use Ibexa\Contracts\Core\Repository\Values\Content\Location;
+use PHPUnit\Framework\TestCase;
+
+class IsRootTest extends TestCase
+{
+    /**
+     * @covers \Ibexa\AdminUi\Specification\Location\IsRoot::isSatisfiedBy
+     */
+    public function testReturnsTrueWhenLocationDepthIsOne(): void
+    {
+        $specification = new IsRoot();
+
+        $location = $this->createLocationWithDepth(1);
+
+        $this->assertTrue($specification->isSatisfiedBy($location));
+    }
+
+    /**
+     * @covers \Ibexa\AdminUi\Specification\Location\IsRoot::isSatisfiedBy
+     */
+    public function testReturnsFalseWhenLocationDepthIsNotOne(): void
+    {
+        $specification = new IsRoot();
+
+        $location = $this->createLocationWithDepth(2);
+
+        $this->assertFalse($specification->isSatisfiedBy($location));
+    }
+
+    private function createLocationWithDepth(int $depth): Location
+    {
+        $location = $this->createMock(Location::class);
+        $location->method('getDepth')->willReturn($depth);
+
+        return $location;
+    }
+}
