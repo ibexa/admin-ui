370fe439fb1bb186c79464c3ef166028ff9d462a
tomaszszopinski <59650405+tomaszszopinski@users.noreply.github.com>
2025-09-12 10:34:47 +0200

    IBX-9630: [Behat] Added search in trash coverage (#1695)
    * [Behat] Added search in trash coverage
    
    * cs & phpstan fix
    
    * Additional scenarios
    
    * fixed phpstan
    
    * updated behat yml
    
    * Added setup tag
    
    * changed oss tags in trash.feature
    
    * changed feature approach
    
    * removed migration context
    
    * switched approach to front steps
    
    * Changed OSS tag
    
    * fixed gherkin syntax
    
    * improved locators
    
    * after CR
    
    * more CR fixes
    
    * removed ununse function
    
    * removed unused function from context

--- src/lib/Behat/Page/TrashPage.php
+++ src/lib/Behat/Page/TrashPage.php
@@ -12,6 +12,7 @@ use Behat\Mink\Session;
 use Ibexa\AdminUi\Behat\Component\ContentActionsMenu;
 use Ibexa\AdminUi\Behat\Component\Dialog;
 use Ibexa\AdminUi\Behat\Component\Table\TableBuilder;
+use Ibexa\AdminUi\Behat\Component\TrashSearch;
 use Ibexa\AdminUi\Behat\Component\UniversalDiscoveryWidget;
 use Ibexa\Behat\Browser\Locator\VisibleCSSLocator;
 use Ibexa\Behat\Browser\Page\Page;
@@ -32,19 +33,24 @@ class TrashPage extends Page
     /** @var \Ibexa\AdminUi\Behat\Component\Table\Table */
     private $table;
 
+    /** @var \Ibexa\AdminUi\Behat\Component\TrashSearch */
+    private TrashSearch $trashSearch;
+
     public function __construct(
         Session $session,
         Router $router,
         UniversalDiscoveryWidget $universalDiscoveryWidget,
         Dialog $dialog,
         ContentActionsMenu $contentActionsMenu,
-        TableBuilder $tableBuilder
+        TableBuilder $tableBuilder,
+        TrashSearch $trashSearch
     ) {
         parent::__construct($session, $router);
         $this->universalDiscoveryWidget = $universalDiscoveryWidget;
         $this->dialog = $dialog;
         $this->contentActionsMenu = $contentActionsMenu;
         $this->table = $tableBuilder->newTable()->build();
+        $this->trashSearch = $trashSearch;
     }
 
     public function hasElement(string $itemType, string $itemName): bool
@@ -82,6 +88,27 @@ class TrashPage extends Page
     public function select(array $parameters)
     {
         $this->table->getTableRow($parameters)->select();
+    }
+
+    public function searchByText(string $searchQuery): void
+    {
+        $this->trashSearch->submitSearchText($searchQuery);
+        $this->trashSearch->confirmSearch();
+    }
+
+    public function filterByContentType(string $contentType): void
+    {
+        $this->trashSearch->filterByContentType($contentType);
+    }
+
+    public function filterBySection(string $section): void
+    {
+        $this->trashSearch->filterBySection($section);
+    }
+
+    public function filterByContentItemCreator(string $contentItemCreator): void
+    {
+        $this->trashSearch->filterByContentItemCreator($contentItemCreator);
     }
 
     public function restoreSelectedItems()
