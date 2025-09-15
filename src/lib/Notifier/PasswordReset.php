4716125ecca760da4f00b9a24bd29ff328b939db
Dawid Parafinski <dawid.parafinski@ibexa.co>
2025-09-15 16:16:51 +0200

    override user bundle notifiers

---
+++ src/lib/Notifier/PasswordReset.php
@@ -0,0 +1,107 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\AdminUi\Notifier;
+
+use Ibexa\Contracts\Core\Repository\Values\User\User;
+use Ibexa\Contracts\Core\SiteAccess\ConfigResolverInterface;
+use Ibexa\Contracts\Notifications\Service\NotificationServiceInterface;
+use Ibexa\Contracts\Notifications\Value\Notification\SymfonyNotificationAdapter;
+use Ibexa\Contracts\Notifications\Value\Recipent\SymfonyRecipientAdapter;
+use Ibexa\Contracts\Notifications\Value\Recipent\UserRecipient;
+use Ibexa\Contracts\User\Notification\UserPasswordReset;
+use Ibexa\Contracts\User\PasswordReset\Notifier;
+use Swift_Image;
+use Swift_Mailer;
+use Swift_Message;
+use Twig\Environment;
+
+class PasswordReset implements Notifier
+{
+    private ConfigResolverInterface $configResolver;
+
+    private Swift_Mailer $mailer;
+
+    private Environment $twig;
+
+    private NotificationServiceInterface $notificationService;
+
+    private string $projectDir;
+
+    public function __construct(
+        ConfigResolverInterface $configResolver,
+        Swift_Mailer $mailer,
+        Environment $twig,
+        NotificationServiceInterface $notificationService,
+        string $projectDir
+    ) {
+        $this->configResolver = $configResolver;
+        $this->mailer = $mailer;
+        $this->twig = $twig;
+        $this->notificationService = $notificationService;
+        $this->projectDir = $projectDir;
+    }
+
+    public function sendMessage(User $user, string $hashKey): void
+    {
+        if ($this->isNotifierConfigured()) {
+            $this->sendNotification($user, $hashKey);
+
+            return;
+        }
+
+        // Swiftmailer delivery has to be kept to maintain backwards compatibility
+        $template = $this->twig->load($this->configResolver->getParameter('user_forgot_password.templates.mail'));
+
+        $senderAddress = $this->configResolver->hasParameter('sender_address', 'swiftmailer.mailer')
+            ? $this->configResolver->getParameter('sender_address', 'swiftmailer.mailer')
+            : '';
+
+        $subject = $template->renderBlock('subject');
+        $from = $template->renderBlock('from') ?: $senderAddress;
+
+        $message = (new Swift_Message())
+            ->setSubject($subject)
+            ->setTo($user->email);
+
+        $embeddedHeader = $message->embed(Swift_Image::fromPath(
+            $this->projectDir . '/public/bundles/ibexaadminui/img/mail/header.png'
+        ));
+
+        $body = $template->renderBlock('body', [
+            'hash_key' => $hashKey,
+            'header_img_path' => $embeddedHeader,
+        ]);
+
+        $message->setBody($body, 'text/html');
+
+        if (empty($from) === false) {
+            $message->setFrom($from);
+        }
+
+        $this->mailer->send($message);
+    }
+
+    private function sendNotification(User $user, string $token): void
+    {
+        $this->notificationService->send(
+            new SymfonyNotificationAdapter(
+                new UserPasswordReset($user, $token),
+            ),
+            [new SymfonyRecipientAdapter(new UserRecipient($user))],
+        );
+    }
+
+    private function isNotifierConfigured(): bool
+    {
+        $subscriptions = $this->configResolver->getParameter('notifications.subscriptions');
+
+        return array_key_exists(UserPasswordReset::class, $subscriptions)
+            && !empty($subscriptions[UserPasswordReset::class]['channels']);
+    }
+}
---
+++ src/lib/Notifier/UserInvitation.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\AdminUi\Notifier;
+
+use Ibexa\Contracts\Core\SiteAccess\ConfigResolverInterface;
+use Ibexa\Contracts\User\Invitation\Invitation;
+use Ibexa\Contracts\User\Invitation\InvitationSender;
+use Swift_Image;
+use Swift_Mailer;
+use Swift_Message;
+use Twig\Environment;
+
+final class UserInvitation implements InvitationSender
+{
+    private Environment $twig;
+
+    private ConfigResolverInterface $configResolver;
+
+    private Swift_Mailer $mailer;
+
+    private string $projectDir;
+
+    public function __construct(
+        Environment $twig,
+        ConfigResolverInterface $configResolver,
+        Swift_Mailer $mailer,
+        string $projectDir
+    ) {
+        $this->twig = $twig;
+        $this->configResolver = $configResolver;
+        $this->mailer = $mailer;
+        $this->projectDir = $projectDir;
+    }
+
+    public function sendInvitation(Invitation $invitation): void
+    {
+        $template = $this->twig->load(
+            $this->configResolver->getParameter(
+                'user_invitation.templates.mail',
+                null,
+                $invitation->getSiteAccessIdentifier()
+            )
+        );
+
+        $senderAddress = $this->configResolver->hasParameter('sender_address', 'swiftmailer.mailer')
+            ? $this->configResolver->getParameter('sender_address', 'swiftmailer.mailer')
+            : '';
+
+        $subject = $template->renderBlock('subject');
+        $from = $template->renderBlock('from') ?: $senderAddress;
+
+        $message = (new Swift_Message())
+            ->setSubject($subject)
+            ->setTo($invitation->getEmail());
+
+        $mailImagesDir = $this->projectDir . '/public/bundles/ibexaadminui/img/mail/';
+        $embeddedHeader = $message->embed(Swift_Image::fromPath($mailImagesDir . 'header.jpg'));
+        $embeddedBtnPrimaryLeftSide = $message->embed(Swift_Image::fromPath($mailImagesDir . 'btn_primary_left_side.jpg'));
+        $embeddedBtnPrimaryRightSide = $message->embed(Swift_Image::fromPath($mailImagesDir . 'btn_primary_right_side.jpg'));
+
+        $body = $template->renderBlock('body', [
+            'invite_hash' => $invitation->getHash(),
+            'siteaccess' => $invitation->getSiteAccessIdentifier(),
+            'invitation' => $invitation,
+            'header_path' => $embeddedHeader,
+            'btn_primary_left_side' => $embeddedBtnPrimaryLeftSide,
+            'btn_primary_right_side' => $embeddedBtnPrimaryRightSide,
+        ]);
+
+        $message->setBody($body, 'text/html');
+
+        if (empty($from) === false) {
+            $message->setFrom($from);
+        }
+
+        $this->mailer->send($message);
+    }
+}
