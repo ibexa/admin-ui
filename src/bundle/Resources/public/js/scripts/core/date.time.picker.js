884c366fecb3e81e3debc2f63000fcd98d87c555
Robert Patrascu <162968581+RopRaptor@users.noreply.github.com>
2025-10-22 14:42:04 +0200

    IBX-10668: make date picker configuration respect 'isStatic' attribute (#1741)
    Co-authored-by: konradoboza <konrad.oboza@ibexa.co>
    Co-authored-by: Łukasz Ostafin <ostafin.lucas@gmail.com>

--- src/bundle/Resources/public/js/scripts/core/date.time.picker.js
+++ src/bundle/Resources/public/js/scripts/core/date.time.picker.js
@@ -136,6 +136,8 @@ class DateTimePicker {
 
     init() {
         const flatpickr = getFlatpickr();
+
+        this.flatpickrConfig.static = this.inputField.dataset.isStatic === 'true';
         this.flatpickrInstance = flatpickr(this.inputField, this.flatpickrConfig);
 
         this.inputField.addEventListener('input', this.onInput, false);
