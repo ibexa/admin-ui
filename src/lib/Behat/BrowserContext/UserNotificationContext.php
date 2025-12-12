fc64923431ba0dd45069df043a1ed31b0636fa70
Kamil Sznajdrowicz <93484579+KamilSznajdrowicz@users.noreply.github.com>
2025-12-12 09:55:50 +0100

    IBX-10592: [Behat] Implemented basic behat coverage for notification feature (#1746)
    * Add notification config
    
    * Add new scenarios to notification feature
    
    * Code cleanup and naming improvments
    
    * Fix
    
    * Fix after review
    
    * Fix flaky issue in notification tests
    
    * Fix hover issue in notification tests
    
    * Fix mouseoverandclick method
    
    * Fix flaky issues
    
    * Add count step to scenarios
    
    * Fix count scenario
    
    * Fixes after review
    
    * Fix after CR

--- src/lib/Behat/BrowserContext/UserNotificationContext.php
+++ src/lib/Behat/BrowserContext/UserNotificationContext.php
@@ -12,6 +12,7 @@ use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\TableNode;
 use Ibexa\AdminUi\Behat\Component\UpperMenu;
 use Ibexa\AdminUi\Behat\Component\UserNotificationPopup;
+use Ibexa\AdminUi\Behat\Page\NotificationsViewAllPage;
 use PHPUnit\Framework\Assert;
 
 class UserNotificationContext implements Context
@@ -22,10 +23,13 @@ class UserNotificationContext implements Context
     /** @var \Ibexa\AdminUi\Behat\Component\UserNotificationPopup */
     private $userNotificationPopup;
 
-    public function __construct(UpperMenu $upperMenu, UserNotificationPopup $userNotificationPopup)
+    private NotificationsViewAllPage $notificationsViewAllPage;
+
+    public function __construct(UpperMenu $upperMenu, UserNotificationPopup $userNotificationPopup, NotificationsViewAllPage $notificationsViewAllPage)
     {
         $this->upperMenu = $upperMenu;
         $this->userNotificationPopup = $userNotificationPopup;
+        $this->notificationsViewAllPage = $notificationsViewAllPage;
     }
 
     /**
@@ -46,5 +50,134 @@ class UserNotificationContext implements Context
 
         $this->userNotificationPopup->verifyIsLoaded();
         $this->userNotificationPopup->clickNotification($type, $description);
+    }
+
+    /**
+     * @Then the notification appears with details:
+     */
+    public function notificationAppearsWithDetails(TableNode $notificationDetails): void
+    {
+        $type = $notificationDetails->getHash()[0]['Type'];
+        $author = $notificationDetails->getHash()[0]['Author'];
+        $description = $notificationDetails->getHash()[0]['Description'];
+        $date = $notificationDetails->getHash()[0]['Date'];
+
+        $this->userNotificationPopup->verifyIsLoaded();
+        $this->userNotificationPopup->verifyNotification($type, $author, $description, $date, true);
+    }
+
+    /**
+     * @When I open notification menu with description :description
+     */
+    public function iOpenNotificationMenu(string $description): void
+    {
+        $this->userNotificationPopup->openNotificationMenu($description);
+    }
+
+    /**
+     * @When I perform the :action action on the notification
+     */
+    public function iPerformNotificationAction(string $action): void
+    {
+        $this->userNotificationPopup->clickActionButton($action);
+    }
+
+    /**
+     * @Then the notification should have :expectedAction action available
+     */
+    public function verifyNotificationAction(string $expectedAction): void
+    {
+        $this->userNotificationPopup->findActionButton($expectedAction);
+    }
+
+    /**
+     * @When I mark all notifications as read
+     */
+    public function markAllNotificationsAsRead(): void
+    {
+        $this->userNotificationPopup->verifyIsLoaded();
+        $this->userNotificationPopup->clickOnMarkAllAsReadButton();
+    }
+
+    /**
+     * @When I go to the list of all notifications
+     */
+    public function goToAllNotificationsList(): void
+    {
+        $this->userNotificationPopup->verifyIsLoaded();
+        $this->userNotificationPopup->clickViewAllNotificationsButton();
+    }
+
+    /**
+     * @Then there is :notificationTitle notification on list
+     */
+    public function thereIsNotificationOnList(string $notificationTitle): void
+    {
+        $this->notificationsViewAllPage->verifyIsLoaded();
+        $this->notificationsViewAllPage->verifyNotificationIsOnList($notificationTitle);
+    }
+
+    /**
+     * @When I mark notification as unread with title :notificationTitle
+     */
+    public function iMarkNotificationAsUnread(string $notificationTitle): void
+    {
+        $this->notificationsViewAllPage->markAsUnread($notificationTitle);
+    }
+
+    /**
+     * @Then the notification with title :notificationTitle has status :notificationStatus
+     */
+    public function verifyNotificationStatus(string $notificationTitle, string $notificationStatus): void
+    {
+        Assert::assertEquals($notificationStatus, $this->notificationsViewAllPage->getStatusForNotification($notificationTitle));
+    }
+
+    /**
+     * @When I go to content of notification with title :notificationTitle
+     */
+    public function iGoToContent(string $notificationTitle): void
+    {
+        $this->notificationsViewAllPage->goToContent($notificationTitle);
+    }
+
+    /**
+     * @When I delete notification with title :notificationTitle
+     */
+    public function iDeleteNotification(string $notificationTitle): void
+    {
+        $this->notificationsViewAllPage->deleteNotification($notificationTitle);
+    }
+
+    /**
+     * @Then an empty notifications state in the popup should be visible
+     */
+    public function emptyNotificationsStateInPopup(): void
+    {
+        $this->userNotificationPopup->assertEmptyStateVisible();
+    }
+
+    /**
+     * @Then an empty notifications state should be visible in the All Notifications view
+     */
+    public function emptyNotificationsStateInAllNotificationsView(): void
+    {
+        $this->notificationsViewAllPage->assertEmptyStateVisible();
+    }
+
+    /**
+     * @Then the notifications popup counter should display :expectedCount
+     */
+    public function iShouldSeeNotificationsCountInPopup(int $expectedCount): void
+    {
+        $this->userNotificationPopup->verifyNotificationsCount($expectedCount);
+    }
+
+    /**
+     * @Then I should see :expectedCount notifications in the All Notifications view
+     */
+    public function thereShouldBeNotificationsInAllNotificationsView(int $expectedCount): void
+    {
+        $this->notificationsViewAllPage->verifyNotificationsCount($expectedCount);
     }
 }
