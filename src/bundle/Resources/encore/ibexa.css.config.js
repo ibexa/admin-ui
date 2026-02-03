9031fbdf904eff979136124a3747096e94e1ba8d
tischsoic <symfiz@gmail.com>
2026-02-03 15:18:41 +0100

    draft list delete button

--- src/bundle/Resources/encore/ibexa.css.config.js
+++ src/bundle/Resources/encore/ibexa.css.config.js
@@ -19,8 +19,6 @@ module.exports = (Encore) => {
         ])
         .addEntry('ibexa-admin-ui-security-base-css', [
             path.resolve(__dirname, '../public/scss/ibexa-bootstrap.scss'),
-            path.resolve('./vendor/ibexa/admin-ui-assets/src/bundle/Resources/public/vendors/ids-assets/dist/css/styles.css'),
             path.resolve(__dirname, '../public/scss/ibexa.scss'),
-            path.resolve('./vendor/ibexa/admin-ui-assets/src/bundle/Resources/public/vendors/ids-assets/dist/css/styles.css'),
         ]);
 };
