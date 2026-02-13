91e7c8888f33bfb9447db828370ae90bbe932a74
Mikolaj Adamczyk <mikolaj.adamczyk@ez.no>
2026-02-13 15:12:46 +0100

    IBX-11213: Updated translation modal footer hook and source language options (#1819)
    IBX-11213: Wired side-by-side controller flow and create request mapping

--- src/bundle/Resources/public/js/scripts/admin.location.add.translation.js
+++ src/bundle/Resources/public/js/scripts/admin.location.add.translation.js
@@ -2,7 +2,13 @@
     const SELECTOR_MODAL = '.ibexa-modal';
     const toggleBtnDisabledState = (select) => {
         const modal = select.closest(SELECTOR_MODAL);
+        if (!modal) {
+            return;
+        }
         const buttonCreate = modal.querySelector('.ibexa-btn--create-translation');
+        if (!buttonCreate) {
+            return;
+        }
 
         buttonCreate.toggleAttribute('disabled', !select.value);
     };
