2ec4d5b74f33c7214a7e06acb5229cb16ed5e821
Paweł Niedzielski <pawel.niedzielski@ibexa.co>
2026-02-20 15:10:22 +0100

    Added baseline for versions prior to PHP 8.4

--- phpstan-baseline.neon.php
+++ phpstan-baseline.neon.php
@@ -7,10 +7,14 @@
 declare(strict_types=1);
 
 $includes = [];
-if (PHP_VERSION_ID < 80000) {
+if (PHP_VERSION_ID < 8_00_00) {
     $includes[] = __DIR__ . '/phpstan-baseline-7.4.neon';
 } else {
     $includes[] = __DIR__ . '/phpstan-baseline-gte-8.0.neon';
+}
+
+if (PHP_VERSION_ID < 8_04_00) {
+    $includes[] = __DIR__ . '/phpstan-baseline-lte-8.3.neon';
 }
 
 $config = [];
--- tests/lib/Form/DataTransformer/LanguageTransformerTest.php
+++ tests/lib/Form/DataTransformer/LanguageTransformerTest.php
@@ -52,7 +52,7 @@ class LanguageTransformerTest extends TestCase
         $this->expectException(TransformationFailedException::class);
         $this->expectExceptionMessage('Expected a ' . Language::class . ' object.');
 
-        $result = $transformer->transform($value);
+        $transformer->transform($value);
     }
 
     public function testReverseTransformWithLanguageCode(): void
