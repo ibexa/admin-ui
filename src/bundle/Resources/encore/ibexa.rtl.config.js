1fbb2005dbbd774e127d549b7a262b9a710891b9
Lukasz Ostafin <ostafin.lucas@gmail.com>
2026-03-04 13:50:50 +0100

    rlt poc

---
+++ src/bundle/Resources/encore/ibexa.rtl.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    excludedFiles: ['ibexa-admin-ui-overwrite-rtl-css.css', 'ibexa-admin-ui-overwrite-rtl-css.js'],
+    excludedEntrypoints: ['ibexa-admin-ui-overwrite-rtl-css'],
+};
