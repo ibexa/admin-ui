0686d90a2f1f0f7fb0958ba8a1105a1eb52064c3
Bartek Wajda <bartlomiej.wajda@ibexa.co>
2026-03-25 18:37:23 +0100

    IBX-10602: Refactored `NodeFactory` to fetch translations for n first items with a new endpoint fetching translated names (#1777)

---
+++ src/lib/REST/Output/ValueObjectVisitor/ContentTree/TranslatedNamesListVisitor.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\AdminUi\REST\Output\ValueObjectVisitor\ContentTree;
+
+use Ibexa\Contracts\Rest\Output\Generator;
+use Ibexa\Contracts\Rest\Output\ValueObjectVisitor;
+use Ibexa\Contracts\Rest\Output\Visitor;
+
+final class TranslatedNamesListVisitor extends ValueObjectVisitor
+{
+    private const MAIN_ELEMENT = 'ContentTreeTranslatedNamesList';
+
+    /**
+     * @param \Ibexa\AdminUi\REST\Value\ContentTree\TranslatedNamesList $data
+     */
+    public function visit(Visitor $visitor, Generator $generator, $data): void
+    {
+        $generator->startObjectElement(self::MAIN_ELEMENT);
+        $visitor->setHeader('Content-Type', $generator->getMediaType(self::MAIN_ELEMENT));
+
+        $generator->startList('entries');
+        foreach ($data->getVersionInfoList() as $versionInfo) {
+            $generator->startHashElement('entry');
+            $generator->valueElement('contentId', $versionInfo->getContentInfo()->getId());
+
+            $generator->startList('translatedNames');
+            foreach ($versionInfo->getNames() as $languageCode => $name) {
+                $generator->startHashElement('translatedName');
+                $generator->valueElement('languageCode', $languageCode);
+                $generator->valueElement('name', $name);
+                $generator->endHashElement('translatedName');
+            }
+            $generator->endList('translatedNames');
+
+            $generator->endHashElement('entry');
+        }
+        $generator->endList('entries');
+
+        $generator->endObjectElement(self::MAIN_ELEMENT);
+    }
+}
