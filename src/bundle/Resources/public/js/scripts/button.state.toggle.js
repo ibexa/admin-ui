a09f062520db5749bdb7c6a9c144fb83212a802d
tischsoic <symfiz@gmail.com>
2026-03-26 10:38:47 +0100

    IBX-11506: Update bulk toggle checkbox selectors

--- src/bundle/Resources/public/js/scripts/button.state.toggle.js
+++ src/bundle/Resources/public/js/scripts/button.state.toggle.js
@@ -1,8 +1,9 @@
 (function (global, doc) {
+    const SELECTOR_TABLE_CHECKBOX = '.ids-input--checkbox';
     const toggleForms = doc.querySelectorAll('.ibexa-toggle-btn-state');
 
     toggleForms.forEach((toggleForm) => {
-        const checkboxes = [...toggleForm.querySelectorAll('.ibexa-table__cell--has-checkbox .ibexa-input--checkbox')];
+        const checkboxes = [...toggleForm.querySelectorAll(`.ibexa-table__cell--has-checkbox ${SELECTOR_TABLE_CHECKBOX}`)];
         const buttonRemove = doc.querySelector(toggleForm.dataset.toggleButtonId);
 
         if (!buttonRemove) {
