883c70dca8f6f03cc150e03536ea743752dea08c
Bartek Wajda <bartlomiej.wajda@ibexa.co>
2026-04-16 13:43:59 +0200

    IBX-11487: Refactored backend

---
+++ src/bundle/Asset/RtlEntrypointLookup.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Bundle\AdminUi\Asset;
+
+use Ibexa\Contracts\AdminUi\Rtl\RtlModeResolverInterface;
+use Symfony\WebpackEncoreBundle\Asset\EntrypointLookup;
+use Symfony\WebpackEncoreBundle\Asset\EntrypointLookupInterface;
+use Symfony\WebpackEncoreBundle\Asset\IntegrityDataProviderInterface;
+
+final readonly class RtlEntrypointLookup implements EntrypointLookupInterface, IntegrityDataProviderInterface
+{
+    public function __construct(
+        private EntrypointLookupInterface $inner,
+        private RtlModeResolverInterface $rtlModeResolver,
+    ) {
+    }
+
+    /**
+     * @return array<string>
+     */
+    public function getJavaScriptFiles(string $entryName): array
+    {
+        return $this->inner->getJavaScriptFiles($entryName);
+    }
+
+    /**
+     * @return array<string>
+     */
+    public function getCssFiles(string $entryName): array
+    {
+        if ($this->rtlModeResolver->isRtl() && $this->entryExists($entryName . '-rtl')) {
+            $entryName .= '-rtl';
+        }
+
+        return $this->inner->getCssFiles($entryName);
+    }
+
+    private function entryExists(string $entryName): bool
+    {
+        if ($this->inner instanceof EntrypointLookup) {
+            return $this->inner->entryExists($entryName);
+        }
+
+        return false;
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    public function getIntegrityData(): array
+    {
+        if ($this->inner instanceof IntegrityDataProviderInterface) {
+            return $this->inner->getIntegrityData();
+        }
+
+        return [];
+    }
+
+    public function reset(): void
+    {
+        $this->inner->reset();
+    }
+}
