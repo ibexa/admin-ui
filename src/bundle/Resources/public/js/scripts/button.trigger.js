21c7f1db617ea58d4d01c02d3b6d01a83dda51ca
tischsoic <symfiz@gmail.com>
2026-04-22 14:55:39 +0200

    fix: finish segmentation DS button migration

--- src/bundle/Resources/public/js/scripts/button.trigger.js
+++ src/bundle/Resources/public/js/scripts/button.trigger.js
@@ -2,7 +2,7 @@
     doc.addEventListener(
         'DOMContentLoaded',
         () => {
-            const buttons = doc.querySelectorAll('.ids-button--trigger, .ibexa-btn--trigger');
+            const buttons = doc.querySelectorAll('.ids-btn--trigger, .ids-button--trigger, .ibexa-btn--trigger');
             const trigger = (event) => {
                 event.preventDefault();
 
