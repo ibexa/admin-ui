a0b4841584f0bef89c7095c20a635e44304c5b65
Aleksandra Bozek <aleksandra.m.bozek@gmail.com>
2026-05-14 14:08:12 +0200

    IBX-8555: Prevented toggle btn listeners duplication

--- src/bundle/Resources/public/js/scripts/toggle.button.state.toggle.js
+++ src/bundle/Resources/public/js/scripts/toggle.button.state.toggle.js
@@ -1,5 +1,13 @@
 (function (global, doc, eZ) {
+    const TOGGLE_INITIALIZED_ATTR = 'data-ibexa-toggle-initialized';
+
     doc.querySelectorAll('.ibexa-toggle').forEach((toggleNode) => {
+        if (toggleNode.hasAttribute(TOGGLE_INITIALIZED_ATTR)) {
+            return;
+        }
+
+        toggleNode.setAttribute(TOGGLE_INITIALIZED_ATTR, 'true');
+
         const toggleButton = new eZ.core.ToggleButton({ toggleNode });
 
         toggleButton.init();
