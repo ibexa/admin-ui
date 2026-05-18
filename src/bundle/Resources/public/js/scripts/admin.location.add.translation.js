e86ef53b5329c395752383be6f5b74c0234b5b12
Aleksandra Bożek <aleksandra.m.bozek@gmail.com>
2026-05-18 10:03:31 +0200

    IBX-11362: Disabled create btn when no target langugage available (#1880)

--- src/bundle/Resources/public/js/scripts/admin.location.add.translation.js
+++ src/bundle/Resources/public/js/scripts/admin.location.add.translation.js
@@ -1,15 +1,22 @@
 (function (global, doc) {
     const SELECTOR_MODAL = '.ibexa-modal';
+    const observerConfig = {
+        attributes: true,
+        attributeFilter: ['class'],
+    };
     const toggleBtnDisabledState = (select) => {
         const modal = select.closest(SELECTOR_MODAL);
         const buttonCreate = modal.querySelector('.ibexa-btn--create-translation');
-
         buttonCreate.toggleAttribute('disabled', !select.value);
     };
 
     doc.querySelectorAll('.ibexa-translation__language-wrapper--language').forEach((select) => {
-        toggleBtnDisabledState(select);
+        const dropdown = select.closest('.ibexa-dropdown');
+        const observer = new MutationObserver(() => toggleBtnDisabledState(select));
 
+        toggleBtnDisabledState(select);
         select.addEventListener('change', ({ target }) => toggleBtnDisabledState(target), false);
+
+        observer.observe(dropdown, observerConfig);
     });
 })(window, window.document);
